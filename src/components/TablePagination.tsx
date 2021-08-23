83f4ecccaba7b0e7c8434aac34f7181312280134
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2021-08-23 13:05:58 +0200

    Improved pagination style

--- src/components/TablePagination.tsx
+++ src/components/TablePagination.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 export function TablePagination() {
+  const pages = [1, 2, 3, 4, 5]
+
   return (
-    <div className="d-flex justify-content-between py-2">
+    <div className="d-flex justify-content-between align-items-center py-1">
       <div>1–50 di 2.056</div>
-      <div>1 2 3 4 5</div>
+      <div className="d-flex align-items-center">
+        <i className="text-primary fs-5 bi bi-arrow-left" />
+        {pages.map((p, i) => {
+          return (
+            <Button className="text-decoration-none" key={i} variant="link" onClick={() => {}}>
+              {p}
+            </Button>
+          )
+        })}
+        <i className="text-primary fs-5 bi bi-arrow-right" />
+      </div>
     </div>
   )
 }
