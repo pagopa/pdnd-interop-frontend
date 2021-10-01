6bc7f21f1601b1965e662cb0162bc0691c7522f2
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2021-10-01 16:57:07 +0200

    Fixed bug on logout/login with different user

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,15 +1,19 @@
 import { useContext, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-import { UserContext } from '../lib/context'
+import { PartyContext, UserContext } from '../lib/context'
 import { storageDelete } from '../lib/storage-utils'
 
 export const useLogout = () => {
+  const { setParty, setAvailableParties } = useContext(PartyContext)
   const { setUser } = useContext(UserContext)
   const history = useHistory()
 
   useEffect(() => {
     // clean up user
     setUser(null)
+    // clean up parties
+    setParty(null)
+    setAvailableParties([])
     // delete everything from the storage
     storageDelete('user')
     storageDelete('bearer')
