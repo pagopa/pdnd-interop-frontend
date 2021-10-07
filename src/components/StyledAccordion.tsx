b775f754f3115e0e6eddd103de4f949f17a74ec0
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2021-10-07 18:34:54 +0200

    Added accordion to explore attributes and updated agreement UI to show refused attributes

---
+++ src/components/StyledAccordion.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+type AccordionEntry = {
+  summary: string | JSX.Element
+  details: string | JSX.Element
+}
+
+type StyledAccordionProps = {
+  entries: AccordionEntry[]
+}
+
+export function StyledAccordion({ entries }: StyledAccordionProps) {
+  const [index, setIndex] = useState<number | null>(null)
+
+  const collapse = () => {
+    setIndex(null)
+  }
+
+  const wrapToggle = (newIndex: number) => (_: any) => {
+    if (newIndex === index) {
+      collapse()
+    } else {
+      setIndex(newIndex)
+    }
+  }
+  return (
+    <React.Fragment>
+      {entries.map(({ summary, details }, i: number) => {
+        return (
+          <div className="mt-2 py-3 px-3 border-bottom border-secondary" key={i}>
+            <button
+              className="reset-btn w-100 d-flex justify-content-between align-items-center"
+              onClick={wrapToggle(i)}
+            >
+              <span>{summary}</span>
+              <i className={`text-primary fs-5 bi bi-chevron-${i === index ? 'up' : 'down'}`} />
+            </button>
+            <div
+              style={{
+                overflow: 'hidden',
+                transition: '0.45s max-height ease-in-out',
+                maxHeight: i !== index ? 0 : 500,
+              }}
+            >
+              <div className="pt-3">{details}</div>
+            </div>
+          </div>
+        )
+      })}
+    </React.Fragment>
+  )
+}
