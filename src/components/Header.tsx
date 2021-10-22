05a27dfd10026be037e70d0cca467ad4ee2bf6ed
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2021-10-22 17:41:18 +0200

    Slightly refactored header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,121 +1,11 @@
-import React, { FunctionComponent, useContext, useEffect, useState } from 'react'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
-import { Link, useLocation } from 'react-router-dom'
-import { RouteConfig } from '../../types'
-import { isOperatorAPI, isOperatorSecurity } from '../lib/auth-utils'
-import { ROUTES, USER_ROLE_LABEL } from '../lib/constants'
-import { PartyContext } from '../lib/context'
-import { storageWrite } from '../lib/storage-utils'
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+import { ROUTES } from '../lib/constants'
 import { includesAny } from '../lib/string-utils'
 import { MainHeader } from './MainHeader'
-import { PageTitle } from './PageTitle'
+import { PlatformHeader } from './PlatformHeader'
 import { StyledContainer } from './Shared/StyledContainer'
-
-type NavLinkProps = {
-  route: RouteConfig
-  eventKey: string
-  customStyle?: boolean
-}
-
-// TEMP REFACTOR component
-
-function PlatformHeader() {
-  const { party, availableParties, setParty } = useContext(PartyContext)
-  const [activeKey, setActiveKey] = useState('')
-  const location = useLocation()
-
-  useEffect(() => {
-    const bits = location.pathname
-      .split('/')
-      .filter((b) => b)
-      .map((b) => `/${b}`)
-
-    setActiveKey(`${bits[0]}${bits[1]}`)
-  }, [location.pathname])
-
-  const isActive = (route: RouteConfig) => activeKey === route.PATH
-
-  const NavLink: FunctionComponent<NavLinkProps> = ({
-    route,
-    eventKey,
-    children,
-    customStyle = false,
-  }) => {
-    let classNames = `text-primary ${isActive(route) ? 'fw-bold' : 'fw-normal'}`
-    if (customStyle) {
-      classNames = `text-primary border-bottom border-3 ps-0 pe-5 py-2 me-3 text-left lh-sm ${
-        isActive(route) ? 'border-primary' : 'border-white'
-      }`
-    }
-
-    return (
-      <Nav.Link active={false} eventKey={eventKey} as={Link} to={route.PATH} className={classNames}>
-        {children || route.LABEL}
-      </Nav.Link>
-    )
-  }
-
-  const updateActiveParty = (eventKey: string | null, event: any) => {
-    if (!eventKey) {
-      return
-    }
-
-    const bits = eventKey!.split('.')
-    const index = +bits[1] - 1
-
-    if (!Number.isNaN(index)) {
-      setParty(availableParties[index])
-      storageWrite('currentParty', availableParties[index], 'object')
-    }
-  }
-
-  return (
-    <Navbar className="justify-content-between py-0">
-      <StyledContainer className="d-flex align-items-stretch">
-        <Nav>
-          {!isOperatorAPI(party) && (
-            <NavLink customStyle={true} eventKey="1" route={ROUTES.SUBSCRIBE} />
-          )}
-          {!isOperatorSecurity(party) && (
-            <NavLink customStyle={true} eventKey="2" route={ROUTES.PROVIDE} />
-          )}
-        </Nav>
-
-        <div className="d-flex align-items-center justify-content-between">
-          <Nav onSelect={updateActiveParty} className="me-3">
-            <NavDropdown
-              title={`${party ? `${party!.description} (${USER_ROLE_LABEL[party!.role]})` : ''}`}
-              id="nav-dropdown"
-            >
-              {availableParties.map((availableParty, i) => (
-                <NavDropdown.Item key={i} eventKey={`3.${i + 1}`}>
-                  {availableParty.description} ({USER_ROLE_LABEL[availableParty.role]})
-                </NavDropdown.Item>
-              ))}
-            </NavDropdown>
-          </Nav>
-
-          <Nav>
-            <NavLink eventKey="4" route={ROUTES.NOTIFICATION}>
-              <i
-                className={`text-primary fs-5 bi ${
-                  isActive(ROUTES.NOTIFICATION) ? 'bi-bell-fill' : 'bi-bell'
-                }`}
-              />
-            </NavLink>
-            <NavLink eventKey="5" route={ROUTES.PROFILE}>
-              <i
-                className={`text-primary fs-5 bi ${
-                  isActive(ROUTES.PROFILE) ? 'bi-person-fill' : 'bi-person'
-                }`}
-              />
-            </NavLink>
-          </Nav>
-        </div>
-      </StyledContainer>
-    </Navbar>
-  )
-}
+import { StyledRow } from './Shared/StyledRow'
 
 export function Header() {
   const location = useLocation()
@@ -129,7 +19,13 @@ export function Header() {
   return (
     <header>
       <MainHeader />
-      <PageTitle />
+      <div className="bg-white">
+        <StyledContainer>
+          <StyledRow>
+            <h1 className="bg-white py-4">Portale interoperabilità</h1>
+          </StyledRow>
+        </StyledContainer>
+      </div>
       {isInPlatform && <PlatformHeader />}
     </header>
   )
