fbf5de4dfc794c45caaaa4bbf87aa77157fbe8ed
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2021-10-25 15:58:33 +0200

    Moved main navigation to the left side

--- src/views/Provide.tsx
+++ src/views/Provide.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { SectionHeader } from '../components/SectionHeader'
 import { ProtectedSubroutes } from '../components/ProtectedSubroutes'
 import { ROUTES } from '../lib/constants'
 import { Layout } from '../components/Shared/Layout'
@@ -7,8 +6,6 @@ import { Layout } from '../components/Shared/Layout'
 export function Provide() {
   return (
     <Layout>
-      <SectionHeader view="provider" />
-
       <ProtectedSubroutes
         subroutes={ROUTES.PROVIDE.SUBROUTES!}
         redirectSrcRoute={ROUTES.PROVIDE}
