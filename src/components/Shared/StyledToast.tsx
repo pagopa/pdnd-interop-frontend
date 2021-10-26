4bd19a05af9562a49f45b49b1b41eb944255a346
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2021-10-26 15:35:29 +0200

    Replaced Dialog, Toast and Spinner

--- src/components/Shared/StyledToast.tsx
+++ src/components/Shared/StyledToast.tsx
@@ -1,20 +1,9 @@
-import React, { useEffect, useState } from 'react'
-// import { Toast } from 'react-bootstrap'
+import React from 'react'
 import { RequestOutcome, ToastContent, ToastContentWithOutcome } from '../../../types'
-import noop from 'lodash/noop'
+import { Alert, AlertTitle, Snackbar } from '@mui/material'
 
 type StyledToastProps = ToastContentWithOutcome & {
   onClose?: any
-}
-
-const BG_TYPE: { [key in RequestOutcome]: string } = {
-  success: 'success',
-  error: 'danger',
-}
-
-const BG_TYPE_EMOJI: { [key in RequestOutcome]: string } = {
-  success: '🎉',
-  error: '🤬',
 }
 
 const DEFAULT_TEXT: { [key in RequestOutcome]: ToastContent } = {
@@ -25,62 +14,17 @@ const DEFAULT_TEXT: { [key in RequestOutcome]: ToastContent } = {
   },
 }
 
-export function StyledToast({ outcome, title, description, onClose = noop }: StyledToastProps) {
-  const [appear, setAppear] = useState(false)
-
-  useEffect(() => {
-    setAppear(true)
-  }, [])
-
-  const transitionClasses = appear ? 'opacity-100' : 'opacity-0'
-  const transitionTransform = appear ? 'translate(-50%, 0)' : 'translate(-50%, 0.5rem)'
-
+export function StyledToast({ outcome, title, description, onClose }: StyledToastProps) {
   return (
-    <div
-      className={`position-fixed bottom-0 mb-3 shadow-lg border-start border-5 bg-white border-${BG_TYPE[outcome]} ${transitionClasses}`}
-      style={{
-        transform: transitionTransform,
-        left: '50%',
-        width: '20rem',
-        transition: '0.3s all ease-in-out',
-      }}
-      role="alert"
-      aria-live="assertive"
-      aria-atomic="true"
+    <Snackbar
+      open={true}
+      onClose={onClose}
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
     >
-      <div className="border">
-        <div className="d-flex justify-content-between align-items-center pt-2 px-2">
-          <strong>
-            {BG_TYPE_EMOJI[outcome]} {title || DEFAULT_TEXT[outcome].title}
-          </strong>
-          <button
-            type="button"
-            className="btn-close"
-            aria-label="Close"
-            data-dismiss="toast"
-            onClick={onClose}
-          ></button>
-        </div>
-        <p className="px-2 pt-1 pb-2 mb-0 lh-sm">
-          {description || DEFAULT_TEXT[outcome].description}
-        </p>
-      </div>
-    </div>
+      <Alert severity={outcome} onClose={onClose}>
+        <AlertTitle>{title || DEFAULT_TEXT[outcome].title}</AlertTitle>
+        {description || DEFAULT_TEXT[outcome].description}
+      </Alert>
+    </Snackbar>
   )
 }
-
-/* React-bootstrap toast */
-// <Toast
-//   animation={true}
-//   className="position-fixed bottom-0 mb-4"
-//   bg={BG_TYPE[outcome]}
-//   style={{ zIndex: 3, left: '50%', transform: `translate(-50%, 0)` }}
-//   onClose={onClose}
-// >
-//   <Toast.Header>
-//     <strong className="me-auto">
-//       {BG_TYPE_EMOJI[outcome]} {title || DEFAULT_TEXT[outcome].title}
-//     </strong>
-//   </Toast.Header>
-//   <Toast.Body>{description || DEFAULT_TEXT[outcome].description}</Toast.Body>
-// </Toast>
