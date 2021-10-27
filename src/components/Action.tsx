19cc48f880b45844e0ac571202a649c662f5de8a
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2021-10-27 09:43:55 +0200

    Finalized fixing actions

--- src/components/Action.tsx
+++ src/components/Action.tsx
@@ -1,28 +1,10 @@
 import React from 'react'
+import { ActionProps } from '../../types'
 import { StyledButton } from './Shared/StyledButton'
 
-type BtnProps = {
-  to?: string
-  onClick?: any
-  component?: any
-}
-
-type ActionProps = {
-  btnProps?: BtnProps
-  label: string
-  isMock?: boolean
-  className?: string
-  style?: any
-}
-
-export function Action({ btnProps, label, isMock = false, className, style }: ActionProps) {
+export function Action({ btnProps, label, isMock = false }: ActionProps) {
   return (
-    <StyledButton
-      variant="outlined"
-      className={`${isMock ? 'mockFeature' : ''} ${className || ''}`}
-      sx={style || {}}
-      {...btnProps}
-    >
+    <StyledButton variant="outlined" className={isMock ? 'mockFeature' : ''} {...btnProps}>
       {label}
     </StyledButton>
   )
