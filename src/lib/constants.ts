d76acaa095364e5207e3dab7317d1590277edfed
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2021-11-10 15:03:51 +0100

    Fixed operator api create action

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -40,8 +40,8 @@ import { EmptyComponent } from '../components/Shared/EmptyComponent'
 const isDevelopment = !!(process.env.NODE_ENV === 'development')
 
 export const SHOW_DEV_LABELS = isDevelopment || getDevLabels()
-export const USE_MOCK_SPID_USER = false // isDevelopment
-export const DISPLAY_LOGS = false // isDevelopment
+export const USE_MOCK_SPID_USER = isDevelopment
+export const DISPLAY_LOGS = isDevelopment
 
 export const BASE_ROUTE = '/ui'
 
