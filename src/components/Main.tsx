f0ce2687c6320fde2e859a7ab0c76ad6e3b5a832
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2021-11-10 10:00:51 +0100

    Improved breadcrumbs for client

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,17 +1,19 @@
 import React, { useContext } from 'react'
 import { Box } from '@mui/system'
-import { Redirect, Route, Switch } from 'react-router-dom'
+import { Redirect, Route, Switch, useLocation } from 'react-router-dom'
 import { BASE_ROUTE, ROUTES, SHOW_DEV_LABELS } from '../lib/constants'
 import { UserContext } from '../lib/context'
 import { AuthGuard } from './AuthGuard'
 import { StyledBreadcrumbs } from './Shared/StyledBreadcrumbs'
+import { isInPlatform } from '../lib/router-utils'
 
 export function Main() {
   const { user } = useContext(UserContext)
+  const location = useLocation()
 
   return (
     <Box component="main" sx={{ pb: 4 }} className={!SHOW_DEV_LABELS ? ' hideDevLabels' : ''}>
-      <StyledBreadcrumbs />
+      {isInPlatform(location) && <StyledBreadcrumbs />}
       <Switch>
         {Object.values(ROUTES).map(({ PATH, EXACT, COMPONENT, PUBLIC, AUTH_LEVELS }, i) => (
           <Route path={PATH} exact={EXACT} key={i}>
