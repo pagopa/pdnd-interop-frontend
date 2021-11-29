81c3731c8bb9c018ae58944be71f77555d3139ac
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2021-11-29 17:51:21 +0100

    test: added tests for auth-utils

---
+++ src/lib/__tests__/auth-utils-test.ts
@@ -0,0 +1,33 @@
+import { Party } from '../../../types'
+import { isAdmin, isOperatorAPI, isOperatorSecurity } from '../auth-utils'
+
+const exampleParty: Party = {
+  role: 'Manager',
+  platformRole: 'admin',
+  status: 'active',
+  partyId: 'dsjdf-djsfsdoj-sdfjdso',
+  attributes: [],
+  description: 'Lorem ipsum...',
+  institutionId: 'dmsfisd-sdfjsdis-df',
+  digitalAddress: 'comune@test.it',
+}
+
+describe('Check PlatformRole', () => {
+  it('Is admin', () => {
+    expect(isAdmin({ ...exampleParty, role: 'Manager', platformRole: 'admin' })).toBeTruthy()
+    expect(isAdmin({ ...exampleParty, role: 'Delegate', platformRole: 'admin' })).toBeTruthy()
+    expect(isAdmin(null)).toBeFalsy()
+  })
+
+  it('Is operator API', () => {
+    expect(isOperatorAPI({ ...exampleParty, role: 'Operator', platformRole: 'api' })).toBeTruthy()
+    expect(isOperatorAPI(null)).toBeFalsy()
+  })
+
+  it('Is operator security', () => {
+    expect(
+      isOperatorSecurity({ ...exampleParty, role: 'Operator', platformRole: 'security' })
+    ).toBeTruthy()
+    expect(isOperatorSecurity(null)).toBeFalsy()
+  })
+})
