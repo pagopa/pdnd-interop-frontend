1a43ad250bdf7763ce63556da52bfc9e39da2643
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2022-01-17 12:04:30 +0100

    chore: moved test token to env variables

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -6,16 +6,12 @@ import { jwtToUser, parseJwt } from '../lib/jwt-utils'
 import { fetchWithLogs } from '../lib/api-utils'
 import { isFetchError } from '../lib/error-utils'
 
-const testToken =
-  'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImYyNmUwM2RiLWY4YjMtNDk5ZS04ZjMyLTA3MmM5M2VjZjc2MSJ9.eyJlbWFpbCI6ImNhcmxhLnJvc3NpQHRlc3QucGFnb3BhLml0IiwiZmFtaWx5X25hbWUiOiJSb3NzaSIsImZpc2NhbF9udW1iZXIiOiJJU1BYTkIzMlI4Mlk3NjZEIiwibW9iaWxlX3Bob25lIjoiMzMzMzMzMzMzIiwibmFtZSI6IkNhcmxhIiwiZnJvbV9hYSI6ZmFsc2UsInVpZCI6ImZiMjgwNGQwLTllN2QtNGIwNC05MjVhLWNhNmY4MDJhNzI4MiIsImxldmVsIjoiTDIiLCJpYXQiOjE2NDI0MTY4NTIsImV4cCI6MTY0MjQyMDQ1MiwiaXNzIjoiU1BJRCIsImp0aSI6IjAxRlNLVFJLRlk2NFFHUUFOMzRNV0VCNUE3In0.Opo5aDTQmJrtK0lMhqEMSpY6UZ7Otgf-lZkO4ke-rd70AzygaIBub6ECDeA5jloV9cm3TDeri86hMpb-owzcgMHVAhkDivawzvZbR_W4lC32YjsBIEPl2L5c5hOBx7BOJGHrSKHLcrOX9Lc1a3fHNBAPEUMYEwm99z7SAEDB6ixPjboE0JfvU8i0U4qBexhSKVpenEUMYOWu7oBpfA0pv_q-St_-p0PTT7cTuYJ19IkOK-_7x48NmZ_i2P5pGs-4rFe4F7_SNfJOjRJlm2-Ducb-lBk7gFc6jqcQdRb1_CILp7A8ebEqTlpcng3A-GSmlJQS4L5wB3nMfj97DYLuvQ'
-// 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImYyNmUwM2RiLWY4YjMtNDk5ZS04ZjMyLTA3MmM5M2VjZjc2MSJ9.eyJlbWFpbCI6ImNhcmxhLnJvc3NpQHRlc3QucGFnb3BhLml0IiwiZmFtaWx5X25hbWUiOiJSb3NzaSIsImZpc2NhbF9udW1iZXIiOiJJU1BYTkIzMlI4Mlk3NjZEIiwibW9iaWxlX3Bob25lIjoiMzMzMzMzMzMzIiwibmFtZSI6IkNhcmxhIiwiZnJvbV9hYSI6ZmFsc2UsInVpZCI6ImZiMjgwNGQwLTllN2QtNGIwNC05MjVhLWNhNmY4MDJhNzI4MiIsImxldmVsIjoiTDIiLCJpYXQiOjE2NDIwODUxMTAsImV4cCI6MTY0MjA4ODcxMCwiaXNzIjoiU1BJRCIsImp0aSI6IjAxRlM5WUNNWlhKUkczNENGMkQ4QUc4VktFIn0.a2xrKtsl44B9YvAAr5UnQcP0bz3pZCz0mv1eYltOPmMdN3wyjAtk1V6wbuMvvr0Fgg5BOGDAEci7hF0AhxQ-dZ9-BRcLwt0xqWqpv1aUo-C2NEU_q9xZgRUeq4nG1Ok-lHI8_FuBPR8IogbmZvDlMWjesofSuU4DnxyHGILiPE6fWrQW-Y8QZ5-ZRykeCPPm1Ezj4SyWvQ5rWYK1oms6PJIFQDHZMozTed-6J7Oh5ekor9uUIAj658DPK3oCHCgQ-alSn2YNekHVgn2rUHP_WoBs8xCTmIlGx3I63-kVsiQk7Y-WDMDAnmQty_bkNWeOPqUN9DwNTXhD814dMwax4g'
-
 export const useLogin = () => {
   const { setUser } = useContext(UserContext)
 
   const silentLoginAttempt = async (): Promise<boolean> => {
     if (USE_MOCK_SPID_USER) {
-      storageWrite(STORAGE_KEY_TOKEN, testToken, 'string')
+      storageWrite(STORAGE_KEY_TOKEN, process.env.REACT_APP_MOCK_TOKEN as string, 'string')
     }
 
     // Try to get the token from the sessionStorage
