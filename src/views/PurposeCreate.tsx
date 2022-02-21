b33f392b425f34b7fcee696da2ca02fe63eeb11c
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2022-02-21 11:40:50 +0100

    feat: avoid editing of purpose if not in draft

--- src/views/PurposeCreate.tsx
+++ src/views/PurposeCreate.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { StepperStep } from '../../types'
+import React, { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import { Purpose, StepperStep } from '../../types'
 import { PurposeCreateStep1General } from '../components/PurposeCreateStep1General'
 import { PurposeCreateStep2RiskAnalysis } from '../components/PurposeCreateStep2RiskAnalysis'
 import { PurposeCreateStep3Clients } from '../components/PurposeCreateStep3Clients'
 import { Contained } from '../components/Shared/Contained'
 import { StyledIntro, StyledIntroChildrenProps } from '../components/Shared/StyledIntro'
 import { StyledStepper } from '../components/Shared/StyledStepper'
+import { ROUTES } from '../config/routes'
 import { useActiveStep } from '../hooks/useActiveStep'
+import { useAsyncFetch } from '../hooks/useAsyncFetch'
+import { buildDynamicPath, getBits } from '../lib/router-utils'
 
 const STEPS: Array<StepperStep & { intro: StyledIntroChildrenProps }> = [
   {
@@ -34,6 +38,21 @@ export const PurposeCreate = () => {
   const { activeStep, forward, back } = useActiveStep()
   const { component: Step, intro } = STEPS[activeStep]
   const stepProps = { forward, back }
+  const history = useHistory()
+  const bits = getBits(history.location)
+  const purposeId = bits[bits.length - 1]
+
+  const { data } = useAsyncFetch<Purpose>(
+    { path: { endpoint: 'PURPOSE_GET_SINGLE' }, config: { params: { purposeId } } },
+    { loadingTextLabel: 'Stiamo caricando le informazioni della finalità' }
+  )
+
+  useEffect(() => {
+    // If this purpose is not in draft, you cannot edit it
+    if (data && data.versions.length === 1 && data.versions[0].state !== 'DRAFT') {
+      history.replace(buildDynamicPath(ROUTES.SUBSCRIBE_PURPOSE_VIEW.PATH, { purposeId }))
+    }
+  }, [data]) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <React.Fragment>
