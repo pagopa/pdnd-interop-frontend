1f9d74648c200496c7b0486f45e6c01ccda75be2
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2022-03-07 12:58:34 +0100

    fix: fixed path for help file

--- scripts/help/markdown-to-html.js
+++ scripts/help/markdown-to-html.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const { marked } = require('marked')
 
-const OUTPUT_URL = 'https://uat.gateway.test.pdnd-interop.pagopa.it/ui/it/aiuto'
+const OUTPUT_URL = 'https://fe-test.gateway.test.pdnd-interop.pagopa.it/ui'
 
 const MARKDOWN_LOCATION = 'data/help.md'
 const DEST_LOCATION = '../../public/data/help.json'
@@ -11,7 +11,9 @@ try {
   const markdownPath = path.join(__dirname, MARKDOWN_LOCATION)
   const markdownData = fs.readFileSync(markdownPath, 'utf8')
   const html = marked.parse(markdownData)
-  const tempReplaced = html.replace(/href=\"\#/g, `href="${OUTPUT_URL}#`)
+  const tempReplaced = html
+    .replace(/href=\"\#/g, `href="${OUTPUT_URL}/it/aiuto#`)
+    .replace(/src=\"/g, `src="${OUTPUT_URL}`)
   fs.writeFileSync(path.join(__dirname, DEST_LOCATION), JSON.stringify({ html: tempReplaced }), {
     flag: 'w',
   })
