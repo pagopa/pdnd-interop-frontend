8452b45d1eb4049cc73da18380eec36d8a0fa7de
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2022-03-17 12:00:13 +0100

    Improved typing for table row

--- src/components/Shared/StyledTableRow.tsx
+++ src/components/Shared/StyledTableRow.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent, ReactElement } from 'react'
 import { Chip, TableCell, TableRow, Typography } from '@mui/material'
-import has from 'lodash/has'
 import { MUIColor } from '../../../types'
 
 type LabelCell = {
@@ -27,16 +26,16 @@ export const StyledTableRow: FunctionComponent<StyledTableRowProps> = ({ cellDat
 
         return (
           <TableCell key={i} sx={{ py: 2 }}>
-            {has(cell, 'chipLabel') ? (
-              <Chip label={(cell as ChipCell).chipLabel} color={(cell as ChipCell).color} />
+            {'chipLabel' in cell ? (
+              <Chip label={cell.chipLabel} color={cell.color} />
             ) : (
               <Typography
                 component="span"
                 sx={{ fontWeight: firstCell ? 600 : 300, display: 'inline-block' }}
                 variant={firstCell ? 'body2' : 'caption'}
               >
-                {(cell as LabelCell).label}
-                {(cell as LabelCell).tooltip}
+                {cell.label}
+                {cell.tooltip}
               </Typography>
             )}
           </TableCell>
