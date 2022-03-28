1bb7db3e2e5d550ae73ce0b65b9548e1683eeea6
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2022-03-28 17:29:02 +0200

    Improved client activity feedback error message

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,13 +1,23 @@
 import { ClientPurpose } from '../../types'
 
 export function getComputedClientAssertionState(clientPurpose: ClientPurpose) {
-  const agreementState = clientPurpose.states.agreement.state
-  const eserviceState = clientPurpose.states.eservice.state
-  const purposeState = clientPurpose.states.purpose.state
+  const agreementState = { type: 'agreement', state: clientPurpose.states.agreement.state }
+  const eserviceState = { type: 'eservice', state: clientPurpose.states.eservice.state }
+  const purposeState = { type: 'purpose', state: clientPurpose.states.purpose.state }
 
-  if ([agreementState, eserviceState, purposeState].every((s) => s === 'ACTIVE')) {
+  const problems = [agreementState, eserviceState, purposeState].filter(
+    (s) => s.state === 'INACTIVE'
+  )
+
+  if (problems.length === 0) {
     return 'Sì'
   }
 
-  return 'No'
+  const LABELS: Record<string, string> = {
+    agreement: "la richiesta di fruizione per l'E-Service a cui è associata questa finalità",
+    eservice: "l'E-Service a cui è associata questa finalità",
+    purpose: 'questa finalità',
+  }
+
+  return `No, perché attualmente sono inattivi: ${problems.map((p) => LABELS[p.type]).join(', ')}`
 }
