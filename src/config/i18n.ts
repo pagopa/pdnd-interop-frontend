ad2043caaf1f7f5b09ab7595ea432163b2d97e6b
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2022-05-17 12:23:58 +0200

    Fixed first load if lang is not italian

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,13 +1,13 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import HttpApi from 'i18next-http-backend'
-import { isDevelopment, PUBLIC_URL } from '../lib/constants'
+import { PUBLIC_URL } from '../lib/constants'
 
 i18n
   .use(initReactI18next)
   .use(HttpApi)
   .init({
-    debug: isDevelopment,
+    debug: false,
     fallbackLng: 'it',
     backend: {
       // DANGER DANGER DANGER. The path needs to be prepended with the PUBLIC_URL
