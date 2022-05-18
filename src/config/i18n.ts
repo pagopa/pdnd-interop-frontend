8714d92a44cc4d8d6ad524d8911888653bd4f6db
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2022-05-18 10:04:34 +0200

    Added function to rebuild config if initial lang is not the default one

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,14 +1,14 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import HttpApi from 'i18next-http-backend'
-import { PUBLIC_URL } from '../lib/constants'
+import { DEFAULT_LANG, PUBLIC_URL } from '../lib/constants'
 
 i18n
   .use(initReactI18next)
   .use(HttpApi)
   .init({
     debug: false,
-    fallbackLng: 'it',
+    fallbackLng: DEFAULT_LANG,
     backend: {
       // DANGER DANGER DANGER. The path needs to be prepended with the PUBLIC_URL
       // See: https://stackoverflow.com/a/65396448
