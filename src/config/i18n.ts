e0428ade22e634bb4e10e025cfe193503927d34c
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2022-05-19 12:47:19 +0200

    Added translations for lib functions

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,13 +1,17 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import HttpApi from 'i18next-http-backend'
-import { DEFAULT_LANG, PUBLIC_URL } from '../lib/constants'
+import { DEFAULT_LANG, LANGUAGES, PUBLIC_URL } from '../lib/constants'
 
 i18n
   .use(initReactI18next)
   .use(HttpApi)
   .init({
     debug: false,
+    // Preload is needed to fetch translation in a different language from the current one
+    // See https://github.com/i18next/i18next/issues/959#issuecomment-325196655
+    // In code, see isProviderOrSubscriber in lib/router-utils.ts
+    preload: Object.keys(LANGUAGES),
     fallbackLng: DEFAULT_LANG,
     backend: {
       // DANGER DANGER DANGER. The path needs to be prepended with the PUBLIC_URL
