2ea9e4784174e9431de1948a22888cd758531cbd
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2022-06-29 15:12:04 +0200

    Added random params to check for possible bugs

--- src/lib/emotion-cache.ts
+++ src/lib/emotion-cache.ts
@@ -4,4 +4,8 @@ type ExtendedWindow = Window & { nonce: string }
 export const noncedCache = createCache({
   key: 'emotion-cache',
   nonce: (window as unknown as ExtendedWindow).nonce,
+  prepend: false,
+  container: document.body,
 })
+
+console.log('created nonced cache with nonce: ', (window as unknown as ExtendedWindow).nonce)
