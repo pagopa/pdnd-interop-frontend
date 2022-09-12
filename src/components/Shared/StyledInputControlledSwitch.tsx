1214dc6787df571bd1afce334f1abcf653fa4a74
Carmine Porricelli <carmine.porricelli@pagopa.it>
2022-09-12 18:26:07 +0200

    Added StyledInputControlledSwitch

---
+++ src/components/Shared/StyledInputControlledSwitch.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { FormLabel, Switch, SxProps } from '@mui/material'
+import { StyledInputWrapper } from './StyledInputWrapper'
+
+export type StyledInputControlledSwitchProps = {
+  label?: string
+  infoLabel?: string | JSX.Element
+  error?: string
+  value: boolean
+  name: string
+
+  onChange: (e: React.SyntheticEvent) => void
+
+  sx?: SxProps
+
+  type?: 'switch'
+}
+
+export function StyledInputControlledSwitch({
+  label,
+  infoLabel,
+
+  name,
+  value,
+  error,
+
+  sx,
+
+  onChange,
+}: StyledInputControlledSwitchProps) {
+  return (
+    <StyledInputWrapper name={name} error={error} sx={sx} infoLabel={infoLabel}>
+      {label && (
+        <FormLabel component="legend" sx={{ color: 'text.secondary' }}>
+          {label}
+        </FormLabel>
+      )}
+      <Switch checked={value} id={name} name={name} onChange={onChange} />
+    </StyledInputWrapper>
+  )
+}
