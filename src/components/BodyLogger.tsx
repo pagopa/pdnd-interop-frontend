4f60115884a4b9154b85ab7d02c32b73d64ae6a9
Carmine Porricelli <carmine.porricelli@pagopa.it>
2022-09-16 13:03:14 +0200

    Replaced useHistory with useLocation

--- src/components/BodyLogger.tsx
+++ src/components/BodyLogger.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, useContext, useEffect, useState } from 'react'
 import { DialogProps, ToastContentWithOutcome, ToastProps } from '../../types'
-import { useHistory } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import isEmpty from 'lodash/isEmpty'
 import {
   DialogContext,
@@ -81,7 +81,7 @@ export const WhitePanel: FunctionComponent = ({ children }) => {
 
 export function BodyLogger() {
   const { doesRouteAllowTwoColumnsLayout } = useRoute()
-  const history = useHistory()
+  const location = useLocation()
   const [toast, setToast] = useState<ToastProps | null>(null)
   const [dialog, setDialog] = useState<DialogProps | null>(null)
   const [loadingText, setLoadingText] = useState<string | null>(null)
@@ -96,7 +96,8 @@ export function BodyLogger() {
       setToast(null)
     }
 
-    const locationState: Record<string, unknown> = history.location.state as Record<string, unknown>
+    const locationState: Record<string, unknown> = location.state as Record<string, unknown>
+
     // If there is explicitly a new toast to show on this page, display it
     if (!isEmpty(locationState) && !isEmpty(locationState.toast)) {
       const toastContent = locationState.toast as ToastContentWithOutcome
@@ -107,14 +108,14 @@ export function BodyLogger() {
         },
       })
     }
-  }, [history.location]) // eslint-disable-line react-hooks/exhaustive-deps
+  }, [location]) // eslint-disable-line react-hooks/exhaustive-deps
 
   /*
    * Handle data logging (now console.log, in the future might be Analytics)
    */
   useEffect(() => {
-    logAction('Route change', history.location)
-  }, [history.location])
+    logAction('Route change', location)
+  }, [location])
 
   return (
     <TableActionMenuContext.Provider value={{ tableActionMenu, setTableActionMenu }}>
@@ -124,7 +125,7 @@ export function BodyLogger() {
             <RebuildI18N />
             <HeaderWrapper />
 
-            {doesRouteAllowTwoColumnsLayout(history.location) ? (
+            {doesRouteAllowTwoColumnsLayout(location) ? (
               <Box sx={{ flexGrow: 1 }}>
                 <Stack direction="row" sx={{ height: '100%', overflowX: 'hidden' }}>
                   <MainNav />
