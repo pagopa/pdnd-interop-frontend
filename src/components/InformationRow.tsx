d61f575ef9658b5e047d42737ff4e91e6466a175
Carmine Porricelli <carmine.porricelli@pagopa.it>
2022-09-29 11:56:22 +0200

    Added InformationRow component

---
+++ src/components/InformationRow.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Box, Typography, Stack, Divider } from '@mui/material'
+
+interface InformationRowProps {
+  label: string
+  labelDescription?: string
+  children: React.ReactNode
+  rightContent?: React.ReactNode
+}
+
+export function InformationRow({
+  label,
+  labelDescription,
+  rightContent,
+  children,
+}: InformationRowProps) {
+  return (
+    <Stack spacing={4} direction="row">
+      <Box sx={{ flexShrink: 0, maxWidth: '200px', flex: 1 }}>
+        <Typography variant="body2">{label}</Typography>
+        {labelDescription && (
+          <>
+            <Divider sx={{ my: 0.2 }} />
+            <Typography sx={{ fontsize: '0.625rem' }} color="text.secondary" variant="body2">
+              {labelDescription}
+            </Typography>
+          </>
+        )}
+      </Box>
+      <Box sx={{ flex: 1 }}>
+        <Typography component="div" variant="body2" fontWeight={600}>
+          {children}
+        </Typography>
+      </Box>
+      {rightContent && <Stack justifyContent="start">{rightContent}</Stack>}
+    </Stack>
+  )
+}
