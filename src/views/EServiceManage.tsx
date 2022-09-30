54f01d5e2ed5d277106ae7ee3b10d592b514709e
Carmine Porricelli <carmine.porricelli@pagopa.it>
2022-09-30 13:01:55 +0200

    Added actions and menu action from the eservice on top of the page

--- src/views/EServiceManage.tsx
+++ src/views/EServiceManage.tsx
@@ -1,9 +1,12 @@
 import React, { FormEvent, useMemo, useState } from 'react'
 import { StyledIntro } from '../components/Shared/StyledIntro'
 import {
+  ActionProps,
+  ApiEndpointKey,
   EServiceDescriptorRead,
   EServiceDocumentRead,
   EServiceReadType,
+  EServiceState,
   FrontendAttributes,
 } from '../../types'
 import { RunActionOutput, useFeedback } from '../hooks/useFeedback'
@@ -22,7 +25,7 @@ import { AttributeSection } from '../components/AttributeSection'
 import StyledSection from '../components/Shared/StyledSection'
 import { Box, Chip, Divider, Grid, Stack, Typography } from '@mui/material'
 import { StyledLink } from '../components/Shared/StyledLink'
-import { formatThousands, secondsToHoursMinutes } from '../lib/format-utils'
+import { formatThousands, minutesToSeconds, secondsToHoursMinutes } from '../lib/format-utils'
 import { downloadFile } from '../lib/file-utils'
 import { AxiosResponse } from 'axios'
 import { ButtonNaked } from '@pagopa/mui-italia'
@@ -37,9 +40,10 @@ import {
 } from '../lib/constants'
 import { PageBottomActions } from '../components/Shared/PageBottomActions'
 import { useRoute } from '../hooks/useRoute'
-import { buildDynamicRoute } from '../lib/router-utils'
+import { buildDynamicPath, buildDynamicRoute } from '../lib/router-utils'
 import { StyledInputControlledSelect } from '../components/Shared/StyledInputControlledSelect'
 import { WELL_KNOWN_URL } from '../lib/env'
+import { ActionMenu } from '../components/Shared/ActionMenu'
 
 export function EServiceManage() {
   const { eserviceId, descriptorId } = useParams<{
@@ -49,6 +53,7 @@ export function EServiceManage() {
   const { t } = useTranslation('eservice')
   const history = useHistory()
   const { routes } = useRoute()
+  const { runAction } = useFeedback()
 
   const {
     data: eserviceData,
@@ -62,6 +67,154 @@ export function EServiceManage() {
       disabled: !eserviceId || !descriptorId,
     }
   )
+
+  /*
+   * List of possible actions for the user to perform
+   */
+  const wrapPublishDraft = (eserviceId: string, descriptorId?: string) => async () => {
+    await runAction(
+      {
+        path: {
+          endpoint: 'ESERVICE_VERSION_DRAFT_PUBLISH',
+          endpointParams: { eserviceId, descriptorId },
+        },
+      },
+      { showConfirmDialog: true }
+    )
+  }
+
+  const wrapDeleteDraft = (eserviceId: string, descriptorId?: string) => async () => {
+    let endpoint: ApiEndpointKey = 'ESERVICE_DRAFT_DELETE'
+    const endpointParams: Record<string, string> = { eserviceId }
+
+    if (descriptorId) {
+      endpoint = 'ESERVICE_VERSION_DRAFT_DELETE'
+      endpointParams.descriptorId = descriptorId
+    }
+
+    await runAction({ path: { endpoint, endpointParams } }, { showConfirmDialog: true })
+  }
+
+  const wrapSuspend = (eserviceId: string, descriptorId?: string) => async () => {
+    await runAction(
+      {
+        path: {
+          endpoint: 'ESERVICE_VERSION_SUSPEND',
+          endpointParams: { eserviceId, descriptorId },
+        },
+      },
+      { showConfirmDialog: true }
+    )
+  }
+
+  const wrapReactivate = (eserviceId: string, descriptorId?: string) => async () => {
+    await runAction(
+      {
+        path: {
+          endpoint: 'ESERVICE_VERSION_REACTIVATE',
+          endpointParams: { eserviceId, descriptorId },
+        },
+      },
+      { showConfirmDialog: true }
+    )
+  }
+
+  // const archive = () => {
+  //   //
+  // }
+
+  // Clones the properties and generates a new service
+  const wrapClone = (eserviceId: string, descriptorId?: string) => async () => {
+    await runAction(
+      {
+        path: {
+          endpoint: 'ESERVICE_CLONE_FROM_VERSION',
+          endpointParams: { eserviceId, descriptorId },
+        },
+      },
+      { showConfirmDialog: true }
+    )
+  }
+
+  // Clones all the properties of the previous version and generates a new draft version
+  const wrapCreateNewVersionDraft = (eserviceId: string) => async () => {
+    const { outcome, response } = (await runAction(
+      {
+        path: { endpoint: 'ESERVICE_VERSION_DRAFT_CREATE', endpointParams: { eserviceId } },
+        config: {
+          data: {
+            voucherLifespan: minutesToSeconds(1),
+            audience: [],
+            description: '',
+            dailyCallsPerConsumer: 1,
+            dailyCallsTotal: 1,
+          },
+        },
+      },
+      { showConfirmDialog: true }
+    )) as RunActionOutput
+
+    if (outcome === 'success') {
+      const successResponse = response as AxiosResponse<EServiceDescriptorRead>
+      const descriptorId = successResponse.data.id
+      history.push(
+        buildDynamicPath(routes.PROVIDE_ESERVICE_EDIT.PATH, {
+          eserviceId,
+          descriptorId,
+        }),
+        { stepIndexDestination: 1 }
+      )
+    }
+  }
+  /*
+   * End list of actions
+   */
+
+  type EServiceAction = Record<EServiceState, Array<ActionProps | null>>
+  // Build list of available actions for each service in its current state
+  const getAvailableActions = () => {
+    if (!eserviceData?.activeDescriptor || !eserviceId || !descriptorId) return []
+
+    const { state } = eserviceData.activeDescriptor
+
+    const suspendAction = {
+      onClick: wrapSuspend(eserviceId, descriptorId),
+      label: t('actions.suspend', { ns: 'common' }),
+    }
+    const reactivateAction = {
+      onClick: wrapReactivate(eserviceId, descriptorId),
+      label: t('actions.activate', { ns: 'common' }),
+    }
+    const cloneAction = {
+      onClick: wrapClone(eserviceId, descriptorId),
+      label: t('actions.clone', { ns: 'common' }),
+    }
+    const createVersionDraftAction = {
+      onClick: wrapCreateNewVersionDraft(eserviceId),
+      label: t('actions.createNewDraft', { ns: 'common' }),
+    }
+    // TEMP PIN-645
+    // const archiveAction = { onClick: archive, label: 'Archivia' }
+    const publishDraftAction = {
+      onClick: wrapPublishDraft(eserviceId, descriptorId),
+      label: t('actions.publish', { ns: 'common' }),
+    }
+    const deleteDraftAction = {
+      onClick: wrapDeleteDraft(eserviceId, descriptorId),
+      label: t('actions.delete', { ns: 'common' }),
+    }
+
+    const availableActions: EServiceAction = {
+      PUBLISHED: [suspendAction, cloneAction, createVersionDraftAction],
+      ARCHIVED: [],
+      DEPRECATED: [suspendAction /*, archiveAction */],
+      DRAFT: [descriptorId ? publishDraftAction : null, deleteDraftAction],
+      SUSPENDED: [reactivateAction, cloneAction, createVersionDraftAction],
+    }
+
+    // Return all the actions available for this particular status
+    return availableActions[state || 'DRAFT'].filter((a) => a !== null) as Array<ActionProps>
+  }
 
   const handleGoBackToEServices = () => {
     history.push(routes.PROVIDE.PATH)
@@ -79,11 +232,28 @@ export function EServiceManage() {
   const activeDescriptor = getLatestActiveVersion(eserviceData)
   const isCurrentVersion = activeDescriptor?.id === descriptorId
 
+  const availableActions = getAvailableActions()
+  let primaryAction: ActionProps | undefined
+
+  if (availableActions.length > 0) {
+    primaryAction = availableActions.shift()
+  }
+
   return (
     <Box sx={{ maxWidth: MAX_WIDTH }}>
-      <StyledIntro isLoading={isLoading}>
-        {{ title: eserviceData?.name, description: eserviceData?.description }}
-      </StyledIntro>
+      <Stack direction="row" spacing={2}>
+        <StyledIntro sx={{ flex: 1 }} isLoading={isLoading}>
+          {{ title: eserviceData?.name, description: eserviceData?.description }}
+        </StyledIntro>
+        <Stack direction="row" alignItems="center" spacing={2}>
+          {primaryAction && (
+            <StyledButton variant="outlined" onClick={primaryAction.onClick}>
+              {primaryAction.label}
+            </StyledButton>
+          )}
+          {availableActions.length > 0 && <ActionMenu actions={availableActions} />}
+        </Stack>
+      </Stack>
 
       {eserviceData && (
         <>
