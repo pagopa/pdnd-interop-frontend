cb92e500e6055511ff1f44ddd6112977e41ca85d
Carmine Porricelli <carmine.porricelli@pagopa.it>
2022-10-10 16:38:41 +0200

    Added "vertical" style prop to StyledInputControlledSwitch

--- src/components/Shared/StyledInputControlledSwitch.tsx
+++ src/components/Shared/StyledInputControlledSwitch.tsx
@@ -1,14 +1,15 @@
 import React, { ChangeEventHandler } from 'react'
-import { Box, FormLabel, Switch, SxProps } from '@mui/material'
+import { Box, FormLabel, Switch, SxProps, Typography } from '@mui/material'
 import { StyledInputWrapper } from './StyledInputWrapper'
 
 export type StyledInputControlledSwitchProps = {
   label: string
   infoLabel?: string | JSX.Element
-  error?: string
-  value: boolean
   name: string
+  value: boolean
+  error?: string
 
+  vertical?: boolean
   onChange: ChangeEventHandler
 
   sx?: SxProps
@@ -19,19 +20,29 @@ export type StyledInputControlledSwitchProps = {
 export function StyledInputControlledSwitch({
   label,
   infoLabel,
-
   name,
   value,
   error,
 
+  vertical = false,
   sx,
 
   onChange,
 }: StyledInputControlledSwitchProps) {
+  let formLabelSxProps: SxProps = {}
+  let typographyLabelSxProps: SxProps = {}
+
+  if (vertical) {
+    formLabelSxProps = { display: 'flex', alignItems: 'center' }
+    typographyLabelSxProps = { order: 2, ml: 2 }
+  }
+
   return (
     <StyledInputWrapper name={name} error={error} sx={sx} infoLabel={infoLabel}>
-      <FormLabel sx={{ color: 'text.primary' }}>
-        {label}
+      <FormLabel sx={{ color: 'text.primary', ...formLabelSxProps }}>
+        <Typography sx={typographyLabelSxProps} component="span" variant="body1">
+          {label}
+        </Typography>
         <Box>
           <Switch checked={value} id={name} name={name} onChange={onChange} />
         </Box>
