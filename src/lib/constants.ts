c71d2a78b31c7246030ae80a6c6397f09c99f013
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2022-10-18 10:51:04 +0200

    Updated logic to keep dev tokens

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,11 @@
 import { AgreementState, EServiceState, LangCode, MUIColor, UserState } from '../../types'
 import { isDevelopment } from './env'
+import { DEV_MOCK_TOKEN } from '../config/dev-tokens'
 
 export const DISPLAY_LOGS = false // isDevelopment
+export const MOCK_TOKEN = isDevelopment && DEV_MOCK_TOKEN
 
 export const STORAGE_KEY_SESSION_TOKEN = 'token'
-
-export const MOCK_TOKEN = isDevelopment && process.env.REACT_APP_MOCK_TOKEN
 
 export const MAX_WIDTH = 1280
 
