2d34c5a5bb9729c97c0514f234057417bb5690dd
Carmine Porricelli <carmine.porricelli@pagopa.it>
2022-11-24 18:56:46 +0100

    Added pagination to e-service Catalog

--- src/pages/ConsumerEServiceCatalogPage/ConsumerEServiceCatalog.page.tsx
+++ src/pages/ConsumerEServiceCatalogPage/ConsumerEServiceCatalog.page.tsx
@@ -1,16 +1,29 @@
+import React from 'react'
 import { PageContainer } from '@/components/layout/containers'
-import React, { Suspense } from 'react'
+import usePagination from '@/hooks/usePagination'
 import { useTranslation } from 'react-i18next'
 import { EServiceCatalogGrid, EServiceCatalogGridSkeleton } from './components'
+import { EServiceQueries } from '@/api/eservice'
+import { Pagination } from '@/components/shared/Pagination'
 
 const ConsumerEServiceCatalogPage: React.FC = () => {
   const { t } = useTranslation('pages', { keyPrefix: 'consumerEServiceCatalog' })
+  const { props, params, getTotalPageCount } = usePagination({
+    limit: 15,
+  })
 
+  const { data, isFetching } = EServiceQueries.useGetCatalogList(
+    {
+      states: ['PUBLISHED'],
+      ...params,
+    },
+    { suspense: false, keepPreviousData: true }
+  )
   return (
     <PageContainer title={t('title')} description={t('description')}>
-      <Suspense fallback={<EServiceCatalogGridSkeleton />}>
-        <EServiceCatalogGrid />
-      </Suspense>
+      {isFetching && <EServiceCatalogGridSkeleton />}
+      {!isFetching && <EServiceCatalogGrid eservices={data?.results} />}
+      <Pagination {...props} totalPages={getTotalPageCount(data?.pagination.totalCount)} />
     </PageContainer>
   )
 }
