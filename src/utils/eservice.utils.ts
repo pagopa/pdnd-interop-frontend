b7357b52d915d1d6ceaa0195b913070eca552dc6
Carmine Porricelli <carmine.porricelli@pagopa.it>
2022-11-24 12:35:18 +0100

    Removed unused function

--- src/utils/eservice.utils.ts
+++ src/utils/eservice.utils.ts
@@ -1,20 +1,4 @@
 import { DocumentRead } from '@/types/common.types'
-import { EServiceReadType } from '@/types/eservice.types'
-
-export function getLatestActiveDescriptor(eserviceData: EServiceReadType | undefined) {
-  if (!eserviceData) return undefined
-  const sortedByVersionDescriptors = eserviceData.descriptors.sort(
-    (a, b) => parseInt(b.version.replace('v', '')) - parseInt(a.version.replace('v', ''))
-  )
-
-  if (sortedByVersionDescriptors[0].state === 'DRAFT') {
-    sortedByVersionDescriptors.shift()
-  }
-
-  return sortedByVersionDescriptors[0]?.state !== 'ARCHIVED'
-    ? sortedByVersionDescriptors[0]
-    : undefined
-}
 
 export function getDownloadDocumentName(document: DocumentRead) {
   const filename: string = document.name
