78fa94ebcb7c4e0e0ae59a4de9db0921caeaeb09
Carmine Porricelli <carmine.porricelli@pagopa.it>
2022-12-06 16:42:13 +0100

    Added EServiceDescriptorCatalog mock factory function

--- src/__mocks__/data/eservice.mocks.ts
+++ src/__mocks__/data/eservice.mocks.ts
@@ -1,4 +1,9 @@
-import { EServiceCatalog, EServiceProvider, EServiceRead } from '../../types/eservice.types'
+import {
+  EServiceCatalog,
+  EServiceDescriptorCatalog,
+  EServiceProvider,
+  EServiceRead,
+} from '../../types/eservice.types'
 import { createMockFactory } from '../mock.utils'
 
 const createMockEServiceRead = createMockFactory<EServiceRead>({
@@ -35,4 +40,48 @@ const createMockEServiceCatalog = createMockFactory<EServiceCatalog>({
   },
 })
 
-export { createMockEServiceProvider, createMockEServiceCatalog, createMockEServiceRead }
+const createMockEServiceDescriptorCatalog = createMockFactory<EServiceDescriptorCatalog>({
+  agreementApprovalPolicy: 'MANUAL',
+  audience: ['Lorem'],
+  dailyCallsPerConsumer: 1,
+  dailyCallsTotal: 1,
+  description: 'Lorem',
+  docs: [],
+  eservice: {
+    activeDescriptor: {
+      id: 'ec94e366-cbb2-4203-ac07-95acf5289a31',
+      state: 'PUBLISHED',
+      version: '1',
+    },
+    attributes: {
+      certified: [],
+      declared: [],
+      verified: [],
+    },
+    description: 'lorem',
+    descriptors: [{ id: 'ec94e366-cbb2-4203-ac07-95acf5289a31', state: 'PUBLISHED', version: '1' }],
+    hasCertifiedAttributes: false,
+    id: '03d0c725-47e5-4ec5-8ecd-1a1f3ce45d29',
+    isMine: false,
+    isSubscribed: false,
+    name: '!! -- CAMMELLO -- Test 18/10 [1] - clone',
+    technology: 'REST',
+  },
+  id: 'ec94e366-cbb2-4203-ac07-95acf5289a31',
+  interface: {
+    contentType: 'application/octet-stream',
+    id: '4cd1ef19-77b7-468f-8693-c6cebac12912',
+    name: 'example_open_api.yml',
+    prettyName: 'Specifica API',
+  },
+  state: 'PUBLISHED',
+  version: '1',
+  voucherLifespan: 60,
+})
+
+export {
+  createMockEServiceProvider,
+  createMockEServiceCatalog,
+  createMockEServiceRead,
+  createMockEServiceDescriptorCatalog,
+}
