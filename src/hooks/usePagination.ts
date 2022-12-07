2820e894e82de2cd4797a7eb1444542394d6c1a9
Carmine Porricelli <carmine.porricelli@pagopa.it>
2022-12-07 12:35:14 +0100

    Added usePagination jsDocs

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,9 +1,19 @@
 import { useSearchParams } from 'react-router-dom'
 import omit from 'lodash/omit'
 
-function usePagination({ limit }: { limit: number }) {
+/**
+ * Contains all the logic needed for the pagination.
+ *
+ * @returns an object with three properties:
+ * - `props` - an object with props to be passed to the `Pagination` component.
+ * - `params` - an object with url param to be passed to the paginated service.
+ * - `getTotalPageCount` - an utility function that given the total records returns the total page count.
+ */
+function usePagination(options: { limit: number }) {
   const [searchParams, setSearchParams] = useSearchParams()
   const offset = parseInt(searchParams.get('offset') ?? '0')
+
+  const limit = options.limit
 
   const pageNum = Math.ceil(offset / limit) + 1
 
