b312e33823801870c3a487fff55d6a9f2895a08b
Carmine Porricelli <carmine.porricelli@pagopa.it>
2022-12-09 13:07:39 +0100

    Added useMutationWrapper unit tests

---
+++ src/api/react-query-wrappers/__tests__/useMutationWrapper.test.tsx
@@ -0,0 +1,320 @@
+import React from 'react'
+import {
+  DialogContextProvider,
+  LoadingOverlayContextProvider,
+  ToastNotificationContextProvider,
+} from '@/contexts'
+import axiosInstance from '@/config/axios'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClientMock } from '@/__mocks__/query-client.mock'
+import { fireEvent, renderHook, screen, waitForElementToBeRemoved } from '@testing-library/react'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { act } from 'react-dom/test-utils'
+import { useMutationWrapper } from '../useMutationWrapper'
+
+const server = setupServer(
+  rest.post('/test-success', (_, res, ctx) => {
+    return res(ctx.status(200))
+  }),
+  rest.post('/test-error', (_, res, ctx) => {
+    return res(ctx.status(500))
+  })
+)
+
+beforeAll(() => {
+  server.listen()
+})
+
+afterAll(() => {
+  server.close()
+})
+
+const mockMutationServices = {
+  success: async () => {
+    return await axiosInstance.post('/test-success')
+  },
+  error: async () => {
+    return await axiosInstance.post('/test-error')
+  },
+}
+
+const wrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <DialogContextProvider>
+      <QueryClientProvider client={queryClientMock}>
+        <LoadingOverlayContextProvider>
+          <ToastNotificationContextProvider>{children}</ToastNotificationContextProvider>
+        </LoadingOverlayContextProvider>
+      </QueryClientProvider>
+    </DialogContextProvider>
+  )
+}
+
+describe('useMutationWrapper tests', () => {
+  it('Should show and hide the loading overlay when the loadingLabel property option is set on mutate', async () => {
+    const { result } = renderHook(
+      () =>
+        useMutationWrapper(mockMutationServices.success, {
+          successToastLabel: 'success',
+          errorToastLabel: 'error',
+          loadingLabel: 'loading',
+        }),
+      { wrapper }
+    )
+
+    act(() => {
+      result.current.mutate()
+    })
+
+    screen.getByRole('progressbar', { hidden: true })
+
+    await waitForElementToBeRemoved(screen.getByRole('progressbar', { hidden: true }))
+  })
+
+  it('Should show and hide the loading overlay when the loadingLabel property option is set on mutateAsync', async () => {
+    const { result } = renderHook(
+      () =>
+        useMutationWrapper(mockMutationServices.success, {
+          successToastLabel: 'success',
+          errorToastLabel: 'error',
+          loadingLabel: 'loading',
+        }),
+      { wrapper }
+    )
+
+    act(() => {
+      result.current.mutateAsync()
+    })
+
+    screen.getByRole('progressbar', { hidden: true })
+
+    await waitForElementToBeRemoved(screen.getByRole('progressbar', { hidden: true }))
+  })
+
+  it('Should not show loading overlay if suppressLoadingOverlay is set to true', async () => {
+    const { result } = renderHook(
+      () =>
+        useMutationWrapper(mockMutationServices.success, {
+          successToastLabel: 'success',
+          errorToastLabel: 'error',
+          suppressLoadingOverlay: true,
+        }),
+      { wrapper }
+    )
+
+    act(() => {
+      result.current.mutate()
+    })
+
+    expect(screen.queryByRole('progressbar', { hidden: true })).not.toBeInTheDocument()
+  })
+
+  it('Should show the success label on mutation success', async () => {
+    const { result } = renderHook(
+      () =>
+        useMutationWrapper(mockMutationServices.success, {
+          successToastLabel: 'success',
+          errorToastLabel: 'error',
+          loadingLabel: 'loading',
+        }),
+      { wrapper }
+    )
+    act(() => {
+      result.current.mutate()
+    })
+    expect(await screen.findByRole('alert')).toBeInTheDocument()
+  })
+
+  it('Should not show the success label on mutation success when suppressSuccessToast is set to true', async () => {
+    const { result } = renderHook(
+      () =>
+        useMutationWrapper(mockMutationServices.success, {
+          suppressSuccessToast: true,
+          errorToastLabel: 'error',
+          loadingLabel: 'loading',
+        }),
+      { wrapper }
+    )
+    await act(() => {
+      result.current.mutate()
+    })
+    expect(screen.queryByRole('alert')).not.toBeInTheDocument()
+  })
+
+  it('Should show the error label on mutation error', async () => {
+    const { result } = renderHook(
+      () =>
+        useMutationWrapper(mockMutationServices.error, {
+          successToastLabel: 'success',
+          errorToastLabel: 'error',
+          loadingLabel: 'loading',
+        }),
+      { wrapper }
+    )
+    act(() => {
+      result.current.mutate()
+    })
+    expect(await screen.findByRole('alert')).toBeInTheDocument()
+  })
+
+  it('Should not show the error label on mutation error when suppressErrorToast is set to true', async () => {
+    const { result } = renderHook(
+      () =>
+        useMutationWrapper(mockMutationServices.error, {
+          successToastLabel: 'success',
+          suppressErrorToast: true,
+          loadingLabel: 'loading',
+        }),
+      { wrapper }
+    )
+    await act(() => {
+      result.current.mutate()
+    })
+    expect(screen.queryByRole('alert')).not.toBeInTheDocument()
+  })
+
+  it('Should show the confirmation modal on mutate', async () => {
+    const { result } = renderHook(
+      () =>
+        useMutationWrapper(mockMutationServices.success, {
+          successToastLabel: 'success',
+          errorToastLabel: 'error',
+          loadingLabel: 'loading',
+          showConfirmationDialog: true,
+          dialogConfig: {
+            title: 'title',
+            description: 'description',
+          },
+        }),
+      { wrapper }
+    )
+    await act(() => {
+      result.current.mutate()
+    })
+
+    expect(screen.getByRole('dialog', { name: 'title' })).toBeInTheDocument()
+  })
+
+  it('Should not show the confirmation modal on mutate', async () => {
+    const { result } = renderHook(
+      () =>
+        useMutationWrapper(mockMutationServices.success, {
+          successToastLabel: 'success',
+          errorToastLabel: 'error',
+          loadingLabel: 'loading',
+          showConfirmationDialog: false,
+          dialogConfig: {
+            title: 'title',
+            description: 'description',
+          },
+        }),
+      { wrapper }
+    )
+    await act(() => {
+      result.current.mutate()
+    })
+
+    expect(screen.queryByRole('dialog', { name: 'title' })).not.toBeInTheDocument()
+
+    // fireEvent.click(screen.getByRole('button', { name: 'confirm' }))
+
+    // expect(screen.getByRole('progressbar', { hidden: true })).toBeInTheDocument()
+  })
+
+  it('Should show the confirmation modal on mutateAsync', async () => {
+    const { result } = renderHook(
+      () =>
+        useMutationWrapper(mockMutationServices.success, {
+          successToastLabel: 'success',
+          errorToastLabel: 'error',
+          loadingLabel: 'loading',
+          showConfirmationDialog: true,
+          dialogConfig: {
+            title: 'title',
+            description: 'description',
+          },
+        }),
+      { wrapper }
+    )
+    await act(() => {
+      result.current.mutateAsync()
+    })
+
+    expect(screen.getByRole('dialog', { name: 'title' })).toBeInTheDocument()
+  })
+
+  it('Should not show the confirmation modal on mutateAsync', async () => {
+    const { result } = renderHook(
+      () =>
+        useMutationWrapper(mockMutationServices.success, {
+          successToastLabel: 'success',
+          errorToastLabel: 'error',
+          loadingLabel: 'loading',
+          showConfirmationDialog: false,
+          dialogConfig: {
+            title: 'title',
+            description: 'description',
+          },
+        }),
+      { wrapper }
+    )
+    await act(() => {
+      result.current.mutateAsync()
+    })
+
+    expect(screen.queryByRole('dialog', { name: 'title' })).not.toBeInTheDocument()
+  })
+
+  it('Should show the confirmation modal and call the mutation on confirm button press', async () => {
+    const { result } = renderHook(
+      () =>
+        useMutationWrapper(mockMutationServices.success, {
+          successToastLabel: 'success',
+          errorToastLabel: 'error',
+          loadingLabel: 'loading',
+          showConfirmationDialog: true,
+          dialogConfig: {
+            title: 'title',
+            description: 'description',
+          },
+        }),
+      { wrapper }
+    )
+    await act(() => {
+      result.current.mutate()
+    })
+
+    expect(screen.queryByRole('dialog', { name: 'title' })).toBeInTheDocument()
+
+    fireEvent.click(screen.getByRole('button', { name: 'confirm' }))
+
+    expect(screen.getByRole('progressbar', { hidden: true })).toBeInTheDocument()
+  })
+
+  it('Should show the confirmation modal and not call the mutation on cancel button press', async () => {
+    const { result } = renderHook(
+      () =>
+        useMutationWrapper(mockMutationServices.success, {
+          successToastLabel: 'success',
+          errorToastLabel: 'error',
+          loadingLabel: 'loading',
+          showConfirmationDialog: true,
+          dialogConfig: {
+            title: 'title',
+            description: 'description',
+          },
+        }),
+      { wrapper }
+    )
+    await act(() => {
+      result.current.mutate()
+    })
+
+    expect(screen.getByRole('dialog', { name: 'title' })).toBeInTheDocument()
+
+    fireEvent.click(screen.getByRole('button', { name: 'cancel' }))
+
+    expect(screen.queryByRole('progressbar', { hidden: true })).not.toBeInTheDocument()
+  })
+})
