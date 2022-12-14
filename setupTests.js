ae244be206b7c15b911885a38b6167531cae92c8
Carmine Porricelli <carmine.porricelli@pagopa.it>
2022-12-14 10:39:39 +0100

    Added react-i18next testing stubs

--- setupTests.js
+++ setupTests.js
@@ -11,3 +11,18 @@ afterEach(() => {
 })
 
 vi.stubGlobal('scroll', vi.fn())
+
+vi.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str) => str,
+      i18n: {
+        changeLanguage: () =>
+          new Promise(() => {
+            /**/
+          }),
+      },
+    }
+  },
+}))
