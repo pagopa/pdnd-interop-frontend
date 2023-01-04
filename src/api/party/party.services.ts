56e7b4a9be1181f77195499567adb607d3ef7416
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-01-04 17:08:49 +0100

    Added remap TenantAttribute to PartyAttribute type

--- src/api/party/party.services.ts
+++ src/api/party/party.services.ts
@@ -2,13 +2,14 @@ import { BACKEND_FOR_FRONTEND_URL } from '@/config/env'
 import axiosInstance from '@/config/axios'
 import { SelfCareUser, UserType } from '@/types/party.types'
 import { PartyGetUsersListUrlParams } from './party.api.types'
+import { remapUserResponseData } from './party.utils'
 
 async function getUser(partyId: string) {
   const response = await axiosInstance.get<UserType>(
     `${BACKEND_FOR_FRONTEND_URL}/tenants/${partyId}`
   )
 
-  return response.data
+  return remapUserResponseData(response.data)
 }
 
 async function getUsersList(partyId: string, params?: PartyGetUsersListUrlParams) {
--- src/api/party/party.utils.ts
+++ src/api/party/party.utils.ts
@@ -1,5 +1,11 @@
+import {
+  CertifiedTenantAttribute,
+  DeclaredTenantAttribute,
+  VerifiedTenantAttribute,
+} from '@/types/attribute.types'
 import { EServiceDescriptorCatalog, EServiceDescriptorProvider } from '@/types/eservice.types'
-import { PartyMail } from '@/types/party.types'
+import { PartyMail, UserType } from '@/types/party.types'
+import { remapTenantAttributeToPartyAttribute } from '../attribute/attribute.api.utils'
 
 export function updateDescriptorCatalogPartyMailCache(
   mail: PartyMail,
@@ -17,4 +23,23 @@ export function updateDescriptorProviderPartyMailCache(
   if (!descriptorProvider) return descriptorProvider
 
   return { ...descriptorProvider, eservice: { ...descriptorProvider.eservice, mail } }
+}
+
+export function remapUserResponseData(data: UserType, verifierId?: string) {
+  const certifiedAttributes = data.attributes.certified as unknown as CertifiedTenantAttribute[]
+  const verifiedAttributes = data.attributes.verified as unknown as VerifiedTenantAttribute[]
+  const declaredAttributes = data.attributes.declared as unknown as DeclaredTenantAttribute[]
+
+  const attributes = {
+    certified: certifiedAttributes.map((att) =>
+      remapTenantAttributeToPartyAttribute(att, 'certified')
+    ),
+    verified: verifiedAttributes.map((att) =>
+      remapTenantAttributeToPartyAttribute(att, 'verified', verifierId)
+    ),
+    declared: declaredAttributes.map((att) =>
+      remapTenantAttributeToPartyAttribute(att, 'declared')
+    ),
+  }
+  return { ...data, attributes }
 }
