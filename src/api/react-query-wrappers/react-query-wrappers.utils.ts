5a362588c1ccfbb743aa2f707c20d1dcf2a83d1d
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-01-11 11:07:41 +0100

    Added useDownloadFile tests

--- src/api/react-query-wrappers/react-query-wrappers.utils.ts
+++ src/api/react-query-wrappers/react-query-wrappers.utils.ts
@@ -56,3 +56,21 @@ export function clearExponentialInterval(instanceId: string | undefined) {
     exponentialIntervalInstances.delete(instanceId)
   }
 }
+
+export function downloadFile(responseData: string, filename = 'download') {
+  const blob = new Blob([responseData], { type: 'application/octet-stream' })
+  // Create a pointer to the local memory where the blob is temporarily stored
+  const href = window.URL.createObjectURL(blob)
+  // Create link to append to the DOM, it will be clicked programmatically
+  // to initiate file download
+  const link = document.createElement('a')
+  link.setAttribute('download', filename)
+  // Set the link href to the local memory pointer
+  link.setAttribute('href', href)
+  document.body.appendChild(link)
+  link.click()
+  // Remove link
+  document.body.removeChild(link)
+  // Release memory
+  URL.revokeObjectURL(link.href)
+}
--- src/api/react-query-wrappers/useDownloadFile.ts
+++ src/api/react-query-wrappers/useDownloadFile.ts
@@ -1,36 +1,21 @@
 import { useLoadingOverlay, useToastNotification } from '@/contexts'
-
-function downloadFile(responseData: string, filename = 'download') {
-  const blob = new Blob([responseData], { type: 'application/octet-stream' })
-  // Create a pointer to the local memory where the blob is temporarily stored
-  const href = window.URL.createObjectURL(blob)
-  // Create link to append to the DOM, it will be clicked programmatically
-  // to initiate file download
-  const link = document.createElement('a')
-  link.setAttribute('download', filename)
-  // Set the link href to the local memory pointer
-  link.setAttribute('href', href)
-  document.body.appendChild(link)
-  link.click()
-  // Remove link
-  document.body.removeChild(link)
-  // Release memory
-  URL.revokeObjectURL(link.href)
-}
+import { logger } from '@/utils/common.utils'
+import { downloadFile } from './react-query-wrappers.utils'
 
 export function useDownloadFile<T = unknown[]>(
-  service: (args: T) => Promise<string>,
+  service: (args?: T) => Promise<string>,
   config: { errorToastLabel?: string; loadingLabel: string }
 ) {
   const { showOverlay, hideOverlay } = useLoadingOverlay()
   const { showToast } = useToastNotification()
 
-  return async (args: T, filename?: string) => {
+  return async (args?: T, filename?: string) => {
     showOverlay(config.loadingLabel)
     try {
       const data = await service(args)
       downloadFile(data, filename)
-    } catch {
+    } catch (error) {
+      logger.error(error)
       config.errorToastLabel && showToast(config.errorToastLabel, 'error')
     } finally {
       hideOverlay()
