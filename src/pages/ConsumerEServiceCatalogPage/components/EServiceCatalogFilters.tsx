a61763a14b31fe18067d88d1d362712e9ac221d8
alten-dturus <davide.turus@alten.it>
2023-01-17 11:19:05 +0100

    Filter component
    - Implemented the filters component for the catalog
    - Added the filters component to the catalog page

---
+++ src/pages/ConsumerEServiceCatalogPage/components/EServiceCatalogFilters.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Button, Stack } from '@mui/material'
+import { FormProvider, UseFormReturn } from 'react-hook-form'
+import { AutocompleteMultiple, TextField } from '@/components/shared/ReactHookFormInputs'
+import { EServiceQueries } from '@/api/eservice'
+
+interface EServiceCatalogFiltersProps {
+  clearFilters: VoidFunction
+  enableFilters: VoidFunction
+  filtersFormMethods: UseFormReturn<any, any>
+}
+
+const EServiceCatalogFilters: React.FC<EServiceCatalogFiltersProps> = ({
+  clearFilters,
+  enableFilters,
+  filtersFormMethods,
+}) => {
+  const { data: producers, isLoading: isLoadingProducers } = EServiceQueries.useGetProducers(
+    { limit: 50, offset: 0 },
+    {
+      suspense: false,
+    }
+  )
+
+  const producersOptions =
+    producers?.results.map((o) => ({
+      label: o.name || 'default',
+      value: o.id,
+    })) || []
+
+  return (
+    <FormProvider {...filtersFormMethods}>
+      <Stack
+        onSubmit={enableFilters}
+        component="form"
+        direction="row"
+        spacing={2}
+        justifyContent="space-between"
+        sx={{ mb: 4 }}
+      >
+        <Stack direction="row" spacing={2} sx={{ width: '60%' }}>
+          <TextField
+            sx={{ m: 0, width: '55%' }}
+            size="small"
+            name="q"
+            label="Cerca per nome dell’e-service"
+          />
+          <AutocompleteMultiple
+            sx={{ width: '45%' }}
+            size="small"
+            name="producersIds"
+            label="producers"
+            options={producersOptions}
+            loading={isLoadingProducers}
+          />
+        </Stack>
+
+        <Stack direction="row" spacing={2}>
+          <Button size="small" variant="outlined" type="submit">
+            Filtra
+          </Button>
+          <Button size="small" variant="text" type="button" onClick={clearFilters}>
+            Annulla filtri
+          </Button>
+        </Stack>
+      </Stack>
+    </FormProvider>
+  )
+}
+
+export default EServiceCatalogFilters
