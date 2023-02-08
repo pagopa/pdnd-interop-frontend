c0169754c090cf3734a07d4a820e25772cf37708
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-02-08 17:33:53 +0100

    Added useGetProviderPurposesActions testing

---
+++ src/hooks/__tests__/useGetProviderPurposesActions.test.ts
@@ -0,0 +1,76 @@
+import { DecoratedPurpose, PurposeListingItem } from '@/types/purpose.types'
+import { renderHookWithApplicationContext } from '@/utils/testing.utils'
+import useGetProviderPurposesActions from '../useGetProviderPurposesActions'
+import {
+  createMockDecoratedPurpose,
+  createMockPurposeListingItem,
+} from '__mocks__/data/purpose.mocks'
+
+function renderUseGetProviderPurposesActionsHook(purpose?: DecoratedPurpose | PurposeListingItem) {
+  return renderHookWithApplicationContext(() => useGetProviderPurposesActions(purpose), {
+    withReactQueryContext: true,
+    withRouterContext: true,
+  })
+}
+
+describe('', () => {
+  it('shoud not return any action if no purpose is given', () => {
+    const { result } = renderUseGetProviderPurposesActionsHook()
+    expect(result.current.actions).toHaveLength(0)
+  })
+
+  it('shoud not return any action if an archived purpose is given', () => {
+    const purposeMock = createMockDecoratedPurpose({ currentVersion: { state: 'ARCHIVED' } })
+    const { result } = renderUseGetProviderPurposesActionsHook(purposeMock)
+    expect(result.current.actions).toHaveLength(0)
+  })
+
+  it('should return the suspend function if the current version is active', () => {
+    const purposeMock = createMockPurposeListingItem({ currentVersion: { state: 'ACTIVE' } })
+    const { result } = renderUseGetProviderPurposesActionsHook(purposeMock)
+
+    const suspendAction = result.current.actions.find((action) => action.label === 'suspend')
+    expect(suspendAction).toBeTruthy()
+  })
+
+  it('shoud have action to update completion date and active the waiting for approval version if the purpose has one', () => {
+    const purposeMock = createMockPurposeListingItem({
+      waitingForApprovalVersion: {
+        id: 'test-id',
+        dailyCalls: 2,
+        state: 'WAITING_FOR_APPROVAL',
+      },
+    })
+    const { result } = renderUseGetProviderPurposesActionsHook(purposeMock)
+    const activateVersionAction = result.current.actions.find(
+      (action) => action.label === 'confirmUpdate'
+    )
+    const updateCompletionDateAction = result.current.actions.find(
+      (action) => action.label === 'updateCompletionDate'
+    )
+    expect(activateVersionAction).toBeTruthy()
+    expect(updateCompletionDateAction).toBeTruthy()
+  })
+
+  it('shoud have the suspend action if the purpose is suspended but not suspended by the provider ', () => {
+    const purposeMock = createMockPurposeListingItem({
+      currentVersion: { state: 'SUSPENDED' },
+      suspendedByProducer: false,
+    })
+
+    const { result } = renderUseGetProviderPurposesActionsHook(purposeMock)
+    const suspendAction = result.current.actions.find((action) => action.label === 'suspend')
+    expect(suspendAction).toBeTruthy()
+  })
+
+  it('shoud have the activate action if the purpose is suspended by the provider ', () => {
+    const purposeMock = createMockPurposeListingItem({
+      currentVersion: { state: 'SUSPENDED' },
+      suspendedByProducer: true,
+    })
+
+    const { result } = renderUseGetProviderPurposesActionsHook(purposeMock)
+    const suspendAction = result.current.actions.find((action) => action.label === 'activate')
+    expect(suspendAction).toBeTruthy()
+  })
+})
