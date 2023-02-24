4a16533a69d9d8793a4473a939c5d4647682bc86
alten-dturus <davide.turus@alten.it>
2023-02-24 15:08:15 +0100

    Implemented test for useGetClientActions hook
    - Added client mock function
    - Modified routes for wrapper in testing.utils
    - Implemented tests for useGetClientActions hook

---
+++ __mocks__/data/client.mocks.ts
@@ -0,0 +1,14 @@
+import { Client } from '../../src/types/client.types'
+import { createMockFactory } from '../../src/utils/testing.utils'
+
+const createMockClient = createMockFactory<Client>({
+  id: '85ceaa96-a95e-4cf9-b1f9-b85be1e09369',
+  name: 'test - 24/02/23',
+  description: 'test test test',
+  operators: [],
+  kind: 'CONSUMER',
+  purposes: [],
+  consumer: { description: "Agenzia per L'Italia Digitale", institutionId: 'agid' },
+})
+
+export { createMockClient }
