72e5a3614eb27dce389d51781406e2d4062310cb
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-02-28 09:58:29 +0100

    Fixes purpose clone service

--- src/api/purpose/purpose.hooks.ts
+++ src/api/purpose/purpose.hooks.ts
@@ -191,6 +191,7 @@ function useClone() {
     successToastLabel: t('outcome.success'),
     errorToastLabel: t('outcome.error'),
     loadingLabel: t('loading'),
+    showConfirmationDialog: true,
     dialogConfig: {
       title: t('confirmDialog.title'),
       description: t('confirmDialog.description'),
