5f68a2067d3bb48665d929fecc75c139250fa0b3
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-02-28 10:27:58 +0100

    Update tests

--- src/hooks/__tests__/useGetConsumerPurposesActions.test.ts
+++ src/hooks/__tests__/useGetConsumerPurposesActions.test.ts
@@ -5,6 +5,34 @@ import {
   createMockDecoratedPurpose,
   createMockPurposeListingItem,
 } from '__mocks__/data/purpose.mocks'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { BACKEND_FOR_FRONTEND_URL } from '@/config/env'
+import { act, fireEvent, screen, waitForElementToBeRemoved } from '@testing-library/react'
+import { routes } from '@/router/routes'
+import { generatePath } from 'react-router-dom'
+
+const server = setupServer(
+  rest.post(
+    `${BACKEND_FOR_FRONTEND_URL}/purposes/e8a8153e-9ab2-4aeb-a14c-96aebd4fa049/clone`,
+    (_, res, ctx) => {
+      return res(
+        ctx.json({
+          purposeId: 'test-purpose-id',
+          versionId: 'test-purpose-version-id',
+        })
+      )
+    }
+  )
+)
+
+beforeAll(() => {
+  server.listen()
+})
+
+afterAll(() => {
+  server.close()
+})
 
 function renderUseGetConsumerPurposesActionsHook(purpose?: DecoratedPurpose | PurposeListingItem) {
   return renderHookWithApplicationContext(() => useGetConsumerPurposesActions(purpose), {
@@ -19,10 +47,14 @@ describe('check if useGetConsumerPurposesActions returns the correct actions bas
     expect(result.current.actions).toHaveLength(0)
   })
 
-  it('shoud not return any action if an archived purpose is given', () => {
+  it('shoud only return clone action if an archived purpose is given', () => {
     const purposeMock = createMockDecoratedPurpose({ currentVersion: { state: 'ARCHIVED' } })
     const { result } = renderUseGetConsumerPurposesActionsHook(purposeMock)
-    expect(result.current.actions).toHaveLength(0)
+
+    const cloneAction = result.current.actions.find((action) => action.label === 'clone')
+
+    expect(result.current.actions).toHaveLength(1)
+    expect(cloneAction).toBeTruthy()
   })
 
   it('should return the activate and delete functions if the current version is in draft', () => {
@@ -67,7 +99,10 @@ describe('check if useGetConsumerPurposesActions returns the correct actions bas
     const deleteDailyCallsUpdateAction = result.current.actions.find(
       (action) => action.label === 'deleteDailyCallsUpdate'
     )
+    const cloneAction = result.current.actions.find((action) => action.label === 'clone')
+
     expect(deleteDailyCallsUpdateAction).toBeTruthy()
+    expect(cloneAction).toBeTruthy()
   })
 
   it('should return the updated daily calls update action if the purpose has more than one version and no waiting for approval version', () => {
@@ -78,7 +113,10 @@ describe('check if useGetConsumerPurposesActions returns the correct actions bas
     const updateDailyCallsAction = result.current.actions.find(
       (action) => action.label === 'updateDailyCalls'
     )
+    const cloneAction = result.current.actions.find((action) => action.label === 'clone')
+
     expect(updateDailyCallsAction).toBeTruthy()
+    expect(cloneAction).toBeTruthy()
   })
 
   it('should return the suspend action if the purpose is active', () => {
@@ -89,7 +127,10 @@ describe('check if useGetConsumerPurposesActions returns the correct actions bas
     expect(result.current.actions.length).toBeGreaterThanOrEqual(1)
 
     const suspendAction = result.current.actions.find((action) => action.label === 'suspend')
+    const cloneAction = result.current.actions.find((action) => action.label === 'clone')
+
     expect(suspendAction).toBeTruthy()
+    expect(cloneAction).toBeTruthy()
   })
 
   it('should return the suspend action if the purpose is suspended by the provider', () => {
@@ -101,7 +142,10 @@ describe('check if useGetConsumerPurposesActions returns the correct actions bas
     expect(result.current.actions.length).toBeGreaterThanOrEqual(1)
 
     const suspendAction = result.current.actions.find((action) => action.label === 'suspend')
+    const cloneAction = result.current.actions.find((action) => action.label === 'clone')
+
     expect(suspendAction).toBeTruthy()
+    expect(cloneAction).toBeTruthy()
   })
 
   it('should return the activate action if the purpose is suspended by the consumer', () => {
@@ -113,6 +157,35 @@ describe('check if useGetConsumerPurposesActions returns the correct actions bas
     expect(result.current.actions.length).toBeGreaterThanOrEqual(1)
 
     const activateAction = result.current.actions.find((action) => action.label === 'activate')
+    const cloneAction = result.current.actions.find((action) => action.label === 'clone')
+
     expect(activateAction).toBeTruthy()
+    expect(cloneAction).toBeTruthy()
+  })
+
+  it('should navigate to the purpose edit page on clone action success', async () => {
+    const purposeMock = createMockPurposeListingItem({
+      id: 'e8a8153e-9ab2-4aeb-a14c-96aebd4fa049',
+      currentVersion: { state: 'SUSPENDED' },
+      suspendedByConsumer: true,
+    })
+    const { result, history } = renderUseGetConsumerPurposesActionsHook(purposeMock)
+
+    const cloneAction = result.current.actions.find((action) => action.label === 'clone')
+    expect(cloneAction).toBeTruthy()
+
+    act(() => {
+      cloneAction?.action()
+    })
+
+    act(() => {
+      fireEvent.click(screen.getByRole('button', { name: 'confirm' }))
+    })
+
+    await waitForElementToBeRemoved(screen.getByRole('progressbar', { hidden: true }))
+
+    expect(history.location.pathname).toBe(
+      '/it/' + generatePath(routes.SUBSCRIBE_PURPOSE_EDIT.PATH.it, { purposeId: 'test-purpose-id' })
+    )
   })
 })
