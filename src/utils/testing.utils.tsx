f45f37192210a092a460976055dcfe27d2de45d5
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-03-02 16:23:38 +0100

    Removed unused testing utils function

--- src/utils/testing.utils.tsx
+++ src/utils/testing.utils.tsx
@@ -33,23 +33,6 @@ const queryClientConfigMock: QueryClientConfig = deepmerge(
 
 export const queryClientMock = new QueryClient(queryClientConfigMock)
 
-/**
- * Wraps the data passed in the pagination data structure
- * */
-export function createPaginatedMockData<T>(
-  data: Array<T>,
-  pagination?: Partial<Paginated<T>['pagination']>
-): Paginated<T> {
-  return {
-    results: data,
-    pagination: {
-      offset: pagination?.offset ?? 0,
-      limit: pagination?.offset ?? 50,
-      totalCount: pagination?.offset ?? data.length,
-    },
-  }
-}
-
 type RecursivePartial<T> = {
   [P in keyof T]?: RecursivePartial<T[P]>
 }
