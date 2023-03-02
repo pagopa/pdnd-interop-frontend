d52773d2466fec41bfdf2bd1a829426a80c3916d
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-03-02 17:27:18 +0100

    Added e-service utils tests

---
+++ src/utils/__tests__/eservice.utils.test.ts
@@ -0,0 +1,14 @@
+import { getDownloadDocumentName } from '../eservice.utils'
+
+describe('getDownloadDocumentName utility function testing', () => {
+  it('should correctly get the document namy from a DocumentRead data type', () => {
+    const result = getDownloadDocumentName({
+      id: 'test-id',
+      contentType: 'pdf',
+      name: 'test.pdf',
+      prettyName: 'document',
+    })
+
+    expect(result).toEqual('document.pdf')
+  })
+})
