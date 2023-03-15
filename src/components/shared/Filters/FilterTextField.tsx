cf5016bac89ca2bf90fb5416ad12c921052e9368
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-03-15 10:36:17 +0100

    Added active filter icon button in filters single text fields

--- src/components/shared/Filters/FilterTextField.tsx
+++ src/components/shared/Filters/FilterTextField.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
-import { TextField } from '@mui/material'
+import { IconButton, TextField } from '@mui/material'
 import type { TextFieldProps } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search'
+import { useTranslation } from 'react-i18next'
 
-export const FilterTextField: React.FC<TextFieldProps> = ({ InputProps, ...props }) => {
+type FilterTextField = TextFieldProps & {
+  onIconClick: VoidFunction
+}
+
+export const FilterTextField: React.FC<FilterTextField> = ({
+  InputProps,
+  onIconClick,
+  ...props
+}) => {
+  const { t } = useTranslation('common', { keyPrefix: 'actions' })
   return (
     <TextField
       size="small"
       {...props}
       InputProps={{
         ...InputProps,
-        endAdornment: <SearchIcon sx={{ color: 'gray' }} />,
+        endAdornment: (
+          <IconButton sx={{ mx: -1 }} onClick={onIconClick}>
+            <SearchIcon aria-label={t('filter')} />
+          </IconButton>
+        ),
       }}
     />
   )
