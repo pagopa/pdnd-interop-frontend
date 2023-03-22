77e0fcfe88e3baa555555ee542037261615de922
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-03-22 14:37:53 +0100

    Update party.api.types.ts

--- src/api/party/party.api.types.ts
+++ src/api/party/party.api.types.ts
@@ -9,6 +9,10 @@ export type PartyGetUsersListUrlParams = {
   query?: string
 }
 
+/**
+ * This type must come from mui-italia.
+ * Waiting for type PartySwitchItem to have property "productRole" as Array<UserProductRole> and not string | undefined
+ */
 export type PartyItem = {
   id: string
   description: string
