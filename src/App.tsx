0294b15d07754e2f18261e29787fd299743f99db
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-03-24 09:22:55 +0100

    Replaced theme

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import { RouterProvider } from '@/router'
 import { LoadingOverlay, ToastNotification } from '@/components/layout'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { queryClientConfig } from '@/config/query-client'
-import { theme } from '@/config/theme'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { theme } from '@pagopa/interop-fe-commons'
 
 const queryClient = new QueryClient(queryClientConfig)
 
