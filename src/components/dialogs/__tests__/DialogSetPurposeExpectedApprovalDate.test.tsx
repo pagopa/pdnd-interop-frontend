ead41ea18481c29d9773988a3cb26bca9e183cb1
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2023-04-13 15:16:38 +0200

    Revert "Fixed failing tests"
    This reverts commit ea3be7133449772becd559442c7c8ea2f1287d51.

--- src/components/dialogs/__tests__/DialogSetPurposeExpectedApprovalDate.test.tsx
+++ src/components/dialogs/__tests__/DialogSetPurposeExpectedApprovalDate.test.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { DialogSetPurposeExpectedApprovalDate } from '../DialogSetPurposeExpectedApprovalDate'
 import { renderWithApplicationContext } from '@/utils/testing.utils'
-import { vi } from 'vitest'
 
 describe('DialogSetPurposeExpectedApprovalDate testing', () => {
   it('should match the snapshot', () => {
-    vi.useFakeTimers().setSystemTime(new Date('2020-01-01'))
-
     const screen = renderWithApplicationContext(
       <DialogSetPurposeExpectedApprovalDate
         type="setPurposeExpectedApprovalDate"
