3a5b3585f9e0c9e595702e47cfb98f86c1b903e3
alten-dturus <davide.turus@alten.it>
2023-04-17 15:54:38 +0200

    Update AttributeAutocomplete component

--- src/pages/ProviderEServiceCreatePage/components/EServiceCreateStep1General/AddAttributesToEServiceForm/AttributeAutocomplete.tsx
+++ src/pages/ProviderEServiceCreatePage/components/EServiceCreateStep1General/AddAttributesToEServiceForm/AttributeAutocomplete.tsx
@@ -4,11 +4,11 @@ import { RHFAutocompleteSingle } from '@/components/shared/react-hook-form-input
 import type { AttributeKey } from '@/types/attribute.types'
 import { Button, Stack } from '@mui/material'
 import { ButtonNaked } from '@pagopa/mui-italia'
-import debounce from 'lodash/debounce'
 import { FormProvider, useForm, useFormContext } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import type { EServiceCreateStep1FormValues } from '../EServiceCreateStep1General'
-import type { Attribute } from '@/api/api.generatedTypes'
+import type { Attribute, AttributeKind } from '@/api/api.generatedTypes'
+import { useAutocompleteTextInput } from '@pagopa/interop-fe-commons'
 
 type AttributeAutocompleteProps = {
   groupIndex: number
@@ -24,10 +24,18 @@ export const AttributeAutocomplete: React.FC<AttributeAutocompleteProps> = ({
   handleHideAutocomplete,
 }) => {
   const { t } = useTranslation('attribute', { keyPrefix: 'group' })
-  const [search, setSearch] = React.useState<string | undefined>()
-  const { data: attributes = [], isLoading } = AttributeQueries.useGetList(search, {
-    suspense: false,
-  })
+  const [attributeSearchParam, setAttributeSearchParam] = useAutocompleteTextInput()
+  const { data, isLoading } = AttributeQueries.useGetList(
+    {
+      kinds: [attributeKey.toUpperCase() as AttributeKind],
+      q: attributeSearchParam,
+      offset: 0,
+      limit: 50,
+    },
+    {
+      suspense: false,
+    }
+  )
 
   const { watch, setValue } = useFormContext<EServiceCreateStep1FormValues>()
   const attributeGroups = watch(`attributes.${attributeKey}`)
@@ -35,14 +43,6 @@ export const AttributeAutocomplete: React.FC<AttributeAutocompleteProps> = ({
   const attributeAutocompleteFormMethods = useForm<AttributeAutocompleteFormValues>({
     defaultValues: { attribute: null },
   })
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const handleSearchChange = React.useCallback(
-    debounce((value: string) => {
-      setSearch(value || undefined)
-    }, 280),
-    []
-  )
 
   const { watch: watchAttribute, handleSubmit } = attributeAutocompleteFormMethods
   const isSelected = !!watchAttribute('attribute')
@@ -56,27 +56,25 @@ export const AttributeAutocomplete: React.FC<AttributeAutocompleteProps> = ({
   })
 
   const options = React.useMemo(() => {
+    const attributes = data?.results ?? []
     const attributesAlreadyInGroups = attributeGroups.reduce(
       (acc, group) => [...acc, ...group.attributes.map(({ id }) => id)],
       [] as Array<string>
     )
     return attributes
-      .filter(
-        (att) =>
-          att.kind === attributeKey.toUpperCase() && !attributesAlreadyInGroups.includes(att.id)
-      )
+      .filter((att) => !attributesAlreadyInGroups.includes(att.id))
       .map((att) => ({
         label: att.name,
         value: att,
       }))
-  }, [attributes, attributeKey, attributeGroups])
+  }, [data?.results, attributeGroups])
 
   return (
     <FormProvider {...attributeAutocompleteFormMethods}>
       <RHFAutocompleteSingle
         label={t('autocompleteInput.label')}
         placeholder={t('autocompleteInput.placeholder')}
-        onInputChange={(_, value) => handleSearchChange(value)}
+        onInputChange={(_, value) => setAttributeSearchParam(value)}
         loading={isLoading}
         sx={{ mb: 0, flex: 1 }}
         options={options}
