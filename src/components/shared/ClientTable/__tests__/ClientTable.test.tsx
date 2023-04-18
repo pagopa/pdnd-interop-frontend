69de666761a382c6fdf433263dae8cec807a7ae5
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-04-18 11:37:09 +0200

    Updated ClientTable tests

--- src/components/shared/ClientTable/__tests__/ClientTable.test.tsx
+++ src/components/shared/ClientTable/__tests__/ClientTable.test.tsx
@@ -8,9 +8,12 @@ import { setupServer } from 'msw/node'
 import { rest } from 'msw'
 import { BACKEND_FOR_FRONTEND_URL } from '@/config/env'
 import type { CompactClients } from '@/api/api.generatedTypes'
+import { ClientQueries } from '@/api/client'
 
 vi.spyOn(useClientKindHook, 'useClientKind').mockReturnValue('API')
 mockUseJwt()
+
+const useGetListSpy = vi.spyOn(ClientQueries, 'useGetList')
 
 const server = setupServer(
   rest.get(`${BACKEND_FOR_FRONTEND_URL}/clients`, (req, res, ctx) => {
@@ -43,32 +46,56 @@ afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
 describe('ClientTable', () => {
+  it('should match the snapshot in loading state', async () => {
+    const screen = renderWithApplicationContext(<ClientTable clientKind="API" />, {
+      withRouterContext: true,
+      withReactQueryContext: true,
+    })
+    expect(screen.baseElement).toMatchSnapshot()
+  })
+
   it('should match the snapshot', async () => {
+    useGetListSpy.mockReturnValue({
+      data: {
+        results: [
+          {
+            id: '1',
+            name: 'client1',
+            hasKeys: true,
+          },
+          {
+            id: '2',
+            name: 'client2',
+            hasKeys: true,
+          },
+        ],
+        pagination: {
+          totalCount: 2,
+          limit: 10,
+          offset: 0,
+        },
+      },
+    } as unknown as ReturnType<typeof ClientQueries.useGetList>)
+
     const screen = renderWithApplicationContext(<ClientTable clientKind="API" />, {
       withRouterContext: true,
       withReactQueryContext: true,
     })
 
-    expect(screen.baseElement).toMatchSnapshot('loading state')
-    await waitFor(() => screen.getByText('client1'))
-    expect(screen.baseElement).toMatchSnapshot('full state')
+    expect(screen.baseElement).toMatchSnapshot()
   })
 
   it('should match the snapshot in empty state', async () => {
-    server.use(
-      rest.get(`${BACKEND_FOR_FRONTEND_URL}/clients`, (req, res, ctx) => {
-        return res(
-          ctx.json<CompactClients>({
-            results: [],
-            pagination: {
-              totalCount: 0,
-              limit: 10,
-              offset: 0,
-            },
-          })
-        )
-      })
-    )
+    useGetListSpy.mockReturnValue({
+      data: {
+        results: [],
+        pagination: {
+          totalCount: 0,
+          limit: 10,
+          offset: 0,
+        },
+      },
+    } as unknown as ReturnType<typeof ClientQueries.useGetList>)
     const screen = renderWithApplicationContext(<ClientTable clientKind="API" />, {
       withRouterContext: true,
       withReactQueryContext: true,
