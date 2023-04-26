d5d69b9fdc2f24423cad529a4141bc6eb8301355
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-04-26 11:47:34 +0200

    Added ConsumerClientM2MList page tests

---
+++ src/pages/ConsumerClientM2MListPage/__tests__/ConsumerClientM2MList.page.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {
+  mockUseClientKind,
+  mockUseCurrentRoute,
+  mockUseJwt,
+  renderWithApplicationContext,
+} from '@/utils/testing.utils'
+import ConsumerClientM2MListPage from '../ConsumerClientM2MList.page'
+import userEvent from '@testing-library/user-event'
+
+mockUseCurrentRoute({ mode: 'consumer' })
+mockUseClientKind('API')
+
+describe('ConsumerClientM2MListPage', () => {
+  it('should match the snapshot', () => {
+    const { baseElement } = renderWithApplicationContext(<ConsumerClientM2MListPage />, {
+      withRouterContext: true,
+      withReactQueryContext: true,
+    })
+    expect(baseElement).toMatchSnapshot()
+  })
+
+  it('should successfully navigate to the create M2M client page', async () => {
+    mockUseJwt({ isAdmin: true })
+    const screen = renderWithApplicationContext(<ConsumerClientM2MListPage />, {
+      withRouterContext: true,
+      withReactQueryContext: true,
+    })
+
+    const createNewBtn = screen.getByRole('button', { name: 'createNewBtn' })
+    const user = userEvent.setup()
+    await user.click(createNewBtn)
+    expect(screen.history.location.pathname).toEqual('/it/fruizione/interop-m2m/crea')
+  })
+})
