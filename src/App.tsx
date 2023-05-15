e4d488bf7dd673db14cb7791c8aa20b49125cb54
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-05-15 14:45:07 +0200

    Feature: PIN-1629 - Added EnvironmentBanner component from mui-italia (#414)
    - Bump mui-italia version to 0.9.5
    - Added EnvironmentBanner component

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,24 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { queryClientConfig } from '@/config/query-client'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { theme } from '@pagopa/interop-fe-commons'
+import { EnvironmentBanner } from '@pagopa/mui-italia'
+import { STAGE } from './config/env'
+import WarningAmberIcon from '@mui/icons-material/WarningAmber'
+import { useTranslation } from 'react-i18next'
 
 const queryClient = new QueryClient(queryClientConfig)
 
 function App() {
+  const { t } = useTranslation('shared-components')
   return (
     <ThemeProvider theme={theme}>
+      {STAGE === 'UAT' && (
+        <EnvironmentBanner
+          env="test"
+          message={t('environmentBanner.content')}
+          icon={<WarningAmberIcon fontSize="small" />}
+        />
+      )}
       <React.Suspense fallback={<FirstLoadingSpinner />}>
         <QueryClientProvider client={queryClient}>
           <CssBaseline />
