07ad976ae8f5264372b7e893dc61f76d4fe4f096
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-05-18 11:43:31 +0200

    Fix breadcrumb

--- src/components/layout/Breadcrumbs.tsx
+++ src/components/layout/Breadcrumbs.tsx
@@ -5,5 +5,5 @@ import { Breadcrumbs as _Breadcrumbs } from '@/router'
 export function Breadcrumbs() {
   const { t } = useTranslation('shared-components')
   const routeLabels = t('routeLabels', { returnObjects: true })
-  return <_Breadcrumbs routeLabels={routeLabels} />
+  return <_Breadcrumbs routeLabels={{ ...routeLabels, DEFAULT: false }} />
 }
