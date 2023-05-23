00164c84e62abf8a0597efb413060fc8e8a11bfc
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-05-23 18:18:40 +0200

    Update Breadcrumbs.tsx

--- src/components/layout/Breadcrumbs.tsx
+++ src/components/layout/Breadcrumbs.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { Breadcrumbs as _Breadcrumbs } from '@/router'
+import { Breadcrumbs as _Breadcrumbs, useLocation } from '@/router'
 
 export function Breadcrumbs() {
   const { t } = useTranslation('shared-components')
   const routeLabels = t('routeLabels', { returnObjects: true })
-  return <_Breadcrumbs routeLabels={{ ...routeLabels, DEFAULT: false }} />
+  const { routeKey } = useLocation()
+
+  return (
+    <_Breadcrumbs
+      routeLabels={{
+        ...routeLabels,
+        PROVIDE_ESERVICE_MANAGE:
+          routeKey === 'PROVIDE_ESERVICE_EDIT' ? false : routeLabels.PROVIDE_ESERVICE_MANAGE,
+        DEFAULT: false,
+      }}
+    />
+  )
 }
