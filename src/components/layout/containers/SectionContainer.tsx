0e666a41b3948cb57e9e4f2b685674612f0f6569
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-05-25 15:49:58 +0200

    Added `innerSection`, `titleTypographyProps ` and `descriptionTypographyProps ` to `SectionContainer` component

--- src/components/layout/containers/SectionContainer.tsx
+++ src/components/layout/containers/SectionContainer.tsx
@@ -2,11 +2,19 @@ import { Box, Typography, Paper, Skeleton, Stack } from '@mui/material'
 import type { PaperProps, SkeletonProps } from '@mui/material'
 import React from 'react'
 
+type TypographyProps = Omit<React.ComponentProps<typeof Typography>, 'ref'> & {
+  component?: React.ElementType
+}
+
 interface SectionContainerProps extends PaperProps {
   title?: string
   description?: React.ReactNode
   children: React.ReactNode
   component?: React.ElementType
+  titleTypographyProps?: TypographyProps
+  descriptionTypographyProps?: TypographyProps
+
+  innerSection?: boolean
 
   /**
    * The `newDesign` prop is temporary and will be removed when the new section container design will be
@@ -19,24 +27,39 @@ export function SectionContainer({
   title,
   description,
   children,
+  innerSection,
   sx,
   component = 'section',
+  titleTypographyProps,
+  descriptionTypographyProps,
   newDesign,
   ...props
 }: SectionContainerProps) {
-  const titleVariant = newDesign ? 'h6' : 'overline'
+  const titleVariant = !newDesign ? 'overline' : innerSection ? 'sidenav' : 'h6'
   const descriptionVariant = newDesign ? 'body2' : 'caption'
 
   return (
-    <Paper component={component} sx={{ bgcolor: 'white', p: 3, mt: 2, ...sx }} {...props}>
+    <Paper
+      component={component}
+      sx={{ bgcolor: 'white', p: !innerSection ? 3 : 0, mt: 2, ...sx }}
+      {...props}
+    >
       <Stack spacing={1}>
         {title && (
-          <Typography component="h2" variant={titleVariant}>
+          <Typography
+            component={innerSection ? 'h3' : 'h2'}
+            variant={titleVariant}
+            {...titleTypographyProps}
+          >
             {title}
           </Typography>
         )}
         {description && (
-          <Typography color="text.secondary" variant={descriptionVariant}>
+          <Typography
+            color="text.secondary"
+            variant={descriptionVariant}
+            {...descriptionTypographyProps}
+          >
             {description}
           </Typography>
         )}
