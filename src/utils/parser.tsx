66c3e1ab360e26d1af57bc8115a1ee7c5e344bf4
alten-dturus <davide.turus@alten.it>
2023-05-29 09:59:34 +0200

    Update parser with new routing system

--- src/utils/parser.tsx
+++ src/utils/parser.tsx
@@ -1,7 +1,4 @@
-import { routes } from '@/router/routes'
 import React from 'react'
-// import ppHtmlJsonFormat from '../static/privacy-policy-it-ast.json'
-// import tosHtmlJsonFormat from '../static/terms-of-service-it-ast.json'
 
 type RootNode = {
   node: 'root'
@@ -22,23 +19,24 @@ type TextNode = {
 
 export type Node = RootNode | ElementNode | TextNode
 
-export const htmlJsonFormatParser = (json: Node): React.ReactNode => {
+export const htmlJsonFormatParser = (json: Node, route: string): React.ReactNode => {
   switch (json.node) {
     case 'root':
-      return <>{json.child.map((item) => htmlJsonFormatParser(item))}</>
+      return <>{json.child.map((item) => htmlJsonFormatParser(item, route))}</>
     case 'element':
       const filteredAttr = { ...json.attr }
       if (filteredAttr.style) delete filteredAttr.style
       if (filteredAttr.class) delete filteredAttr.class
-      if (filteredAttr.href) {
-        if ((filteredAttr.href as string).startsWith('#', 0)) {
-          filteredAttr.href = `it/${routes.TOS.PATH.it}/${filteredAttr.href}`
+      if (filteredAttr.href && (filteredAttr.href as string).startsWith('#', 0)) {
+        if (route.startsWith('/', 0)) {
+          const path = route.substring(1)
+          filteredAttr.href = `${path}/${filteredAttr.href}`
         }
       }
       return React.createElement(
         json.tag,
         { ...filteredAttr, key: Math.random() },
-        json.child ? json.child.map((item) => htmlJsonFormatParser(item)) : undefined
+        json.child ? json.child.map((item) => htmlJsonFormatParser(item, route)) : undefined
       )
     case 'text':
       return json.text
@@ -46,11 +44,3 @@ export const htmlJsonFormatParser = (json: Node): React.ReactNode => {
       break
   }
 }
-
-// export const PrivacyPolicyPage: React.FC = () => {
-//   return <>{htmlJsonFormatParser(ppHtmlJsonFormat as Node)}</>
-// }
-
-// export const TOSPage: React.FC = () => {
-//   return <>{htmlJsonFormatParser(tosHtmlJsonFormat as Node)}</>
-// }
