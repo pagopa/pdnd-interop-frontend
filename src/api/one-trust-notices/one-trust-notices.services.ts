9dd290ec0f8a2ddae346ec3b1d461e1dd493c4f5
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-05-31 15:43:01 +0200

    Moved and renamed html parser function

--- src/api/one-trust-notices/one-trust-notices.services.ts
+++ src/api/one-trust-notices/one-trust-notices.services.ts
@@ -1,17 +1,16 @@
 import { INTEROP_RESOURCES_BASE_URL } from '@/config/env'
-import type { LangCode } from '@/types/common.types'
-import type { RootNode } from '@/utils/parser'
+import type { LangCode, OneTrustContent } from '@/types/common.types'
 import axios from 'axios'
 
 async function getPrivacyPolicyNotice({ lang }: { lang: LangCode }) {
-  const response = await axios.get<RootNode>(
+  const response = await axios.get<OneTrustContent.RootNode>(
     `${INTEROP_RESOURCES_BASE_URL}/one-trust-notices/privacy-policy-${lang}.json`
   )
   return response.data
 }
 
 async function getTermsOfServiceNotice({ lang }: { lang: LangCode }) {
-  const response = await axios.get<RootNode>(
+  const response = await axios.get<OneTrustContent.RootNode>(
     `${INTEROP_RESOURCES_BASE_URL}/one-trust-notices/terms-of-service-${lang}.json`
   )
   return response.data
--- src/utils/common.utils.ts
+++ src/utils/common.utils.ts
@@ -1,5 +1,5 @@
 import type { TopSideActions } from '@/components/layout/containers/PageContainer'
-import type { ActionItem } from '@/types/common.types'
+import type { ActionItem, OneTrustContent } from '@/types/common.types'
 import type { ButtonProps } from '@mui/material'
 import React from 'react'
 
@@ -31,4 +31,57 @@ export function createContext<ContextValue>(name: string, defaultValue: ContextV
     useContext,
     Provider: context.Provider,
   } as const
+}
+
+/**
+ * This function is used to parse the html json to a ReactNode.
+ * We use this approach to avoid the use of dangerouslySetInnerHTML.
+ * @param json the html json
+ * @param route the current route
+ * @returns the ReactNode
+ */
+export const parseHtmlJsonToReactNode = (
+  json: OneTrustContent.Node,
+  route: string
+): React.ReactNode => {
+  function decodeHtml(html: string): string {
+    const txt = document.createElement('textarea')
+    txt.innerHTML = html
+    return txt.value
+  }
+
+  switch (json.node) {
+    case 'root':
+      return React.createElement(
+        React.Fragment,
+        {},
+        json.child.map((item) => parseHtmlJsonToReactNode(item, route))
+      )
+    case 'element':
+      const filteredAttr: Record<string, unknown> = {
+        id: json.attr?.id,
+        href: json.attr?.href,
+        target: json.attr?.target,
+        rel: Array.isArray(json.attr?.rel) ? json.attr?.rel.join(' ') : json.attr?.rel,
+      }
+      /**
+       * During the anchor navigation the root path will be lost.
+       * To avoid that behaviour we put the path behind the anchor in the href
+       */
+      if (filteredAttr.href && (filteredAttr.href as string).startsWith('#', 0)) {
+        if (route.startsWith('/', 0)) {
+          const path = route.substring(1)
+          filteredAttr.href = `${path}${filteredAttr.href}`
+        }
+      }
+      return React.createElement(
+        json.tag,
+        { ...filteredAttr, key: Math.random() },
+        json.child ? json.child.map((item) => parseHtmlJsonToReactNode(item, route)) : undefined
+      )
+    case 'text':
+      return decodeHtml(json.text)
+    default:
+      break
+  }
 }
