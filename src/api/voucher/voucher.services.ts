0db4e9703ded7a3662b86224e56a3dc0a5051ecf
alten-dturus <davide.turus@alten.it>
2023-06-09 15:19:00 +0200

    implemented validateTokenGeneration service and useValidateTokenGeneration hook

---
+++ src/api/voucher/voucher.services.ts
@@ -0,0 +1,21 @@
+import axiosInstance from '@/config/axios'
+import type {
+  TokenGenerationValidationRequest,
+  TokenGenerationValidationResult,
+} from '@/pages/ConsumerDebugVoucherPage/types/debug-voucher.types'
+import { BACKEND_FOR_FRONTEND_URL } from '@/config/env'
+
+async function validateTokenGeneration(payload: TokenGenerationValidationRequest) {
+  const response = await axiosInstance.post<TokenGenerationValidationResult>(
+    `${BACKEND_FOR_FRONTEND_URL}/tools/validateTokenGeneration`,
+    payload
+  )
+
+  return response.data
+}
+
+const VoucherServices = {
+  validateTokenGeneration,
+}
+
+export default VoucherServices
