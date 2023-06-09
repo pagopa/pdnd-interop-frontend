2d104e1d3c4ba58457db4c424dea4faf85969243
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-06-09 16:46:14 +0200

    Added PurposeDetails components tests

---
+++ src/components/shared/PurposeDetails/__tests__/PurposeDetails.test.tsx
@@ -0,0 +1,31 @@
+import { renderWithApplicationContext } from '@/utils/testing.utils'
+import React from 'react'
+import { PurposeDetails, PurposeDetailsSkeleton } from '../PurposeDetails'
+import { createMockPurpose } from '__mocks__/data/purpose.mocks'
+import { render } from '@testing-library/react'
+
+describe('PurposeDetails', () => {
+  it('should match snapshot', () => {
+    const { baseElement } = renderWithApplicationContext(
+      <PurposeDetails purpose={createMockPurpose()} />,
+      {
+        withReactQueryContext: true,
+        withRouterContext: true,
+      }
+    )
+
+    expect(baseElement).toMatchSnapshot()
+  })
+
+  it('should not render if the purpose is falsy', () => {
+    const { container } = render(<PurposeDetails purpose={undefined} />)
+    expect(container).toBeEmptyDOMElement()
+  })
+})
+
+describe('PurposeDetailsSkeleton', () => {
+  it('should match snapshot', () => {
+    const { baseElement } = render(<PurposeDetailsSkeleton />)
+    expect(baseElement).toMatchSnapshot()
+  })
+})
