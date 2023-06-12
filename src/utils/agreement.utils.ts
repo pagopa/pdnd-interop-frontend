59ea242fab4f159ae79c30380f4f88b6866bae0f
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-06-12 11:53:23 +0200

    Update agreement.utils.ts

--- src/utils/agreement.utils.ts
+++ src/utils/agreement.utils.ts
@@ -1,13 +1,32 @@
 import type { Agreement } from '@/api/api.generatedTypes'
 import type { ProviderOrConsumer } from '@/types/common.types'
 
+/**
+ * The agreement can be upgraded if:
+ * - The agreement is in ACTIVE or SUSPENDED state
+ * - The active descriptor is in PUBLISHED or SUSPENDED state
+ * - The active descriptor has a higher version than the agreement
+ * - The user is in the consumer context
+ *
+ * @param agreement The agreement to check
+ * @param mode The actual user context (provider or consumer)
+ * @returns `true` if the agreement can be upgraded, `false` otherwise
+ */
 export const canAgreementBeUpgraded = (agreement: Agreement, mode: ProviderOrConsumer) => {
-  if (mode !== 'consumer') return false
+  const eserviceActiveDescriptor = agreement.eservice.activeDescriptor
+  if (!eserviceActiveDescriptor) return false
 
-  return !!(
-    agreement.eservice.activeDescriptor &&
-    ['PUBLISHED', 'SUSPENDED'].includes(agreement.eservice.activeDescriptor.state) &&
-    agreement.eservice.activeDescriptor.version > agreement.eservice.version &&
-    ['ACTIVE', 'SUSPENDED'].includes(agreement.state)
+  const isConsumer = mode === 'consumer'
+  const isActiveDescriptorPublishedOrSuspended = ['PUBLISHED', 'SUSPENDED'].includes(
+    eserviceActiveDescriptor.state
+  )
+  const hasNewVersion = eserviceActiveDescriptor.version > agreement.eservice.version
+  const isAgreementActiveOrSuspended = ['ACTIVE', 'SUSPENDED'].includes(agreement.state)
+
+  return (
+    isConsumer &&
+    hasNewVersion &&
+    isActiveDescriptorPublishedOrSuspended &&
+    isAgreementActiveOrSuspended
   )
 }
