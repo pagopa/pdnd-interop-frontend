f5f01bc1068ef028e03cd36c3b4d428ec1911a07
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-06-26 17:02:39 +0200

    Update useTOSAgreement.ts

--- src/router/hooks/useTOSAgreement.ts
+++ src/router/hooks/useTOSAgreement.ts
@@ -2,25 +2,25 @@ import React from 'react'
 import { OneTrustNoticesMutations, OneTrustNoticesQueries } from '@/api/one-trust-notices'
 
 export function useTOSAgreement() {
-  const { mutate } = OneTrustNoticesMutations.useAcceptPrivacyNotice()
+  const { mutateAsync } = OneTrustNoticesMutations.useAcceptPrivacyNotice()
 
   const { data: userTOSConsent } = OneTrustNoticesQueries.useGetUserConsent('TOS')
   const { data: userPPConsent } = OneTrustNoticesQueries.useGetUserConsent('PP')
 
-  const hasAcceptedTOS = !userTOSConsent?.firstAccept && userTOSConsent?.isUpdated
-  const hasAcceptedPP = !userPPConsent?.firstAccept && userPPConsent?.isUpdated
+  const hasAcceptedTOS = userTOSConsent?.firstAccept && userTOSConsent?.isUpdated
+  const hasAcceptedPP = userPPConsent?.firstAccept && userPPConsent?.isUpdated
 
-  const hasAcceptedAll = !!(hasAcceptedTOS && hasAcceptedPP)
-
-  console.log(userTOSConsent, userPPConsent, hasAcceptedAll)
+  const isTOSAccepted = !!(hasAcceptedTOS && hasAcceptedPP)
 
   const handleAcceptTOS = React.useCallback(() => {
-    if (hasAcceptedAll) return
+    if (isTOSAccepted) return
     if (!userTOSConsent?.latestVersionId || !userPPConsent?.latestVersionId) return
 
-    mutate({ consentType: 'TOS', latestVersionId: userTOSConsent.latestVersionId })
-    mutate({ consentType: 'PP', latestVersionId: userPPConsent.latestVersionId })
-  }, [hasAcceptedAll, userTOSConsent?.latestVersionId, userPPConsent?.latestVersionId, mutate])
+    Promise.all([
+      mutateAsync({ consentType: 'TOS', latestVersionId: userTOSConsent.latestVersionId }),
+      mutateAsync({ consentType: 'PP', latestVersionId: userPPConsent.latestVersionId }),
+    ])
+  }, [isTOSAccepted, userTOSConsent?.latestVersionId, userPPConsent?.latestVersionId, mutateAsync])
 
-  return { isTOSAccepted: hasAcceptedAll, handleAcceptTOS }
+  return { isTOSAccepted, handleAcceptTOS }
 }
