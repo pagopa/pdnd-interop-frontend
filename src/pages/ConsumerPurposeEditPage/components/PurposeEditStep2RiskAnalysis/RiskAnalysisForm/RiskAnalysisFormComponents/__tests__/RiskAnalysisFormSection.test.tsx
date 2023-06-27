349ced2e7b4b6bac946b3bc0e5ea7c3effe56a08
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-06-27 12:04:37 +0200

    Udpated tests

--- src/pages/ConsumerPurposeEditPage/components/PurposeEditStep2RiskAnalysis/RiskAnalysisForm/RiskAnalysisFormComponents/__tests__/RiskAnalysisFormSection.test.tsx
+++ src/pages/ConsumerPurposeEditPage/components/PurposeEditStep2RiskAnalysis/RiskAnalysisForm/RiskAnalysisFormComponents/__tests__/RiskAnalysisFormSection.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import RiskAnalysisFormSection from '../RiskAnalysisInputWrapper'
+import RiskAnalysisInputWrapper from '../RiskAnalysisInputWrapper'
 import { render } from '@testing-library/react'
 import { Typography } from '@mui/material'
 import { vi } from 'vitest'
 
-describe('RiskAnalysisFormSection testing', () => {
-  it('should be render correclty without description and error', () => {
+describe('RiskAnalysisInputWrapper testing', () => {
+  it('should be render correctly without description and error', () => {
     vi.mock('react-hook-form', async () => ({
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       //@ts-ignore
@@ -18,15 +18,15 @@ describe('RiskAnalysisFormSection testing', () => {
     }))
 
     const screen = render(
-      <RiskAnalysisFormSection title={'Test title'} formFieldName={'test'}>
+      <RiskAnalysisInputWrapper label={'Test title'}>
         <Typography>Test children for snapshot</Typography>
-      </RiskAnalysisFormSection>
+      </RiskAnalysisInputWrapper>
     )
 
     expect(screen.baseElement).toMatchSnapshot()
   })
 
-  it('should be render correclty with description and without error', () => {
+  it('should be render correclty with infoLabel and without error', () => {
     vi.mock('react-hook-form', async () => ({
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       //@ts-ignore
@@ -39,69 +39,44 @@ describe('RiskAnalysisFormSection testing', () => {
     }))
 
     const screen = render(
-      <RiskAnalysisFormSection
-        title={'Test title'}
-        formFieldName={'test'}
-        description="Test description"
-      >
+      <RiskAnalysisInputWrapper label={'Test title'} infoLabel="Test description">
         <Typography>Test children for snapshot</Typography>
-      </RiskAnalysisFormSection>
+      </RiskAnalysisInputWrapper>
     )
 
     expect(screen.baseElement).toMatchSnapshot()
   })
 
-  it('should be render correclty with error and without description', () => {
-    vi.mock('react-hook-form', async () => ({
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      ...(await vi.importActual('react-hook-form')),
-      useFormContext: () => ({
-        formState: {
-          errors: {
-            test: {
-              message: 'test error',
-              type: 'required',
-            },
-          },
-        },
-      }),
-    }))
-
+  it('should be render correclty with error and without infoLabel', () => {
     const screen = render(
-      <RiskAnalysisFormSection title={'Test title'} formFieldName={'test'}>
+      <RiskAnalysisInputWrapper label="Test title" error="test">
         <Typography>Test children for snapshot</Typography>
-      </RiskAnalysisFormSection>
+      </RiskAnalysisInputWrapper>
     )
 
     expect(screen.baseElement).toMatchSnapshot()
   })
 
-  it('should be render correclty with description and error', () => {
-    vi.mock('react-hook-form', async () => ({
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      ...(await vi.importActual('react-hook-form')),
-      useFormContext: () => ({
-        formState: {
-          errors: {
-            test: {
-              message: 'test error',
-              type: 'required',
-            },
-          },
-        },
-      }),
-    }))
-
+  it('should be render correctly with error and infoLabel', () => {
     const screen = render(
-      <RiskAnalysisFormSection
-        title={'Test title'}
-        formFieldName={'test'}
-        description="Test description"
+      <RiskAnalysisInputWrapper label={'Test title'} infoLabel={'test'} error="Test error">
+        <Typography>Test children for snapshot</Typography>
+      </RiskAnalysisInputWrapper>
+    )
+
+    expect(screen.baseElement).toMatchSnapshot()
+  })
+
+  it('should be render correctly with error, infoLabel and helperText', () => {
+    const screen = render(
+      <RiskAnalysisInputWrapper
+        label="Test title"
+        infoLabel="test"
+        error="Test error"
+        helperText="helperText"
       >
         <Typography>Test children for snapshot</Typography>
-      </RiskAnalysisFormSection>
+      </RiskAnalysisInputWrapper>
     )
 
     expect(screen.baseElement).toMatchSnapshot()
