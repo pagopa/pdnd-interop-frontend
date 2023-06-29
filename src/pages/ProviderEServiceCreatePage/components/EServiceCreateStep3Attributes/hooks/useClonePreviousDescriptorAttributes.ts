606b4ce30fb497ec59269e035d6010d7ec6632d3
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-06-29 10:37:42 +0200

    Added no attributes success feedback and no attributes tooltip

---
+++ src/pages/ProviderEServiceCreatePage/components/EServiceCreateStep3Attributes/hooks/useClonePreviousDescriptorAttributes.ts
@@ -0,0 +1,58 @@
+import { remapDescriptorAttributes } from '@/utils/attribute.utils'
+import React from 'react'
+import type { ProducerEServiceDescriptor } from '@/api/api.generatedTypes'
+import { EServiceQueries } from '@/api/eservice'
+import { useToastNotification } from '@/stores'
+import type { UseFormSetValue } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+import type { EServiceCreateStep3FormValues } from '../EServiceCreateStep3Attributes'
+
+export function useClonePreviousDescriptorAttributes(
+  currentDescriptor: ProducerEServiceDescriptor | undefined,
+  setValue: UseFormSetValue<EServiceCreateStep3FormValues>
+) {
+  const { t } = useTranslation('eservice', { keyPrefix: 'create.step3' })
+  const { showToast } = useToastNotification()
+
+  const previousVersionCompactDescriptor = currentDescriptor?.eservice.descriptors.find(
+    (compactDescriptor) =>
+      parseInt(compactDescriptor.version) === parseInt(currentDescriptor.version) - 1
+  )
+
+  const { data: previousVersionDescriptor } = EServiceQueries.useGetDescriptorProvider(
+    currentDescriptor?.eservice.id,
+    previousVersionCompactDescriptor?.id,
+    {
+      suspense: false,
+      enabled: true,
+    }
+  )
+
+  const handleClonePreviousDescriptorAttributes = React.useCallback(() => {
+    if (!previousVersionDescriptor) return
+    const remappedPreviousVersionDescriptorAttributes = remapDescriptorAttributes(
+      previousVersionDescriptor.attributes
+    )
+
+    setValue('attributes.certified', remappedPreviousVersionDescriptorAttributes.certified)
+    setValue('attributes.verified', remappedPreviousVersionDescriptorAttributes.verified)
+    setValue('attributes.declared', remappedPreviousVersionDescriptorAttributes.declared)
+
+    showToast(t('attributeCloneSuccess'), 'success')
+  }, [previousVersionDescriptor, setValue, t, showToast])
+
+  const hasPreviousVersionNoAttributes = React.useMemo(() => {
+    if (!previousVersionDescriptor) return false
+    const remappedPreviousVersionDescriptorAttributes = remapDescriptorAttributes(
+      previousVersionDescriptor.attributes
+    )
+
+    return (
+      !remappedPreviousVersionDescriptorAttributes.certified.length &&
+      !remappedPreviousVersionDescriptorAttributes.verified.length &&
+      !remappedPreviousVersionDescriptorAttributes.declared.length
+    )
+  }, [previousVersionDescriptor])
+
+  return { handleClonePreviousDescriptorAttributes, hasPreviousVersionNoAttributes }
+}
