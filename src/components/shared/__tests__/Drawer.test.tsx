df7696aa7f7ea2672c422415e43a30440be2cf70
alten-dturus <davide.turus@alten.it>
2023-07-04 15:31:42 +0200

    Implemented Drawer tests

---
+++ src/components/shared/__tests__/Drawer.test.tsx
@@ -0,0 +1,53 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+import { Drawer } from '../Drawer'
+import { vi } from 'vitest'
+import { Typography } from '@mui/material'
+
+describe('Drawer test', () => {
+  it('Should not render when isOpen is false', () => {
+    const screen = render(
+      <Drawer isOpen={false} closeAction={vi.fn()} title="test title">
+        <Typography>TEST CHILDREN</Typography>
+      </Drawer>
+    )
+
+    expect(screen.baseElement).toMatchSnapshot()
+  })
+
+  it('Should render correctly without subtitle and button', () => {
+    const screen = render(
+      <Drawer isOpen={true} closeAction={vi.fn()} title="test title">
+        <Typography>TEST CHILDREN</Typography>
+      </Drawer>
+    )
+
+    expect(screen.baseElement).toMatchSnapshot()
+  })
+
+  it('Should render correctly with subtitle and without button', () => {
+    const screen = render(
+      <Drawer isOpen={true} closeAction={vi.fn()} title="test title" subtitle="test subtitle">
+        <Typography>TEST CHILDREN</Typography>
+      </Drawer>
+    )
+
+    expect(screen.baseElement).toMatchSnapshot()
+  })
+
+  it('Should render correctly with subtitle and button', () => {
+    const screen = render(
+      <Drawer
+        isOpen={true}
+        closeAction={vi.fn()}
+        title="test title"
+        subtitle="test subtitle"
+        buttonAction={{ label: 'button label', action: vi.fn() }}
+      >
+        <Typography>TEST CHILDREN</Typography>
+      </Drawer>
+    )
+
+    expect(screen.baseElement).toMatchSnapshot()
+  })
+})
