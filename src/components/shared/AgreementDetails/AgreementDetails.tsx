c5b216d57813d87c4c6dbba0acb0ae986ed7a947
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-07-06 17:38:09 +0200

    Removed ApiInfoSection

--- src/components/shared/AgreementDetails/AgreementDetails.tsx
+++ src/components/shared/AgreementDetails/AgreementDetails.tsx
@@ -7,9 +7,6 @@ import {
 } from './components/AgreementSummarySection'
 import { AgreementRejectedMessageSection } from './components/AgreementRejectedMessageSection'
 import { AgreementUpgradeGuideSection } from './components/AgreementUpgradeGuideSection'
-import { ApiInfoSection, ApiInfoSectionSkeleton } from '../ApiInfoSection'
-import { AgreementQueries } from '@/api/agreement'
-import { useTranslation } from 'react-i18next'
 import {
   AgreementAttributesListSections,
   AgreementAttributesListSectionsSkeleton,
@@ -21,28 +18,12 @@ type AgreementDetailsProps = {
 }
 
 export const AgreementDetails: React.FC<AgreementDetailsProps> = ({ agreementId }) => {
-  const { t } = useTranslation('common', { keyPrefix: 'idLabels' })
-  const { data: agreement } = AgreementQueries.useGetSingle(agreementId)
-
   return (
     <AgreementDetailsContextProvider agreementId={agreementId}>
       <AgreementUpgradeGuideSection />
       <Grid spacing={2} container>
         <Grid item xs={7}>
           <AgreementSummarySection />
-        </Grid>
-        <Grid item xs={5}>
-          {agreement && (
-            <ApiInfoSection
-              ids={[
-                { name: t('eserviceId'), id: agreement.eservice.id },
-                { name: t('descriptorId'), id: agreement.descriptorId },
-                { name: t('agreementId'), id: agreementId },
-                { name: t('providerId'), id: agreement.producer.id },
-                { name: t('consumerId'), id: agreement.consumer.id },
-              ]}
-            />
-          )}
         </Grid>
       </Grid>
 
@@ -59,14 +40,7 @@ export const AgreementDetailsSkeleton: React.FC = () => {
     <Stack sx={{ mt: 6 }} spacing={2}>
       <Grid spacing={2} container>
         <Grid item xs={7}>
-          <Stack spacing={2}>
-            <AgreementSummarySectionSkeleton />
-          </Stack>
-        </Grid>
-        <Grid item xs={5}>
-          <Stack spacing={2}>
-            <ApiInfoSectionSkeleton />
-          </Stack>
+          <AgreementSummarySectionSkeleton />
         </Grid>
       </Grid>
 
