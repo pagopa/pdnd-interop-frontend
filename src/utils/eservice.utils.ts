f368cf36478c63c5bc34031e60ecb960c93680fc
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-07-07 17:23:52 +0200

    Added AgreementAttachedDocumentsDrawer component tests

--- src/utils/eservice.utils.ts
+++ src/utils/eservice.utils.ts
@@ -1,6 +1,6 @@
-import type { EServiceDoc } from '@/api/api.generatedTypes'
+import type { EServiceDoc, Document } from '@/api/api.generatedTypes'
 
-export function getDownloadDocumentName(document: EServiceDoc) {
+export function getDownloadDocumentName(document: EServiceDoc | Document) {
   const filename: string = document.name
   const filenameBits: Array<string> = filename.split('.').filter((b) => b)
   const fileExtension = filenameBits[filenameBits.length - 1]
