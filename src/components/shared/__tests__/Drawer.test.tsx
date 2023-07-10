c365c68b74e733805243b495127e60b44031a796
alten-dturus <davide.turus@alten.it>
2023-07-10 15:18:23 +0200

    Feature: PIN-3581 - Drawer MUI component generalization for interop (#479)

---
+++ src/components/shared/__tests__/Drawer.test.tsx
@@ -0,0 +1,53 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+import { Drawer } from '../Drawer'
+import { vi } from 'vitest'
+import { Typography } from '@mui/material'
+
+describe('Drawer test', () => {
+  it('Should not render when isOpen is false', () => {
+    const { container } = render(
+      <Drawer isOpen={false} onClose={vi.fn()} title="test title">
+        <Typography>TEST CHILDREN</Typography>
+      </Drawer>
+    )
+
+    expect(container).toBeEmptyDOMElement()
+  })
+
+  it('Should render correctly without subtitle and button', () => {
+    const screen = render(
+      <Drawer isOpen={true} onClose={vi.fn()} title="test title">
+        <Typography>TEST CHILDREN</Typography>
+      </Drawer>
+    )
+
+    expect(screen.baseElement).toMatchSnapshot()
+  })
+
+  it('Should render correctly with subtitle and without button', () => {
+    const screen = render(
+      <Drawer isOpen={true} onClose={vi.fn()} title="test title" subtitle="test subtitle">
+        <Typography>TEST CHILDREN</Typography>
+      </Drawer>
+    )
+
+    expect(screen.baseElement).toMatchSnapshot()
+  })
+
+  it('Should render correctly with subtitle and button', () => {
+    const screen = render(
+      <Drawer
+        isOpen={true}
+        onClose={vi.fn()}
+        title="test title"
+        subtitle="test subtitle"
+        buttonAction={{ label: 'button label', action: vi.fn() }}
+      >
+        <Typography>TEST CHILDREN</Typography>
+      </Drawer>
+    )
+
+    expect(screen.baseElement).toMatchSnapshot()
+  })
+})
