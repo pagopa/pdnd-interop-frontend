638d6363bcf911e79f6c8abbda856219f2160a00
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-07-13 15:01:14 +0200

    FIx type errors and updated tests

--- src/components/layout/containers/DocumentContainer.tsx
+++ src/components/layout/containers/DocumentContainer.tsx
@@ -43,7 +43,6 @@ export function DocumentContainer({
   return (
     <Stack sx={sx} direction="row" justifyContent="space-between">
       <InputWrapper
-        name="prettyName"
         sx={{ my: 0, width: '100%' }}
         infoLabel={canEdit ? t('prettyName.infoLabel') : undefined}
       >
