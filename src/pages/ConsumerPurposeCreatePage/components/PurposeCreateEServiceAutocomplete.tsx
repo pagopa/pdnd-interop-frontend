78fb379db0728ca27335e8fbfaa47ecae3184814
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-07-14 14:49:34 +0200

    Feature: PIN-3587 - Fix AutocompleteSingle queries  (#486)
    Before this, if a value was selected in the input field, the suggestion menu would be empty, as if no other options were possible. This fixes it, and now when the input field has a selected value, the suggestion menu shows a default query.
    
    The weird behavior is due to the fact that filtering results happens by querying (with the q parameter) the backend service. When a value is selected, only the selected value is returned from the backend. 
    
    This is a temporary fix, will need to agree a more scalable fix with the backend team

--- src/pages/ConsumerPurposeCreatePage/components/PurposeCreateEServiceAutocomplete.tsx
+++ src/pages/ConsumerPurposeCreatePage/components/PurposeCreateEServiceAutocomplete.tsx
@@ -4,18 +4,40 @@ import { RHFAutocompleteSingle } from '@/components/shared/react-hook-form-input
 import { useTranslation } from 'react-i18next'
 import { useFormContext } from 'react-hook-form'
 import { useAutocompleteTextInput } from '@pagopa/interop-fe-commons'
+import type { CatalogEService } from '@/api/api.generatedTypes'
 
 export const PurposeCreateEServiceAutocomplete: React.FC = () => {
   const { t } = useTranslation('purpose')
+  const selectedEServiceRef = React.useRef<CatalogEService | undefined>(undefined)
+  const hasSetFirstEService = React.useRef(false)
 
   const { setValue } = useFormContext()
-  const hasSetFirstEService = React.useRef(false)
   const [eserviceAutocompleteTextInput, setEserviceAutocompleteTextInput] =
     useAutocompleteTextInput()
 
+  function formatAutocompleteOptionLabel(eservice: CatalogEService) {
+    return `${eservice.name} ${t('edit.eserviceProvider')} ${eservice.producer.name}`
+  }
+
+  /**
+   * TEMP: This is a workaround to avoid the "q" param in the query to be equal to the selected attribute name.
+   */
+  function getQ() {
+    let result = eserviceAutocompleteTextInput
+
+    if (
+      selectedEServiceRef.current &&
+      eserviceAutocompleteTextInput === formatAutocompleteOptionLabel(selectedEServiceRef.current)
+    ) {
+      result = ''
+    }
+
+    return result
+  }
+
   const { data, isInitialLoading } = EServiceQueries.useGetCatalogList(
     {
-      q: eserviceAutocompleteTextInput,
+      q: getQ(),
       agreementStates: ['ACTIVE'],
       // e-service might also be on 'DEPRECATED' state
       states: ['PUBLISHED'],
@@ -27,6 +49,7 @@ export const PurposeCreateEServiceAutocomplete: React.FC = () => {
       onSuccess(eservices) {
         if (!hasSetFirstEService.current && eservices.results.length > 0) {
           setValue('eserviceId', eservices.results[0].id)
+          selectedEServiceRef.current = eservices.results[0]
           hasSetFirstEService.current = true
         }
       },
@@ -34,9 +57,8 @@ export const PurposeCreateEServiceAutocomplete: React.FC = () => {
   )
 
   const eservices = data?.results ?? []
-
   const autocompleteOptions = (eservices ?? []).map((eservice) => ({
-    label: `${eservice.name} ${t('edit.eserviceProvider')} ${eservice.producer.name}`,
+    label: formatAutocompleteOptionLabel(eservice),
     value: eservice.id,
   }))
 
@@ -47,6 +69,9 @@ export const PurposeCreateEServiceAutocomplete: React.FC = () => {
       name="eserviceId"
       label={t('create.eserviceField.label')}
       options={autocompleteOptions}
+      onValueChange={(value) => {
+        selectedEServiceRef.current = eservices.find((eservice) => eservice.id === value?.value)
+      }}
       onInputChange={(_, value) => setEserviceAutocompleteTextInput(value)}
     />
   )
