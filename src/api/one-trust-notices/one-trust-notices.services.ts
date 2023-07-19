b2998e843320e602b7c346522c5d4f61b07821f9
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-07-19 16:29:19 +0200

    Added `getPublicNoticeContent` service

--- src/api/one-trust-notices/one-trust-notices.services.ts
+++ src/api/one-trust-notices/one-trust-notices.services.ts
@@ -1,7 +1,7 @@
 import { BACKEND_FOR_FRONTEND_URL } from '@/config/env'
 import type { ConsentType, PrivacyNotice, PrivacyNoticeSeed } from '../api.generatedTypes'
 import axiosInstance from '@/config/axios'
-import type { OneTrustContent } from '@/types/common.types'
+import type { LangCode, OneTrustContent } from '@/types/common.types'
 
 async function getUserConsent({ consentType }: { consentType: ConsentType }) {
   const response = await axiosInstance.get<PrivacyNotice>(
@@ -17,6 +17,24 @@ async function getNoticeContent({ consentType }: { consentType: ConsentType }) {
   return response.data
 }
 
+/**
+ * This service is used to get directly the content of the notice from the bucket without
+ * relying on the BFF. This is because the BFF is not able to get the content of the notice
+ * for non-logged users, but the notice is public and should be accessible to everyone.
+ */
+async function getPublicNoticeContent({
+  consentType,
+  lang,
+}: {
+  consentType: Lowercase<ConsentType>
+  lang: LangCode
+}) {
+  const response = await axiosInstance.get<OneTrustContent.Node>(
+    `${window.location.origin}/consent/latest/${lang}/${consentType}.json`
+  )
+  return response.data
+}
+
 async function acceptPrivacyNotice({
   consentType,
   ...payload
@@ -27,5 +45,6 @@ async function acceptPrivacyNotice({
 export const OneTrustNoticesServices = {
   getUserConsent,
   getNoticeContent,
+  getPublicNoticeContent,
   acceptPrivacyNotice,
 }
