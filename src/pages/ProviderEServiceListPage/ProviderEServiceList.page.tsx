76bc2356230ca3b2697131a825609b3647508450
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-07-27 14:40:16 +0200

    Removed getTenants and getConsumer query constraints (#509)

--- src/pages/ProviderEServiceListPage/ProviderEServiceList.page.tsx
+++ src/pages/ProviderEServiceListPage/ProviderEServiceList.page.tsx
@@ -25,8 +25,7 @@ const ProviderEServiceListPage: React.FC = () => {
 
   const { data: consumers } = EServiceQueries.useGetConsumers(
     { offset: 0, limit: 50, q: consumersAutocompleteInput },
-    // PIN-3646 - Temporarly avoid fetching consumers if the autocomplete text is less than 3 chars
-    { suspense: false, keepPreviousData: true, enabled: consumersAutocompleteInput.length > 2 }
+    { suspense: false, keepPreviousData: true }
   )
 
   const consumersOptions =
