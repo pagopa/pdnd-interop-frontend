d5d9b06f17ca97d9032fd2f080aa1978d29d50c5
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-08-23 15:07:34 +0200

    Updated tests

--- src/components/layout/SideNav/hooks/__tests__/useGetSideNavItems.test.ts
+++ src/components/layout/SideNav/hooks/__tests__/useGetSideNavItems.test.ts
@@ -2,18 +2,19 @@ import { renderHook } from '@testing-library/react'
 import { useGetSideNavItems } from '../useGetSideNavItems'
 import { mockUseJwt } from '@/utils/testing.utils'
 
-it('Should match the snapshot on empty roles', async () => {
-  mockUseJwt({ currentRoles: [] })
-  const { result } = renderHook(() => useGetSideNavItems())
+describe('useGetSideNavItems', () => {
+  it('Should match the snapshot on empty roles', async () => {
+    mockUseJwt({ currentRoles: [] })
+    const { result } = renderHook(() => useGetSideNavItems())
 
-  expect(result.current).toMatchInlineSnapshot('[]')
-})
+    expect(result.current).toMatchInlineSnapshot('[]')
+  })
 
-it('Should match the snapshot on only admin role', async () => {
-  mockUseJwt({ currentRoles: ['admin'] })
-  const { result } = renderHook(() => useGetSideNavItems())
+  it('Should match the snapshot on only admin role', async () => {
+    mockUseJwt({ currentRoles: ['admin'] })
+    const { result } = renderHook(() => useGetSideNavItems())
 
-  expect(result.current).toMatchInlineSnapshot(`
+    expect(result.current).toMatchInlineSnapshot(`
     [
       {
         "children": [
@@ -52,13 +53,13 @@ it('Should match the snapshot on only admin role', async () => {
       },
     ]
   `)
-})
+  })
 
-it('Should match the snapshot on only api operator role', async () => {
-  mockUseJwt({ currentRoles: ['api'] })
-  const { result } = renderHook(() => useGetSideNavItems())
+  it('Should match the snapshot on only api operator role', async () => {
+    mockUseJwt({ currentRoles: ['api'] })
+    const { result } = renderHook(() => useGetSideNavItems())
 
-  expect(result.current).toMatchInlineSnapshot(`
+    expect(result.current).toMatchInlineSnapshot(`
     [
       {
         "children": [
@@ -93,13 +94,13 @@ it('Should match the snapshot on only api operator role', async () => {
       },
     ]
   `)
-})
+  })
 
-it('Should match the snapshot on only security operator role', async () => {
-  mockUseJwt({ currentRoles: ['security'] })
-  const { result } = renderHook(() => useGetSideNavItems())
+  it('Should match the snapshot on only security operator role', async () => {
+    mockUseJwt({ currentRoles: ['security'] })
+    const { result } = renderHook(() => useGetSideNavItems())
 
-  expect(result.current).toMatchInlineSnapshot(`
+    expect(result.current).toMatchInlineSnapshot(`
     [
       {
         "children": [
@@ -129,58 +130,13 @@ it('Should match the snapshot on only security operator role', async () => {
       },
     ]
   `)
-})
+  })
 
-it('Should match the snapshot on security and api operator roles', async () => {
-  mockUseJwt({ currentRoles: ['security', 'api'] })
-  const { result } = renderHook(() => useGetSideNavItems())
+  it('Should match the snapshot on security and api operator roles', async () => {
+    mockUseJwt({ currentRoles: ['security', 'api'] })
+    const { result } = renderHook(() => useGetSideNavItems())
 
-  expect(result.current).toMatchInlineSnapshot(`
-    [
-      {
-        "children": [
-          "PROVIDE_ESERVICE_LIST",
-          "PROVIDE_AGREEMENT_LIST",
-          "PROVIDE_PURPOSE_LIST",
-        ],
-        "id": "provider",
-        "routeKey": "PROVIDE",
-      },
-      {
-        "children": [
-          "SUBSCRIBE_CATALOG_LIST",
-          "SUBSCRIBE_AGREEMENT_LIST",
-          "SUBSCRIBE_PURPOSE_LIST",
-          "SUBSCRIBE_CLIENT_LIST",
-          "SUBSCRIBE_INTEROP_M2M",
-          "SUBSCRIBE_DEBUG_VOUCHER",
-        ],
-        "id": "subscriber",
-        "routeKey": "SUBSCRIBE",
-      },
-      {
-        "StartIcon": {
-          "$$typeof": Symbol(react.memo),
-          "compare": null,
-          "type": {
-            "$$typeof": Symbol(react.forward_ref),
-            "render": [Function],
-          },
-        },
-        "routeKey": "NOTIFICATION",
-      },
-      {
-        "routeKey": "PARTY_REGISTRY",
-      },
-    ]
-  `)
-})
-
-it('Should match the snapshot on security and admin operator roles', async () => {
-  mockUseJwt({ currentRoles: ['security', 'admin'] })
-  const { result } = renderHook(() => useGetSideNavItems())
-
-  expect(result.current).toMatchInlineSnapshot(`
+    expect(result.current).toMatchInlineSnapshot(`
     [
       {
         "children": [
@@ -219,13 +175,13 @@ it('Should match the snapshot on security and admin operator roles', async () =>
       },
     ]
   `)
-})
+  })
 
-it('Should match the snapshot on api and admin operator roles', async () => {
-  mockUseJwt({ currentRoles: ['api', 'admin'] })
-  const { result } = renderHook(() => useGetSideNavItems())
+  it('Should match the snapshot on security and admin operator roles', async () => {
+    mockUseJwt({ currentRoles: ['security', 'admin'] })
+    const { result } = renderHook(() => useGetSideNavItems())
 
-  expect(result.current).toMatchInlineSnapshot(`
+    expect(result.current).toMatchInlineSnapshot(`
     [
       {
         "children": [
@@ -264,13 +220,13 @@ it('Should match the snapshot on api and admin operator roles', async () => {
       },
     ]
   `)
-})
+  })
 
-it('Should match the snapshot on all roles', async () => {
-  mockUseJwt({ currentRoles: ['api', 'admin'] })
-  const { result } = renderHook(() => useGetSideNavItems())
+  it('Should match the snapshot on api and admin operator roles', async () => {
+    mockUseJwt({ currentRoles: ['api', 'admin'] })
+    const { result } = renderHook(() => useGetSideNavItems())
 
-  expect(result.current).toMatchInlineSnapshot(`
+    expect(result.current).toMatchInlineSnapshot(`
     [
       {
         "children": [
@@ -309,4 +265,57 @@ it('Should match the snapshot on all roles', async () => {
       },
     ]
   `)
+  })
+
+  it('Should match the snapshot on all roles', async () => {
+    mockUseJwt({ currentRoles: ['api', 'admin'] })
+    const { result } = renderHook(() => useGetSideNavItems())
+
+    expect(result.current).toMatchInlineSnapshot(`
+    [
+      {
+        "children": [
+          "PROVIDE_ESERVICE_LIST",
+          "PROVIDE_AGREEMENT_LIST",
+          "PROVIDE_PURPOSE_LIST",
+        ],
+        "id": "provider",
+        "routeKey": "PROVIDE",
+      },
+      {
+        "children": [
+          "SUBSCRIBE_CATALOG_LIST",
+          "SUBSCRIBE_AGREEMENT_LIST",
+          "SUBSCRIBE_PURPOSE_LIST",
+          "SUBSCRIBE_CLIENT_LIST",
+          "SUBSCRIBE_INTEROP_M2M",
+          "SUBSCRIBE_DEBUG_VOUCHER",
+        ],
+        "id": "subscriber",
+        "routeKey": "SUBSCRIBE",
+      },
+      {
+        "StartIcon": {
+          "$$typeof": Symbol(react.memo),
+          "compare": null,
+          "type": {
+            "$$typeof": Symbol(react.forward_ref),
+            "render": [Function],
+          },
+        },
+        "routeKey": "NOTIFICATION",
+      },
+      {
+        "routeKey": "PARTY_REGISTRY",
+      },
+    ]
+  `)
+  })
+
+  it("should not include 'PROVIDE' routes if the user is not an IPA organization", () => {
+    mockUseJwt({ currentRoles: ['admin'], isIPAOrganization: false })
+    const { result } = renderHook(() => useGetSideNavItems())
+
+    expect(result.current).not.toContain('PROVIDE')
+  })
 })
