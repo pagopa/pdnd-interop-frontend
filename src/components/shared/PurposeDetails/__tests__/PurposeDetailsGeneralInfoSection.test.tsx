6f93a510837d517e47e7cc86770f3e640ad21e88
alten-dturus <davide.turus@alten.it>
2023-08-31 14:49:04 +0200

    Feature: PIN-3051 - Remove e-service request in single purpose view (#533)

--- src/components/shared/PurposeDetails/__tests__/PurposeDetailsGeneralInfoSection.test.tsx
+++ src/components/shared/PurposeDetails/__tests__/PurposeDetailsGeneralInfoSection.test.tsx
@@ -4,10 +4,6 @@ import {
   PurposeDetailsGeneralInfoSection,
   PurposeDetailsGeneralInfoSectionSkeleton,
 } from '../PurposeDetailsGeneralInfoSection'
-import { vi } from 'vitest'
-import { EServiceQueries } from '@/api/eservice'
-import type { CatalogEServiceDescriptor } from '@/api/api.generatedTypes'
-import { createMockEServiceDescriptorCatalog } from '@/../__mocks__/data/eservice.mocks'
 import {
   mockUseCurrentRoute,
   mockUseJwt,
@@ -15,20 +11,11 @@ import {
 } from '@/utils/testing.utils'
 import { createMockPurpose } from '@/../__mocks__/data/purpose.mocks'
 
-// EServiceQueries.useGetDescriptorCatalog
-const mockGetDescriptorCatalog = (data: CatalogEServiceDescriptor | undefined) =>
-  vi
-    .spyOn(EServiceQueries, 'useGetDescriptorCatalog')
-    .mockReturnValue({ data } as unknown as ReturnType<
-      typeof EServiceQueries.useGetDescriptorCatalog
-    >)
-
 mockUseJwt()
 
 describe('PurposeDetailsGeneralInfoSection', () => {
   it('should match snapshot (consumer)', () => {
     mockUseCurrentRoute({ mode: 'consumer' })
-    mockGetDescriptorCatalog(createMockEServiceDescriptorCatalog())
 
     const { baseElement } = renderWithApplicationContext(
       <PurposeDetailsGeneralInfoSection purpose={createMockPurpose()} />,
@@ -42,7 +29,6 @@ describe('PurposeDetailsGeneralInfoSection', () => {
 
   it('should match snapshot (provider)', () => {
     mockUseCurrentRoute({ mode: 'provider' })
-    mockGetDescriptorCatalog(createMockEServiceDescriptorCatalog())
 
     const { baseElement } = renderWithApplicationContext(
       <PurposeDetailsGeneralInfoSection purpose={createMockPurpose()} />,
@@ -54,22 +40,8 @@ describe('PurposeDetailsGeneralInfoSection', () => {
     expect(baseElement).toMatchSnapshot()
   })
 
-  it('should not render if the descriptor is falsy', () => {
-    mockGetDescriptorCatalog(undefined)
-
-    const { container } = renderWithApplicationContext(
-      <PurposeDetailsGeneralInfoSection purpose={createMockPurpose()} />,
-      {
-        withRouterContext: true,
-      }
-    )
-
-    expect(container).toBeEmptyDOMElement()
-  })
-
   it("should render freeOfCharge reason field if purpose's freeOfCharge is true", () => {
     mockUseCurrentRoute({ mode: 'provider' })
-    mockGetDescriptorCatalog(createMockEServiceDescriptorCatalog())
 
     const { getByText } = renderWithApplicationContext(
       <PurposeDetailsGeneralInfoSection
