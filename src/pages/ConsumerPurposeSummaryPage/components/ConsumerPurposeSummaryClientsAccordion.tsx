64c9dd029e461ce5650ceb5cc4d355483970b419
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-09-04 17:10:23 +0200

    Added empty state to purpose summary clients accordion

--- src/pages/ConsumerPurposeSummaryPage/components/ConsumerPurposeSummaryClientsAccordion.tsx
+++ src/pages/ConsumerPurposeSummaryPage/components/ConsumerPurposeSummaryClientsAccordion.tsx
@@ -1,8 +1,9 @@
 import type { Purpose } from '@/api/api.generatedTypes'
 import { Link } from '@/router'
-import { Stack } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
 import React from 'react'
 import GroupIcon from '@mui/icons-material/Group'
+import { useTranslation } from 'react-i18next'
 
 type ConsumerPurposeSummaryClientsAccordionProps = {
   purpose: Purpose
@@ -11,6 +12,15 @@ type ConsumerPurposeSummaryClientsAccordionProps = {
 export const ConsumerPurposeSummaryClientsAccordion: React.FC<
   ConsumerPurposeSummaryClientsAccordionProps
 > = ({ purpose }) => {
+  const { t } = useTranslation('purpose', { keyPrefix: 'summary.clientsSection' })
+
+  if (purpose.clients.length === 0)
+    return (
+      <Typography variant="body2" color="text.secondary">
+        {t('empty')}
+      </Typography>
+    )
+
   return (
     <Stack spacing={1} alignItems="start">
       {purpose.clients.map((client) => {
