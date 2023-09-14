f9ce4db7f7dcc46e7846f65f65a149649a83f302
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-09-14 16:34:46 +0200

    Updated snapshots

--- src/components/layout/SideNav/hooks/__tests__/useGetSideNavItems.test.ts
+++ src/components/layout/SideNav/hooks/__tests__/useGetSideNavItems.test.ts
@@ -15,44 +15,44 @@ describe('useGetSideNavItems', () => {
     const { result } = renderHook(() => useGetSideNavItems())
 
     expect(result.current).toMatchInlineSnapshot(`
-    [
-      {
-        "children": [
-          "PROVIDE_ESERVICE_LIST",
-          "PROVIDE_AGREEMENT_LIST",
-          "PROVIDE_PURPOSE_LIST",
-        ],
-        "id": "provider",
-        "routeKey": "PROVIDE",
-      },
-      {
-        "children": [
-          "SUBSCRIBE_CATALOG_LIST",
-          "SUBSCRIBE_AGREEMENT_LIST",
-          "SUBSCRIBE_PURPOSE_LIST",
-          "SUBSCRIBE_CLIENT_LIST",
-          "SUBSCRIBE_INTEROP_M2M",
-          "SUBSCRIBE_DEBUG_VOUCHER",
-        ],
-        "id": "subscriber",
-        "routeKey": "SUBSCRIBE",
-      },
-      {
-        "StartIcon": {
-          "$$typeof": Symbol(react.memo),
-          "compare": null,
-          "type": {
-            "$$typeof": Symbol(react.forward_ref),
-            "render": [Function],
-          },
+      [
+        {
+          "children": [
+            "SUBSCRIBE_CATALOG_LIST",
+            "SUBSCRIBE_AGREEMENT_LIST",
+            "SUBSCRIBE_PURPOSE_LIST",
+            "SUBSCRIBE_CLIENT_LIST",
+            "SUBSCRIBE_INTEROP_M2M",
+            "SUBSCRIBE_DEBUG_VOUCHER",
+          ],
+          "id": "subscriber",
+          "routeKey": "SUBSCRIBE",
         },
-        "routeKey": "NOTIFICATION",
-      },
-      {
-        "routeKey": "PARTY_REGISTRY",
-      },
-    ]
-  `)
+        {
+          "children": [
+            "PROVIDE_ESERVICE_LIST",
+            "PROVIDE_AGREEMENT_LIST",
+            "PROVIDE_PURPOSE_LIST",
+          ],
+          "id": "provider",
+          "routeKey": "PROVIDE",
+        },
+        {
+          "StartIcon": {
+            "$$typeof": Symbol(react.memo),
+            "compare": null,
+            "type": {
+              "$$typeof": Symbol(react.forward_ref),
+              "render": [Function],
+            },
+          },
+          "routeKey": "NOTIFICATION",
+        },
+        {
+          "routeKey": "PARTY_REGISTRY",
+        },
+      ]
+    `)
   })
 
   it('Should match the snapshot on only api operator role', async () => {
@@ -60,40 +60,40 @@ describe('useGetSideNavItems', () => {
     const { result } = renderHook(() => useGetSideNavItems())
 
     expect(result.current).toMatchInlineSnapshot(`
-    [
-      {
-        "children": [
-          "PROVIDE_ESERVICE_LIST",
-          "PROVIDE_AGREEMENT_LIST",
-          "PROVIDE_PURPOSE_LIST",
-        ],
-        "id": "provider",
-        "routeKey": "PROVIDE",
-      },
-      {
-        "children": [
-          "SUBSCRIBE_CATALOG_LIST",
-          "SUBSCRIBE_DEBUG_VOUCHER",
-        ],
-        "id": "subscriber",
-        "routeKey": "SUBSCRIBE",
-      },
-      {
-        "StartIcon": {
-          "$$typeof": Symbol(react.memo),
-          "compare": null,
-          "type": {
-            "$$typeof": Symbol(react.forward_ref),
-            "render": [Function],
-          },
+      [
+        {
+          "children": [
+            "SUBSCRIBE_CATALOG_LIST",
+            "SUBSCRIBE_DEBUG_VOUCHER",
+          ],
+          "id": "subscriber",
+          "routeKey": "SUBSCRIBE",
         },
-        "routeKey": "NOTIFICATION",
-      },
-      {
-        "routeKey": "PARTY_REGISTRY",
-      },
-    ]
-  `)
+        {
+          "children": [
+            "PROVIDE_ESERVICE_LIST",
+            "PROVIDE_AGREEMENT_LIST",
+            "PROVIDE_PURPOSE_LIST",
+          ],
+          "id": "provider",
+          "routeKey": "PROVIDE",
+        },
+        {
+          "StartIcon": {
+            "$$typeof": Symbol(react.memo),
+            "compare": null,
+            "type": {
+              "$$typeof": Symbol(react.forward_ref),
+              "render": [Function],
+            },
+          },
+          "routeKey": "NOTIFICATION",
+        },
+        {
+          "routeKey": "PARTY_REGISTRY",
+        },
+      ]
+    `)
   })
 
   it('Should match the snapshot on only security operator role', async () => {
@@ -137,44 +137,44 @@ describe('useGetSideNavItems', () => {
     const { result } = renderHook(() => useGetSideNavItems())
 
     expect(result.current).toMatchInlineSnapshot(`
-    [
-      {
-        "children": [
-          "PROVIDE_ESERVICE_LIST",
-          "PROVIDE_AGREEMENT_LIST",
-          "PROVIDE_PURPOSE_LIST",
-        ],
-        "id": "provider",
-        "routeKey": "PROVIDE",
-      },
-      {
-        "children": [
-          "SUBSCRIBE_CATALOG_LIST",
-          "SUBSCRIBE_AGREEMENT_LIST",
-          "SUBSCRIBE_PURPOSE_LIST",
-          "SUBSCRIBE_CLIENT_LIST",
-          "SUBSCRIBE_INTEROP_M2M",
-          "SUBSCRIBE_DEBUG_VOUCHER",
-        ],
-        "id": "subscriber",
-        "routeKey": "SUBSCRIBE",
-      },
-      {
-        "StartIcon": {
-          "$$typeof": Symbol(react.memo),
-          "compare": null,
-          "type": {
-            "$$typeof": Symbol(react.forward_ref),
-            "render": [Function],
-          },
+      [
+        {
+          "children": [
+            "SUBSCRIBE_CATALOG_LIST",
+            "SUBSCRIBE_AGREEMENT_LIST",
+            "SUBSCRIBE_PURPOSE_LIST",
+            "SUBSCRIBE_CLIENT_LIST",
+            "SUBSCRIBE_INTEROP_M2M",
+            "SUBSCRIBE_DEBUG_VOUCHER",
+          ],
+          "id": "subscriber",
+          "routeKey": "SUBSCRIBE",
         },
-        "routeKey": "NOTIFICATION",
-      },
-      {
-        "routeKey": "PARTY_REGISTRY",
-      },
-    ]
-  `)
+        {
+          "children": [
+            "PROVIDE_ESERVICE_LIST",
+            "PROVIDE_AGREEMENT_LIST",
+            "PROVIDE_PURPOSE_LIST",
+          ],
+          "id": "provider",
+          "routeKey": "PROVIDE",
+        },
+        {
+          "StartIcon": {
+            "$$typeof": Symbol(react.memo),
+            "compare": null,
+            "type": {
+              "$$typeof": Symbol(react.forward_ref),
+              "render": [Function],
+            },
+          },
+          "routeKey": "NOTIFICATION",
+        },
+        {
+          "routeKey": "PARTY_REGISTRY",
+        },
+      ]
+    `)
   })
 
   it('Should match the snapshot on security and admin operator roles', async () => {
@@ -182,44 +182,44 @@ describe('useGetSideNavItems', () => {
     const { result } = renderHook(() => useGetSideNavItems())
 
     expect(result.current).toMatchInlineSnapshot(`
-    [
-      {
-        "children": [
-          "PROVIDE_ESERVICE_LIST",
-          "PROVIDE_AGREEMENT_LIST",
-          "PROVIDE_PURPOSE_LIST",
-        ],
-        "id": "provider",
-        "routeKey": "PROVIDE",
-      },
-      {
-        "children": [
-          "SUBSCRIBE_CATALOG_LIST",
-          "SUBSCRIBE_AGREEMENT_LIST",
-          "SUBSCRIBE_PURPOSE_LIST",
-          "SUBSCRIBE_CLIENT_LIST",
-          "SUBSCRIBE_INTEROP_M2M",
-          "SUBSCRIBE_DEBUG_VOUCHER",
-        ],
-        "id": "subscriber",
-        "routeKey": "SUBSCRIBE",
-      },
-      {
-        "StartIcon": {
-          "$$typeof": Symbol(react.memo),
-          "compare": null,
-          "type": {
-            "$$typeof": Symbol(react.forward_ref),
-            "render": [Function],
-          },
+      [
+        {
+          "children": [
+            "SUBSCRIBE_CATALOG_LIST",
+            "SUBSCRIBE_AGREEMENT_LIST",
+            "SUBSCRIBE_PURPOSE_LIST",
+            "SUBSCRIBE_CLIENT_LIST",
+            "SUBSCRIBE_INTEROP_M2M",
+            "SUBSCRIBE_DEBUG_VOUCHER",
+          ],
+          "id": "subscriber",
+          "routeKey": "SUBSCRIBE",
         },
-        "routeKey": "NOTIFICATION",
-      },
-      {
-        "routeKey": "PARTY_REGISTRY",
-      },
-    ]
-  `)
+        {
+          "children": [
+            "PROVIDE_ESERVICE_LIST",
+            "PROVIDE_AGREEMENT_LIST",
+            "PROVIDE_PURPOSE_LIST",
+          ],
+          "id": "provider",
+          "routeKey": "PROVIDE",
+        },
+        {
+          "StartIcon": {
+            "$$typeof": Symbol(react.memo),
+            "compare": null,
+            "type": {
+              "$$typeof": Symbol(react.forward_ref),
+              "render": [Function],
+            },
+          },
+          "routeKey": "NOTIFICATION",
+        },
+        {
+          "routeKey": "PARTY_REGISTRY",
+        },
+      ]
+    `)
   })
 
   it('Should match the snapshot on api and admin operator roles', async () => {
@@ -227,44 +227,44 @@ describe('useGetSideNavItems', () => {
     const { result } = renderHook(() => useGetSideNavItems())
 
     expect(result.current).toMatchInlineSnapshot(`
-    [
-      {
-        "children": [
-          "PROVIDE_ESERVICE_LIST",
-          "PROVIDE_AGREEMENT_LIST",
-          "PROVIDE_PURPOSE_LIST",
-        ],
-        "id": "provider",
-        "routeKey": "PROVIDE",
-      },
-      {
-        "children": [
-          "SUBSCRIBE_CATALOG_LIST",
-          "SUBSCRIBE_AGREEMENT_LIST",
-          "SUBSCRIBE_PURPOSE_LIST",
-          "SUBSCRIBE_CLIENT_LIST",
-          "SUBSCRIBE_INTEROP_M2M",
-          "SUBSCRIBE_DEBUG_VOUCHER",
-        ],
-        "id": "subscriber",
-        "routeKey": "SUBSCRIBE",
-      },
-      {
-        "StartIcon": {
-          "$$typeof": Symbol(react.memo),
-          "compare": null,
-          "type": {
-            "$$typeof": Symbol(react.forward_ref),
-            "render": [Function],
-          },
+      [
+        {
+          "children": [
+            "SUBSCRIBE_CATALOG_LIST",
+            "SUBSCRIBE_AGREEMENT_LIST",
+            "SUBSCRIBE_PURPOSE_LIST",
+            "SUBSCRIBE_CLIENT_LIST",
+            "SUBSCRIBE_INTEROP_M2M",
+            "SUBSCRIBE_DEBUG_VOUCHER",
+          ],
+          "id": "subscriber",
+          "routeKey": "SUBSCRIBE",
         },
-        "routeKey": "NOTIFICATION",
-      },
-      {
-        "routeKey": "PARTY_REGISTRY",
-      },
-    ]
-  `)
+        {
+          "children": [
+            "PROVIDE_ESERVICE_LIST",
+            "PROVIDE_AGREEMENT_LIST",
+            "PROVIDE_PURPOSE_LIST",
+          ],
+          "id": "provider",
+          "routeKey": "PROVIDE",
+        },
+        {
+          "StartIcon": {
+            "$$typeof": Symbol(react.memo),
+            "compare": null,
+            "type": {
+              "$$typeof": Symbol(react.forward_ref),
+              "render": [Function],
+            },
+          },
+          "routeKey": "NOTIFICATION",
+        },
+        {
+          "routeKey": "PARTY_REGISTRY",
+        },
+      ]
+    `)
   })
 
   it('Should match the snapshot on all roles', async () => {
@@ -272,44 +272,44 @@ describe('useGetSideNavItems', () => {
     const { result } = renderHook(() => useGetSideNavItems())
 
     expect(result.current).toMatchInlineSnapshot(`
-    [
-      {
-        "children": [
-          "PROVIDE_ESERVICE_LIST",
-          "PROVIDE_AGREEMENT_LIST",
-          "PROVIDE_PURPOSE_LIST",
-        ],
-        "id": "provider",
-        "routeKey": "PROVIDE",
-      },
-      {
-        "children": [
-          "SUBSCRIBE_CATALOG_LIST",
-          "SUBSCRIBE_AGREEMENT_LIST",
-          "SUBSCRIBE_PURPOSE_LIST",
-          "SUBSCRIBE_CLIENT_LIST",
-          "SUBSCRIBE_INTEROP_M2M",
-          "SUBSCRIBE_DEBUG_VOUCHER",
-        ],
-        "id": "subscriber",
-        "routeKey": "SUBSCRIBE",
-      },
-      {
-        "StartIcon": {
-          "$$typeof": Symbol(react.memo),
-          "compare": null,
-          "type": {
-            "$$typeof": Symbol(react.forward_ref),
-            "render": [Function],
-          },
+      [
+        {
+          "children": [
+            "SUBSCRIBE_CATALOG_LIST",
+            "SUBSCRIBE_AGREEMENT_LIST",
+            "SUBSCRIBE_PURPOSE_LIST",
+            "SUBSCRIBE_CLIENT_LIST",
+            "SUBSCRIBE_INTEROP_M2M",
+            "SUBSCRIBE_DEBUG_VOUCHER",
+          ],
+          "id": "subscriber",
+          "routeKey": "SUBSCRIBE",
         },
-        "routeKey": "NOTIFICATION",
-      },
-      {
-        "routeKey": "PARTY_REGISTRY",
-      },
-    ]
-  `)
+        {
+          "children": [
+            "PROVIDE_ESERVICE_LIST",
+            "PROVIDE_AGREEMENT_LIST",
+            "PROVIDE_PURPOSE_LIST",
+          ],
+          "id": "provider",
+          "routeKey": "PROVIDE",
+        },
+        {
+          "StartIcon": {
+            "$$typeof": Symbol(react.memo),
+            "compare": null,
+            "type": {
+              "$$typeof": Symbol(react.forward_ref),
+              "render": [Function],
+            },
+          },
+          "routeKey": "NOTIFICATION",
+        },
+        {
+          "routeKey": "PARTY_REGISTRY",
+        },
+      ]
+    `)
   })
 
   it("should not include 'PROVIDE' routes if the user is not an IPA organization", () => {
