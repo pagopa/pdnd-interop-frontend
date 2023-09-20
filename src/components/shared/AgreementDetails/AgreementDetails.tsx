1bf6e7c35b4c588c8ac124297425127e92b4c9f1
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-09-20 14:26:11 +0200

    Moved e-service details alerts outside grid with 8 columns

--- src/components/shared/AgreementDetails/AgreementDetails.tsx
+++ src/components/shared/AgreementDetails/AgreementDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Grid, Stack } from '@mui/material'
+import { Box, Grid } from '@mui/material'
 import { AgreementDetailsContextProvider } from './AgreementDetailsContext'
 import {
   AgreementSummarySection,
