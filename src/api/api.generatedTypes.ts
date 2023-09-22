89e9d2bcb0c7e8c4788aa7d96d92fb14c9e29c5f
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-09-22 15:19:35 +0200

    Removed `eservice` selection in step 1 form purpose edit

--- src/api/api.generatedTypes.ts
+++ src/api/api.generatedTypes.ts
@@ -702,8 +702,6 @@ export interface CompactClient {
 export interface PurposeUpdateContent {
   title: string
   description: string
-  /** @format uuid */
-  eserviceId: string
   isFreeOfCharge: boolean
   freeOfChargeReason?: string
   riskAnalysisForm?: RiskAnalysisForm
