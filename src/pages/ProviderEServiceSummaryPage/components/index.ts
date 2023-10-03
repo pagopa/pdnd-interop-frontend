3253d9dfabdf020e6bd6e4ac3e7c976a8fcb6c96
alten-dturus <davide.turus@alten.it>
2023-10-03 11:51:11 +0200

    updated index

--- src/pages/ProviderEServiceSummaryPage/components/index.ts
+++ src/pages/ProviderEServiceSummaryPage/components/index.ts
@@ -1,3 +1,4 @@
 export * from './ProviderEServiceGeneralInfoSummary'
 export * from './ProviderEServiceVersionInfoSummary'
 export * from './ProviderEServiceDocumentationSummary'
+export * from './ProviderEServiceRiskAnalysisSummary'
