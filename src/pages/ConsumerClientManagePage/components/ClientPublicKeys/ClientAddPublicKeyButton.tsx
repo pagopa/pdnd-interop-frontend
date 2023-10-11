9b89007126c41323d3bc037e6bf9d822737acd64
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-10-11 15:30:58 +0200

    Feature: PIN-3899 - Redesign single client (#575)
    * Renamed `useDrawerState.tsx` file
    
    * Redesigned voucher instructions
    
    * Fixes
    
    * Redesign client members tab
    
    * Redesigned client keys tab
    
    * Fix voucher steps loading state and small style changes

--- src/pages/ConsumerClientManagePage/components/ClientPublicKeys/ClientAddPublicKeyButton.tsx
+++ src/pages/ConsumerClientManagePage/components/ClientPublicKeys/ClientAddPublicKeyButton.tsx
@@ -1,14 +1,15 @@
 import { ClientQueries, ClientQueryKeys } from '@/api/client'
 import ClientServices from '@/api/client/client.services'
-import { useDialog } from '@/stores'
 import { useQueries } from '@tanstack/react-query'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import identity from 'lodash/identity'
-import { Button, Stack } from '@mui/material'
-import { InfoTooltip, InfoTooltipSkeleton } from '@/components/shared/InfoTooltip'
+import { Button, Stack, Tooltip } from '@mui/material'
 import { ButtonSkeleton } from '@/components/shared/MUI-skeletons'
 import { AuthHooks } from '@/api/auth'
+import PlusOneIcon from '@mui/icons-material/PlusOne'
+import { ClientAddPublicKeyDrawer } from './ClientAddPublicKeyDrawer'
+import { useDrawerState } from '@/hooks/useDrawerState'
 
 interface ClientAddPublicKeyButtonProps {
   clientId: string
@@ -17,9 +18,10 @@ interface ClientAddPublicKeyButtonProps {
 export const ClientAddPublicKeyButton: React.FC<ClientAddPublicKeyButtonProps> = ({ clientId }) => {
   const { t: tCommon } = useTranslation('common')
   const { t } = useTranslation('key')
-  const { openDialog } = useDialog()
   const { jwt, isOperatorSecurity, isAdmin } = AuthHooks.useJwt()
   const { data: users = [] } = ClientQueries.useGetOperatorsList(clientId)
+
+  const { isOpen, openDrawer, closeDrawer } = useDrawerState()
 
   const userQueries = useQueries({
     queries: users.map(({ relationshipId }) => {
@@ -32,28 +34,40 @@ export const ClientAddPublicKeyButton: React.FC<ClientAddPublicKeyButtonProps> =
 
   const usersId = userQueries.map(({ data }) => data?.from).filter(identity)
 
-  const openAddKeyDialog = () => {
-    openDialog({ type: 'addSecurityOperatorKey', clientId })
-  }
-
   const isAdminInClient = Boolean(jwt && usersId.includes(jwt.uid))
   const canAddKey = isOperatorSecurity || (isAdmin && isAdminInClient)
 
   return (
-    <Stack sx={{ mb: 2 }} direction="row" justifyContent="end" alignItems="center" spacing={2}>
-      {isAdmin && !isAdminInClient && <InfoTooltip label={t('list.adminEnableInfo')} />}
-      <Button variant="contained" size="small" onClick={openAddKeyDialog} disabled={!canAddKey}>
-        {tCommon('addBtn')}
-      </Button>
-    </Stack>
+    <>
+      <Stack sx={{ mb: 2 }} direction="row" justifyContent="end" alignItems="center">
+        <Tooltip
+          open={isAdmin && !isAdminInClient ? undefined : false}
+          title={t('list.adminEnableInfo')}
+        >
+          <span>
+            <Button
+              startIcon={<PlusOneIcon />}
+              variant="contained"
+              size="small"
+              disabled={!canAddKey}
+              onClick={openDrawer}
+            >
+              {tCommon('addBtn')}
+            </Button>
+          </span>
+        </Tooltip>
+      </Stack>
+      {canAddKey && (
+        <ClientAddPublicKeyDrawer isOpen={isOpen} onClose={closeDrawer} clientId={clientId} />
+      )}
+    </>
   )
 }
 
 export const ClientAddPublicKeyButtonSkeleton: React.FC = () => {
   return (
     <Stack sx={{ mb: 2 }} direction="row" justifyContent="end" alignItems="center" spacing={2}>
-      <InfoTooltipSkeleton />
-      <ButtonSkeleton size="small" width={103} />
+      <ButtonSkeleton size="small" width={116} />
     </Stack>
   )
 }
