f6300f768024af7c8c332c02cfbb74ee4ec0660d
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-10-11 15:03:51 +0200

    Feature: Fix missing producer menu if user is support (#577)
    * Fix missing producer menu if user is support
    
    * Fix type error
    
    * Updated provider route authorization logic

--- src/api/auth/auth.utils.ts
+++ src/api/auth/auth.utils.ts
@@ -11,7 +11,7 @@ export const parseJwt = memoize((token: string | null | undefined) => {
   const isOperatorAPI = currentRoles.includes('api')
   const isOperatorSecurity = currentRoles.includes('security')
   const isSupport = currentRoles.includes('support')
-  const isIPAOrganization = jwt ? jwt.externalId?.origin === 'IPA' : true
+  const isIPAOrganization = !!(jwt?.externalId && jwt.externalId.origin === 'IPA')
 
   return {
     jwt,
