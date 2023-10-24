00ad60de86634418328501cff8b3002883f12ab6
alten-dturus <davide.turus@alten.it>
2023-10-24 14:05:18 +0200

    Implemented DialogRemoveOperatorFromClient and DialogDeleteOperator
    - implemented dialogs
    - added strings

---
+++ src/components/dialogs/DialogDeleteOperator.tsx
@@ -0,0 +1,63 @@
+import { SELFCARE_BASE_URL } from '@/config/env'
+import { useDialog } from '@/stores'
+import type { DialogDeleteOperatorProps } from '@/types/dialog.types'
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+} from '@mui/material'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router-dom'
+
+export const DialogDeleteOperator: React.FC<DialogDeleteOperatorProps> = ({
+  selfcareId,
+  userId,
+}) => {
+  const ariaLabelId = React.useId()
+  const ariaDescriptionId = React.useId()
+  const { closeDialog } = useDialog()
+  const { t: tCommon } = useTranslation('common', { keyPrefix: 'actions' })
+  const { t } = useTranslation('shared-components', { keyPrefix: 'DialogDeleteOperator' })
+  const navigate = useNavigate()
+
+  const selfcareUserPageUrl = `${SELFCARE_BASE_URL}/dashboard/${selfcareId}/users/${userId}`
+
+  const handleCancel = () => {
+    closeDialog()
+  }
+
+  const handleProceed = () => {
+    navigate(selfcareUserPageUrl)
+    closeDialog()
+  }
+
+  return (
+    <Dialog
+      open
+      onClose={handleCancel}
+      aria-labelledby={ariaLabelId}
+      {...{ 'aria-describedby': ariaDescriptionId }}
+      maxWidth="md"
+      fullWidth
+    >
+      <DialogTitle id={ariaLabelId}>{t('title')}</DialogTitle>
+
+      <DialogContent aria-describedby={ariaDescriptionId}>
+        <Typography variant="body1">{t('description')}</Typography>
+      </DialogContent>
+
+      <DialogActions>
+        <Button variant="outlined" onClick={handleCancel}>
+          {tCommon('cancel')}
+        </Button>
+        <Button variant="contained" onClick={handleProceed}>
+          {tCommon('confirm')}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
