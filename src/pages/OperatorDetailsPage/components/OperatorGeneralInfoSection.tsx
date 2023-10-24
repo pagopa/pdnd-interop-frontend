0c881277a3461ac3de7cc6f0cef76ef439233f2c
alten-dturus <davide.turus@alten.it>
2023-10-24 14:10:00 +0200

    Removed OperatorKeysSection component and updated design for OperatorGeneralInfoSection

--- src/pages/OperatorDetailsPage/components/OperatorGeneralInfoSection.tsx
+++ src/pages/OperatorDetailsPage/components/OperatorGeneralInfoSection.tsx
@@ -5,6 +5,9 @@ import { useTranslation } from 'react-i18next'
 import { InformationContainer } from '@pagopa/interop-fe-commons'
 import { SectionContainer, SectionContainerSkeleton } from '@/components/layout/containers'
 import type { UserProductRole } from '@/types/party.types'
+import OpenInNewIcon from '@mui/icons-material/OpenInNew'
+import { useClientKind } from '@/hooks/useClientKind'
+import { useNavigate, useParams } from '@/router'
 
 interface OperatorGeneralInfoSectionProps {
   operatorId: string
@@ -15,6 +18,11 @@ export const OperatorGeneralInfoSection: React.FC<OperatorGeneralInfoSectionProp
 }) => {
   const { t } = useTranslation('user', { keyPrefix: 'edit' })
   const { t: tCommon } = useTranslation('common')
+  const clientKind = useClientKind()
+  const { clientId: clientId } = useParams<
+    'SUBSCRIBE_INTEROP_M2M_CLIENT_OPERATOR_EDIT' | 'SUBSCRIBE_CLIENT_OPERATOR_EDIT'
+  >()
+  const navigate = useNavigate()
 
   const { data: operator } = ClientQueries.useGetSingleOperator(operatorId)
 
@@ -22,8 +30,33 @@ export const OperatorGeneralInfoSection: React.FC<OperatorGeneralInfoSectionProp
     return null
   }
 
+  const backToOperatorsListRouteKey =
+    clientKind === 'API' ? 'SUBSCRIBE_INTEROP_M2M_CLIENT_EDIT' : 'SUBSCRIBE_CLIENT_EDIT'
+
+  const handleGoToOperatorKeys = () => {
+    const relationshipIdsActiveFilter = [[`${operator.name} ${operator.familyName}`, operator.id]]
+    navigate(backToOperatorsListRouteKey, {
+      params: { clientId },
+      urlParams: {
+        tab: 'publicKeys',
+        relationshipIds: JSON.stringify(relationshipIdsActiveFilter),
+      },
+    })
+  }
+
   return (
-    <SectionContainer title={t('generalInformations')}>
+    <SectionContainer
+      newDesign
+      title={t('generalInformations')}
+      bottomActions={[
+        {
+          label: t('operatorKeysLink.label'),
+          startIcon: <OpenInNewIcon />,
+          component: 'button',
+          onClick: handleGoToOperatorKeys,
+        },
+      ]}
+    >
       <Stack spacing={2}>
         <InformationContainer
           label={t('taxCodeField.label')}
