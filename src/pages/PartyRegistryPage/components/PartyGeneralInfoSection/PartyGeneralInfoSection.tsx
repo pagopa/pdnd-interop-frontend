c5805c2a5dc9ceab46e8cb3e92d49d324acd45b8
diego-dxc <diego.longo2@dxc.com>
2024-01-26 10:07:46 +0100

    Created general info section

---
+++ src/pages/PartyRegistryPage/components/PartyGeneralInfoSection/PartyGeneralInfoSection.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { SectionContainer, SectionContainerSkeleton } from '@/components/layout/containers'
+import { useTranslation } from 'react-i18next'
+import { Grid, Stack } from '@mui/material'
+import { PartyQueries } from '@/api/party/party.hooks'
+import { InformationContainer } from '@pagopa/interop-fe-commons'
+import { formatDateString } from '@/utils/format.utils'
+
+export const PartyGeneralInfoSection: React.FC = () => {
+  const { t } = useTranslation('party', { keyPrefix: 'generalInfo' })
+
+  const { data: user } = PartyQueries.useGetActiveUserParty()
+  const onBoardedAtFormatted = user?.onboardedAt ? formatDateString(user?.onboardedAt) : 'n/a'
+
+  return (
+    <Grid container>
+      <Grid item xs={8}>
+        <SectionContainer title={t('title')}>
+          <Stack spacing={2}>
+            <InformationContainer
+              label={t('onBoardingDateField.label')}
+              content={onBoardedAtFormatted}
+            />
+            {user?.subUnitType && (
+              <InformationContainer
+                label={t('subUnitTypeField.label')}
+                content={user?.subUnitType}
+              />
+            )}
+          </Stack>
+        </SectionContainer>
+      </Grid>
+    </Grid>
+  )
+}
+
+export const PartyContactsSectionSkeleton: React.FC = () => {
+  return (
+    <Grid container>
+      <Grid item xs={8}>
+        <SectionContainerSkeleton height={175} />
+      </Grid>
+    </Grid>
+  )
+}
