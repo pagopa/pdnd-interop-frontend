2f362f0b489c17d2e6ba970aadaa039e996d19b3
alten-dturus <davide.turus@alten.it>
2024-01-31 10:59:20 +0100

    Updated tests and snapshots

--- src/components/layout/__tests__/AppLayout.test.tsx
+++ src/components/layout/__tests__/AppLayout.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { AppLayout } from '../AppLayout'
-import { mockUseJwt, renderWithApplicationContext } from '@/utils/testing.utils'
+import {
+  mockUseGetActiveUserParty,
+  mockUseJwt,
+  renderWithApplicationContext,
+} from '@/utils/testing.utils'
 import { vi } from 'vitest'
 
 mockUseJwt()
+mockUseGetActiveUserParty()
 
 vi.mock('../Breadcrumbs', () => {
   return { Breadcrumbs: () => <></> }
@@ -13,6 +18,7 @@ describe('AppLayout', () => {
   it('should match snapshot', () => {
     const screen = renderWithApplicationContext(<AppLayout>{}</AppLayout>, {
       withRouterContext: true,
+      withReactQueryContext: true,
     })
     expect(screen.baseElement).toMatchSnapshot()
   })
