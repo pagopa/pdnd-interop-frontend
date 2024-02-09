bce8dba51f60cc87faed6f9908cefc20eff13919
Carmine Porricelli <carmine.porricelli@pagopa.it>
2024-02-09 16:39:10 +0100

    Reformat script

--- scripts/generate-interop-token.js
+++ scripts/generate-interop-token.js
@@ -24,6 +24,15 @@ import _ from 'lodash'
 import { VerifyCommand, KMSClient, SignCommand } from '@aws-sdk/client-kms'
 import { readFileSync, write, writeFileSync } from 'fs'
 
+const config = {
+  kms: {
+    kid: null,
+    alg: 'RSASSA_PKCS1_V1_5_SHA_256',
+  },
+}
+
+const kmsClient = new KMSClient()
+
 const SESSION_TOKENS_DURATION_SECONDS = 60 * 60 * 24 // 24 hours
 const ENV_FILE_PATH = '.env.development.local'
 const ENV_TOKEN_KEY = 'REACT_APP_MOCK_TOKEN'
@@ -517,52 +526,26 @@ if (!possibleRoles.includes(chosenRole)) {
 
 console.log(`> Generating token for ${chosenTenant} tenant with role ${chosenRole}...`)
 
-const tenant = {
-  [chosenTenant]: {
-    [chosenRole]: ALL_TENANTS[chosenTenant][chosenRole],
-  },
-}
+const token = await generateSessionToken(chosenTenant, chosenRole)
 
-const sessionTokenHeaderTemplate = {
-  typ: 'at+jwt',
-  alg: 'WELL_KNOWN_ALG',
-  use: 'sig',
-  kid: 'WELL_KNOWN_KID',
-}
-const sessionTokenPayloadTemplate = {
-  externalId: {
-    origin: 'VALUES_EXT_ID_ORIGIN',
-    value: 'VALUES_EXT_ID_VALUE',
-  },
-  'user-roles': 'VALUES_USER_ROLES',
-  selfcareId: 'VALUES_SELFCARE_ID',
-  organizationId: 'VALUES_ORG_ID',
-  uid: 'VALUES_UID',
-  iss: '{{ENVIRONMENT}}.interop.pagopa.it',
-  aud: '{{ENVIRONMENT}}.interop.pagopa.it/ui',
-  nbf: 123,
-  iat: 123,
-  exp: 456,
-  jti: 'uuid',
-}
+console.log(`> Token retrieved: \n${token}\n\n`)
+console.log(`> Writing token to ${ENV_FILE_PATH}...`)
 
-const kmsClient = new KMSClient()
+const envFile = readFileSync(ENV_FILE_PATH, 'utf8')
+  .split('\n')
+  .map((line) => (line.startsWith(ENV_TOKEN_KEY) ? `${ENV_TOKEN_KEY}=${token}` : line))
+  .join('\n')
 
-const config = {
-  kms: {
-    kid: null,
-    alg: 'RSASSA_PKCS1_V1_5_SHA_256',
-  },
-}
+writeFileSync(ENV_FILE_PATH, envFile)
+
+console.log('> Done!')
 
 /**
  *
  * @param {*} stPayloadValuesFilePath File contenente i payload values dei tenant per cui bisogna generare i Session Token
  * @returns stOutputTokens SessionTokens JSON Object - A token for each tenant/role
  */
-async function generateSessionTokens() {
-  logInfo('## Step 1. Parse well known ##')
-
+async function generateSessionToken(tenant, role) {
   const wellKnownUrl = new URL(process.env.REMOTE_WELLKNOWN_URL)
   const { kid, alg } = await fetchWellKnown(
     wellKnownUrl.protocol.indexOf('https') >= 0,
@@ -573,31 +556,40 @@ async function generateSessionTokens() {
     return
   }
   config.kms.kid = kid
-  logInfo(`\tGot kid ${kid} and alg ${alg}`)
-
   // Step 3. Generate STs header - Populate Session Token header from template
-  logInfo('## Step 2. Generate STs header - Populate Session Token header from template ##')
+  const sessionTokenHeaderTemplate = {
+    typ: 'at+jwt',
+    alg: 'WELL_KNOWN_ALG',
+    use: 'sig',
+    kid: 'WELL_KNOWN_KID',
+  }
   const stHeaderCompiled = Object.assign({}, sessionTokenHeaderTemplate, {
     kid,
     alg,
   })
-  logInfo(`\tST Header Compiled: ${JSON.stringify(stHeaderCompiled)}`)
 
   // Step 4. Generate STs payload
-  logInfo('## Step 3. Generate STs payload ##')
-  logInfo('\tDefine time in seconds since epoch')
   const epochTimeSeconds = Math.round(new Date().getTime() / 1000)
-  logInfo(`\tTime in seconds since epoch: ${epochTimeSeconds}`)
-
-  logInfo('\tDefine token expiration time in seconds')
   const epochTimeExpSeconds = epochTimeSeconds + Number(SESSION_TOKENS_DURATION_SECONDS)
-  logInfo(`\tExpiration Time in seconds: ${epochTimeExpSeconds}`)
-
-  logInfo('\tDefine random UUID')
   const randomUUID = uuidv4()
-  logInfo(`\tRandom UUID:: ${randomUUID}`)
 
-  logInfo(`Populate Session Token payload from template`)
+  const sessionTokenPayloadTemplate = {
+    externalId: {
+      origin: 'VALUES_EXT_ID_ORIGIN',
+      value: 'VALUES_EXT_ID_VALUE',
+    },
+    'user-roles': 'VALUES_USER_ROLES',
+    selfcareId: 'VALUES_SELFCARE_ID',
+    organizationId: 'VALUES_ORG_ID',
+    uid: 'VALUES_UID',
+    iss: '{{ENVIRONMENT}}.interop.pagopa.it',
+    aud: '{{ENVIRONMENT}}.interop.pagopa.it/ui',
+    nbf: 123,
+    iat: 123,
+    exp: 456,
+    jti: 'uuid',
+  }
+
   let stPayloadCompiled = Object.assign({}, sessionTokenPayloadTemplate, {
     nbf: epochTimeSeconds,
     iat: epochTimeSeconds,
@@ -607,20 +599,14 @@ async function generateSessionTokens() {
   stPayloadCompiled = JSON.parse(
     JSON.stringify(stPayloadCompiled).replaceAll('{{ENVIRONMENT}}', 'dev')
   )
-  logInfo(`\tST Payload Compiled: ${JSON.stringify(stPayloadCompiled)}`)
 
-  logInfo('## Step 4. Generate unsigned STs ##')
-  const unsignedSTs = unsignedStsGeneration(stHeaderCompiled, stPayloadCompiled, tenant)
-  logInfo(`\tUnsigned STs: ${JSON.stringify(unsignedSTs)}`)
+  const unsignedSTs = unsignedStsGeneration(stHeaderCompiled, stPayloadCompiled, {
+    [tenant]: {
+      [role]: ALL_TENANTS[tenant][role],
+    },
+  })
 
-  logInfo('## Step 5. Generate signed STs ##')
-  const stOutputTokens = await signedStsGeneration(unsignedSTs)
-
-  return stOutputTokens
-}
-
-function logInfo(message) {
-  // console.log(message)
+  return (await signedStsGeneration(unsignedSTs))[tenant][role]
 }
 
 async function fetchWellKnown(isSecure, wellKnownUrl) {
@@ -681,14 +667,10 @@ async function fetchWellKnown(isSecure, wellKnownUrl) {
 // Generate signed tokens from unsigned ones
 async function signedStsGeneration(unsignedStValues) {
   const signedTokens = {}
-  logInfo(`SignedTokenGeneration::START`)
   for (const tenant of Object.keys(unsignedStValues)) {
-    logInfo(`\tBuilding token for tenant ${tenant}`)
     signedTokens[tenant] = {}
 
     for (const tenantRole of Object.keys(unsignedStValues[tenant])) {
-      logInfo(`\tBuilding token for role ${tenantRole}`)
-
       const currentUnsignedJwt = unsignedStValues[tenant][tenantRole]
       const { signedToken, signature } = await kmsSign(currentUnsignedJwt)
 
@@ -699,21 +681,16 @@ async function signedStsGeneration(unsignedStValues) {
       signedTokens[tenant][tenantRole] = signedToken
     }
   }
-  logInfo(`SignedTokenGeneration::END`)
-
   return signedTokens
 }
 
 function unsignedStsGeneration(stHeaderCompiled, stPayloadCompiled, stPayloadValues) {
   try {
-    logInfo(`unsignedStsGeneration::Phase1:START: Build roles dynamic substitutions`)
     const stsSubOutput = {}
     for (const tenant of Object.keys(stPayloadValues)) {
-      logInfo(`\tunsignedStsGeneration::Phase1: Build roles substitutions for tenant ${tenant}`)
       stsSubOutput[tenant] = {}
 
       for (const interopRole of Object.keys(stPayloadValues[tenant])) {
-        logInfo(`\tunsignedStsGeneration::Phase1: Start dynamic substition for role ${interopRole}`)
         stsSubOutput[tenant][interopRole] = Object.assign(
           {},
           stPayloadCompiled,
@@ -721,30 +698,18 @@ function unsignedStsGeneration(stHeaderCompiled, stPayloadCompiled, stPayloadVal
         )
       }
     }
-    logInfo(`unsignedStsGeneration::Phase1:END: Build roles dynamic substitutions`)
-
-    logInfo(
-      `unsignedStsGeneration::Phase2:START: Build base64 header and body for each tenant/role`
-    )
     const base64Header = b64UrlEncode(JSON.stringify(stHeaderCompiled))
-    logInfo('\tunsignedStsGeneration::Phase2: Build base64 header done')
-
     const stOutputIntermediate = {}
     for (const tenant of Object.keys(stsSubOutput)) {
-      logInfo(`\tunsignedStsGeneration::Phase2: Build partial JWT for ${tenant}`)
       stOutputIntermediate[tenant] = {}
 
       for (const interopRole of Object.keys(stsSubOutput[tenant])) {
-        logInfo(
-          `\tunsignedStsGeneration::Phase2: Build partial JWT for tenant ${tenant} role ${interopRole}`
-        )
         const base64Role = b64UrlEncode(JSON.stringify(stsSubOutput[tenant][interopRole]))
         const poJwtForRole = `${base64Header}.${base64Role}`
 
         stOutputIntermediate[tenant][interopRole] = poJwtForRole
       }
     }
-    logInfo(`unsignedStsGeneration::Phase2:END: Build base64 header and body for each tenant/role`)
 
     return stOutputIntermediate
   } catch (ex) {
@@ -822,17 +787,3 @@ function b64UrlEncode(str) {
 function bufferB64UrlEncode(b) {
   return b.toString('base64').replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
 }
-
-const token = (await generateSessionTokens())[chosenTenant][chosenRole]
-
-console.log(`> Token retrieved: \n${token}\n\n`)
-console.log(`> Writing token to ${ENV_FILE_PATH}...`)
-
-const envFile = readFileSync(ENV_FILE_PATH, 'utf8')
-  .split('\n')
-  .map((line) => (line.startsWith(ENV_TOKEN_KEY) ? `${ENV_TOKEN_KEY}=${token}` : line))
-  .join('\n')
-
-writeFileSync(ENV_FILE_PATH, envFile)
-
-console.log('> Done!')
