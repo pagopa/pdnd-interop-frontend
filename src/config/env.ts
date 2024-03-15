fcbfaf06736ffb20689a0405e9b8d6ec40cf7aa3
alten-dturus <davide.turus@alten.it>
2024-03-15 15:07:41 +0100

    removed mock in env STAGE

--- src/config/env.ts
+++ src/config/env.ts
@@ -62,7 +62,6 @@ export const WELL_KNOWN_URLS =
 export const SELFCARE_BASE_URL =
   isProduction && PAGOPA_ENV ? PAGOPA_ENV.SELFCARE_BASE_URL : 'https://uat.selfcare.pagopa.it'
 
-// export const STAGE = PAGOPA_ENV?.STAGE ?? 'DEV'
-export const STAGE = 'CERT' // TODO remove because is to test without BE
+export const STAGE = PAGOPA_ENV?.STAGE ?? 'DEV'
 
 export const SELFCARE_INTEROP_PROD_ID = `prod-interop${STAGE === 'UAT' ? '-coll' : ''}`
