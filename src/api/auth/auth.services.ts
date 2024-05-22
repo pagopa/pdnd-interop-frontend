616c44c2fa775c814ad38e5315b388ee7dd1eedd
alten-dturus <davide.turus@alten.it>
2024-05-22 14:52:25 +0200

    Added identifier and defaultProps for mixpanel event tracking

--- src/api/auth/auth.services.ts
+++ src/api/auth/auth.services.ts
@@ -4,7 +4,9 @@ import axios from 'axios'
 import type { SAMLTokenRequest, SessionToken } from '../api.generatedTypes'
 import { MOCK_TOKEN, STORAGE_KEY_SESSION_TOKEN } from '@/config/constants'
 import { TokenExchangeError } from '@/utils/errors.utils'
-import { isJwtExpired } from './auth.utils'
+import { isJwtExpired, parseJwt } from './auth.utils'
+import { setMixpanelIdentifier } from '@/config/tracking'
+import { hasSessionExpired } from '@/utils/common.utils'
 
 async function swapTokens(identity_token: string) {
   const response = await axiosInstance.post<{ session_token: string }>(
@@ -17,11 +19,13 @@ async function swapTokens(identity_token: string) {
 async function getSessionToken(): Promise<string | null> {
   const resolveToken = (sessionToken: string) => {
     // Check if session has expired. In that case, we need to remove token from localStorage
-    if (isJwtExpired(sessionToken)) {
+    const parsedJwt = parseJwt(sessionToken)
+    if (hasSessionExpired(parsedJwt.jwt?.exp)) {
       window.localStorage.removeItem(STORAGE_KEY_SESSION_TOKEN)
       return null
     }
     window.localStorage.setItem(STORAGE_KEY_SESSION_TOKEN, sessionToken)
+    if (parsedJwt.jwt) setMixpanelIdentifier(parsedJwt.jwt?.selfcareId) // TODO quale è l'id dell'utente autenticato?
     return sessionToken
   }
 
