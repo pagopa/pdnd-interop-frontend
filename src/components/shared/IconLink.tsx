096dddef4c7fdae7f8defb31f114428ef0cd3c4e
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2024-07-25 16:39:20 +0200

    Added link to OpenAPI checker

--- src/components/shared/IconLink.tsx
+++ src/components/shared/IconLink.tsx
@@ -5,6 +5,7 @@ type IconLinkProps<D extends React.ElementType = 'a'> = {
   startIcon?: React.ReactNode
   endIcon?: React.ReactNode
   children: React.ReactNode
+  inline?: boolean
   component?: D
 } & LinkProps<D>
 
@@ -12,18 +13,21 @@ export const IconLink = <D extends React.ElementType = 'a'>({
   startIcon,
   endIcon,
   children,
+  inline,
   ...linkProps
 }: IconLinkProps<D>) => {
   return (
     <Link variant="body2" underline="hover" {...linkProps}>
       <Stack
-        gap={1}
+        gap={0.25}
         flexDirection="row"
         justifyContent="center"
         alignItems="center"
         component="span"
         sx={{
           cursor: 'pointer',
+          textDecoration: 'inherit',
+          display: inline ? 'inline-flex' : 'flex',
         }}
       >
         {startIcon} <span>{children}</span> {endIcon}
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -32,3 +32,4 @@ export const clientKeyGuideLink = `${documentationLink}/manuale-operativo/client
 export const generateKeyGuideLink = `${documentationLink}/manuale-operativo/generare-e-caricare-chiavi-di-sicurezza`
 export const payloadVerificationGuideLink = `${documentationLink}/manuale-operativo/utilizzare-i-voucher#verifiche-sul-payload`
 export const apiGuideLink = `${documentationLink}/manuale-operativo/api-esposte-da-pdnd-interoperabilita`
+export const openApiCheckerLink = 'https://italia.github.io/api-oas-checker/'
