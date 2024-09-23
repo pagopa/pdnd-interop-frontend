1f37d734318d694318a334f03f0cd096e3334443
martinaCampoli <164033351+martinaCampoli@users.noreply.github.com>
2024-09-23 11:30:40 +0200

    Feature/pin 5124 replace purpose client dialog with drawer (#885)
    * Replaced DialogAddClientToPurpose with PurposeAddClientDrawer
    
    * Deleted files and references to DialogAddClientToPurpose
    
    * Update PurposeAddClientDrawer.tsx
    
    * Update PurposeAddClientDrawer.tsx
    
    * Update PurposeAddClientDrawer.tsx
    
    ---------
    
    Co-authored-by: Carmine Porricelli <carmine.porricelli@pagopa.it>

--- src/components/dialogs/Dialog.tsx
+++ src/components/dialogs/Dialog.tsx
@@ -3,7 +3,6 @@ import { DialogBasic } from './DialogBasic'
 import { DialogAttributeDetails } from './DialogAttributeDetails'
 import { DialogSessionExpired } from './DialogSessionExpired'
 import type {
-  DialogAddClientToPurposeProps,
   DialogAttributeDetailsProps,
   DialogBasicProps,
   DialogDeleteOperatorProps,
@@ -18,7 +17,6 @@ import type {
   DialogSetTenantMailProps,
 } from '@/types/dialog.types'
 import { DialogRejectAgreement } from './DialogRejectAgreement'
-import { DialogAddClientToPurpose } from './DialogAddClientToPurpose'
 import { ErrorBoundary } from '../shared/ErrorBoundary'
 import { DialogError } from './DialogError'
 import { useDialogStore } from '@/stores'
@@ -35,7 +33,6 @@ function match<T>(
   onShowAttributeDetails: (props: DialogAttributeDetailsProps) => T,
   onShowSessionExpired: (props: DialogSessionExpiredProps) => T,
   onRejectAgreement: (props: DialogRejectAgreementProps) => T,
-  onAddClientToPurpose: (props: DialogAddClientToPurposeProps) => T,
   onUpgradeAgreementVersion: (props: DialogUpgradeAgreementVersionProps) => T,
   onDeleteOperator: (props: DialogDeleteOperatorProps) => T,
   onRemoveOperatorFromClient: (props: DialogRemoveOperatorFromClientProps) => T,
@@ -54,8 +51,6 @@ function match<T>(
         return onShowSessionExpired(props)
       case 'rejectAgreement':
         return onRejectAgreement(props)
-      case 'addClientToPurpose':
-        return onAddClientToPurpose(props)
       case 'upgradeAgreementVersion':
         return onUpgradeAgreementVersion(props)
       case 'deleteOperator':
@@ -79,7 +74,6 @@ const _Dialog = match(
   (props) => <DialogAttributeDetails {...props} />,
   (props) => <DialogSessionExpired {...props} />,
   (props) => <DialogRejectAgreement {...props} />,
-  (props) => <DialogAddClientToPurpose {...props} />,
   (props) => <DialogUpgradeAgreementVersion {...props} />,
   (props) => <DialogDeleteOperator {...props} />,
   (props) => <DialogRemoveOperatorFromClient {...props} />,
