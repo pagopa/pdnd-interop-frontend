4a96f3c8bea139d9cd90e23cea128279862d34c2
alten-dturus <davide.turus@alten.it>
2024-09-23 10:45:03 +0200

    Updated api.generatedTypes from a new path in script

--- scripts/open-api-type-generator.js
+++ scripts/open-api-type-generator.js
@@ -1,8 +1,12 @@
 import { generateApi } from 'swagger-typescript-api'
 import path from 'path'
 
+// const openApiSpecificationFileUrl =
+//   'https://raw.githubusercontent.com/pagopa/interop-be-backend-for-frontend/1.0.x/src/main/resources/interface-specification.yml'
+
+// TODO remove this and decomment the line below
 const openApiSpecificationFileUrl =
-  'https://raw.githubusercontent.com/pagopa/interop-be-backend-for-frontend/1.0.x/src/main/resources/interface-specification.yml'
+  'https://raw.githubusercontent.com/pagopa/interop-be-monorepo/main/packages/api-clients/open-api/bffApi.yml'
 
 const apiFolderPath = path.resolve('./src/api/')
 
