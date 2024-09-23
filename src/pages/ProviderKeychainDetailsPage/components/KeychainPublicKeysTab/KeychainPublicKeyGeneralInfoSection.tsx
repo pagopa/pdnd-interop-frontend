6d84f048c8eee235c4539744b73d0387d07f83f8
alten-dturus <davide.turus@alten.it>
2024-09-23 10:19:05 +0200

    Implemented KeychainPublicKeyDetails page and its components and the hook to get the producer keychain key acitons

---
+++ src/pages/ProviderKeychainDetailsPage/components/KeychainPublicKeysTab/KeychainPublicKeyGeneralInfoSection.tsx
@@ -0,0 +1,71 @@
+import { KeychainQueries } from '@/api/keychain/keychain.queries'
+import { SectionContainer, SectionContainerSkeleton } from '@/components/layout/containers'
+import { Alert, Grid, Stack } from '@mui/material'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import OpenInNewIcon from '@mui/icons-material/OpenInNew'
+import { InformationContainer } from '@pagopa/interop-fe-commons'
+import { formatDateString } from '@/utils/format.utils'
+
+type KeychainPublicKeyGeneralInfoSectionProps = {
+  keychainId: string
+  keyId: string
+}
+
+export const KeychainPublicKeyGeneralInfoSection: React.FC<
+  KeychainPublicKeyGeneralInfoSectionProps
+> = ({ keychainId, keyId }) => {
+  const { t } = useTranslation('keychain', { keyPrefix: 'publicKey' })
+  const { data: publicKey } = useSuspenseQuery(
+    KeychainQueries.getProducerKeychainKey({ producerKeychainId: keychainId, keyId })
+  )
+
+  return (
+    <Grid container>
+      <Grid item xs={7}>
+        <SectionContainer
+          title={t('generalInformations')}
+          bottomActions={[
+            {
+              label: t('goToTechnicalDocButton'),
+              target: '_blank',
+              href: 'TODO',
+              startIcon: <OpenInNewIcon fontSize="small" />,
+            },
+          ]}
+        >
+          <Stack spacing={2}>
+            {/* <InformationContainer
+              label={t('creationDateField.label')}
+              content={formatDateString(publicKey.createdAt)}
+            /> */}
+            <InformationContainer
+              label={t('uploaderField.label')}
+              content={`${publicKey.user.name} ${publicKey.user.familyName}`}
+            />
+            <InformationContainer
+              label={t('keyIdField.label')}
+              content={publicKey.keyId}
+              copyToClipboard={{
+                value: publicKey.keyId,
+                tooltipTitle: t('keyIdField.copySuccessFeedbackText'),
+              }}
+            />
+            {publicKey.isOrphan && <Alert severity="info">{t('userDeletedAlertMessage')}</Alert>}
+          </Stack>
+        </SectionContainer>
+      </Grid>
+    </Grid>
+  )
+}
+
+export const KeychainPublicKeyGeneralInfoSectionSkeleton = () => {
+  return (
+    <Grid container>
+      <Grid item xs={7}>
+        <SectionContainerSkeleton sx={{ mt: 4 }} height={318} />
+      </Grid>
+    </Grid>
+  )
+}
