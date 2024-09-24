20149870361a905d82b0f86c9532236c10b259ff
martinaCampoli <164033351+martinaCampoli@users.noreply.github.com>
2024-09-24 11:43:34 +0200

    Feature/pin 5163 keychains list page implementation (#945)
    * Added keychains section
    
    * Implemented keychain listing + deletion dialog
    
    * Removed unused imports
    
    * Fixed copy + implemented keychain listing calls
    
    * Minor fixes
    
    ---------
    
    Co-authored-by: Carmine Porricelli <carmine.porricelli@pagopa.it>

---
+++ src/api/keychain/keychain.mutations.ts
@@ -0,0 +1,23 @@
+import { useMutation } from '@tanstack/react-query'
+import { useTranslation } from 'react-i18next'
+import { KeychainServices } from './keychain.services'
+
+function useDeleteKeychain() {
+  const { t } = useTranslation('mutations-feedback', { keyPrefix: 'keychain.deleteKeychain' })
+  return useMutation({
+    mutationFn: KeychainServices.deleteKeychain,
+    meta: {
+      successToastLabel: t('outcome.success'),
+      errorToastLabel: t('outcome.error'),
+      loadingLabel: t('loading'),
+      confirmationDialog: {
+        title: t('confirmDialog.title'),
+        description: t('confirmDialog.description'),
+      },
+    },
+  })
+}
+
+export const KeychainMutations = {
+  useDeleteKeychain,
+}
