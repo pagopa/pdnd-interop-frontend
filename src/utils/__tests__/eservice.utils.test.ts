2b5a038be518b4439a4232d8c003e184ca71df4f
alten-dturus <davide.turus@alten.it>
2024-11-15 11:39:43 +0100

    Implemented getLastDescriptor utility function and test

--- src/utils/__tests__/eservice.utils.test.ts
+++ src/utils/__tests__/eservice.utils.test.ts
@@ -1,4 +1,4 @@
-import { getDownloadDocumentName } from '../eservice.utils'
+import { getDownloadDocumentName, getLastDescriptor } from '../eservice.utils'
 
 describe('getDownloadDocumentName utility function testing', () => {
   it('should correctly get the document namy from a DocumentRead data type', () => {
@@ -10,5 +10,17 @@ describe('getDownloadDocumentName utility function testing', () => {
     })
 
     expect(result).toEqual('document.pdf')
+  })
+})
+
+describe('getLastDescriptor utility function testing', () => {
+  it('should correctly get the last descriptor from an array of descriptors', () => {
+    const result = getLastDescriptor([
+      { id: 'test-id-1', state: 'PUBLISHED', version: '1', audience: ['test-audience'] },
+      { id: 'test-id-2', state: 'PUBLISHED', version: '2', audience: ['test-audience'] },
+      { id: 'test-id-3', state: 'PUBLISHED', version: '3', audience: ['test-audience'] },
+    ])
+
+    expect(result?.id).toEqual('test-id-3')
   })
 })
