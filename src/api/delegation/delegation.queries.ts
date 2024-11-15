a6e7fb0bf2af4e1fb92ab7acc1d69cfbdffb43c3
alten-dturus <davide.turus@alten.it>
2024-11-15 11:09:29 +0100

    Minor fix

--- src/api/delegation/delegation.queries.ts
+++ src/api/delegation/delegation.queries.ts
@@ -1,8 +1,8 @@
 import { queryOptions } from '@tanstack/react-query'
-import type { GetProducerDelegationsParams } from '../api.generatedTypes'
+import type { GetDelegationsParams } from '../api.generatedTypes'
 import { DelegationServices } from './delegation.services'
 
-function getProducerDelegationsList(params: GetProducerDelegationsParams) {
+function getProducerDelegationsList(params: GetDelegationsParams) {
   return queryOptions({
     queryKey: ['DelegationGetProducerDelegationsList', params],
     queryFn: () => DelegationServices.getProducerDelegations(params),
