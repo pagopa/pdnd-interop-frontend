b7b7198ac702c74afaadb3e50875d667813b445b
alten-dturus <davide.turus@alten.it>
2024-11-27 09:34:35 +0100

    Add download contract actions

--- src/pages/DelegationDetailsPage/components/DelegationGeneralInfoSection.tsx
+++ src/pages/DelegationDetailsPage/components/DelegationGeneralInfoSection.tsx
@@ -1,5 +1,5 @@
 import { AuthHooks } from '@/api/auth'
-import { DelegationQueries } from '@/api/delegation'
+import { DelegationDownloads, DelegationQueries } from '@/api/delegation'
 import { SectionContainer, SectionContainerSkeleton } from '@/components/layout/containers'
 import { StatusChip } from '@/components/shared/StatusChip'
 import { Link } from '@/router'
@@ -11,6 +11,7 @@ import { useSuspenseQuery } from '@tanstack/react-query'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { match } from 'ts-pattern'
+import DownloadIcon from '@mui/icons-material/Download'
 
 type DelegationGeneralInfoSectionProps = {
   delegationId: string
@@ -38,10 +39,58 @@ export const DelegationGeneralInfoSection: React.FC<DelegationGeneralInfoSection
 
   const lastDescriptor = getLastDescriptor(delegation.eservice.descriptors)
 
+  const downloadDelegationContract = DelegationDownloads.useDownloadDelegationContract()
+
+  const handleDownloadDelegationDocument = () => {
+    if (!delegation.activationContract) return
+    downloadDelegationContract(
+      {
+        delegationId: delegationId,
+        contractId: delegation.activationContract?.id,
+      },
+      `${delegation.activationContract.prettyName}.pdf`
+    )
+  }
+
+  const handleDownloadRevokeDelegationDocument = () => {
+    if (!delegation.revocationContract) return
+    downloadDelegationContract(
+      {
+        delegationId: delegationId,
+        contractId: delegation.revocationContract?.id,
+      },
+      `${delegation.revocationContract.prettyName}.pdf`
+    )
+  }
+
+  const downloadDelegationContractAction = {
+    startIcon: <DownloadIcon fontSize="small" />,
+    label: t('downloadContractAction.label'),
+    component: 'button',
+    type: 'button',
+    onClick: handleDownloadDelegationDocument,
+  }
+
+  const downloadRevokeDelegationContractAction = {
+    startIcon: <DownloadIcon fontSize="small" />,
+    label: t('downloadRevokedContractAction.label'),
+    component: 'button',
+    type: 'button',
+    onClick: handleDownloadRevokeDelegationDocument,
+  }
+
+  const downloadContractActions = match(delegation.state)
+    .with('ACTIVE', () => [downloadDelegationContractAction])
+    .with('REVOKED', () => [
+      downloadDelegationContractAction,
+      downloadRevokeDelegationContractAction,
+    ])
+    .otherwise(() => [])
+
   return (
     <Grid container>
       <Grid item xs={7}>
-        <SectionContainer title={t('title')}>
+        <SectionContainer title={t('title')} bottomActions={downloadContractActions}>
           <Stack spacing={2}>
             <InformationContainer
               label={t('eserviceNameField.label')}
