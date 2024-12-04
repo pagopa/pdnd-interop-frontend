61e09a2b87c5002e95a6f9dcf0f5c9e6f18019cc
alten-dturus <davide.turus@alten.it>
2024-12-04 16:54:59 +0100

    Implemented dialogRejectDelegatedVersionDraft and strings for it and dialogApproveDelegatedVersionDraft

--- src/components/dialogs/Dialog.tsx
+++ src/components/dialogs/Dialog.tsx
@@ -19,6 +19,7 @@ import type {
   DialogAcceptProducerDelegationProps,
   DialogRejectProducerDelegationProps,
   DialogRevokeProducerDelegationProps,
+  DialogRejectDelegatedVersionDraftProps,
 } from '@/types/dialog.types'
 import { DialogRejectAgreement } from './DialogRejectAgreement'
 import { ErrorBoundary } from '../shared/ErrorBoundary'
@@ -35,6 +36,7 @@ import { DialogDelegations } from './DialogDelegations'
 import { DialogAcceptProducerDelegation } from './DialogAcceptProducerDelegation'
 import { DialogRejectProducerDelegation } from './DialogRejectProducerDelegation'
 import { DialogRevokeProducerDelegation } from './DialogRevokeProducerDelegation'
+import { DialogRejectDelegatedVersionDraft } from './DialogRejectDelegatedVersionDraft'
 
 function match<T>(
   onBasic: (props: DialogBasicProps) => T,
@@ -51,7 +53,8 @@ function match<T>(
   onDelegations: (props: DialogDelegationsProps) => T,
   onAcceptDelegation: (props: DialogAcceptProducerDelegationProps) => T,
   onRejectDelegation: (props: DialogRejectProducerDelegationProps) => T,
-  onRevokeProducerDelegation: (props: DialogRevokeProducerDelegationProps) => T
+  onRevokeProducerDelegation: (props: DialogRevokeProducerDelegationProps) => T,
+  onRejectDelegatedVersionDraft: (props: DialogRejectDelegatedVersionDraftProps) => T
 ) {
   return (props: DialogProps) => {
     switch (props.type) {
@@ -85,6 +88,8 @@ function match<T>(
         return onRejectDelegation(props)
       case 'revokeProducerDelegation':
         return onRevokeProducerDelegation(props)
+      case 'rejectDelegatedVersionDraft':
+        return onRejectDelegatedVersionDraft(props)
     }
   }
 }
@@ -104,7 +109,8 @@ const _Dialog = match(
   (props) => <DialogDelegations {...props} />,
   (props) => <DialogAcceptProducerDelegation {...props} />,
   (props) => <DialogRejectProducerDelegation {...props} />,
-  (props) => <DialogRevokeProducerDelegation {...props} />
+  (props) => <DialogRevokeProducerDelegation {...props} />,
+  (props) => <DialogRejectDelegatedVersionDraft {...props} />
 )
 
 export const Dialog: React.FC = () => {
