7a1c633cbed74ab7b61d88141b2f853c3aaaa138
martinaCampoli <martina.campoli@pagopa.it>
2024-12-09 16:32:05 +0100

    Added array eservices ids in constants.ts

--- src/pages/ConsumerEServiceCatalogPage/components/EServiceCatalogGrid.tsx
+++ src/pages/ConsumerEServiceCatalogPage/components/EServiceCatalogGrid.tsx
@@ -4,6 +4,7 @@ import { CatalogCard, CatalogCardSkeleton } from './CatalogCard'
 import { useTranslation } from 'react-i18next'
 import type { CatalogEService } from '@/api/api.generatedTypes'
 import { STAGE } from '@/config/env'
+import { SH_ESERVICES_ATT_TEMP, SH_ESERVICES_UAT_TEMP } from '@/config/constants'
 
 type EServiceCatalogGridProps = {
   eservices: Array<CatalogEService> | undefined
@@ -14,19 +15,6 @@ export const EServiceCatalogGrid: React.FC<EServiceCatalogGridProps> = ({ eservi
 
   const isEmpty = !eservices || eservices.length === 0
 
-  const shEservicesAtt = ['9b6993ee-60e3-4901-9a32-e6987d690ec4'] //signal hub eservices to hide
-  const shEservicesUat = [
-    //signal hub eservices to hide
-    '7ab0a0fc-7d22-4007-b2f3-fddd68fe2f17',
-    'e8c087eb-627b-4488-a9b7-65b70fd1301b',
-    '407edf51-23b5-462b-af6e-128bbaa4d9ff',
-    '3b0fbe47-2e2c-4d8b-9cff-b2381c92d003',
-    '260e45e1-9a61-49d6-8b6d-da0643da68ac',
-    'a2b84a6e-34cf-44ca-85a4-de21fd232668',
-    '6b14c622-dad2-44ea-82bc-2dd4010364d5',
-    '03c34a8a-a79a-4928-9afc-8647eefabdb1',
-  ]
-
   if (isEmpty) return <Alert severity="info">{t('noDataLabel')}</Alert>
 
   return (
@@ -34,9 +22,13 @@ export const EServiceCatalogGrid: React.FC<EServiceCatalogGridProps> = ({ eservi
       {eservices?.map((eservice) => (
         <Grid item key={eservice.id} xs={4}>
           {(STAGE === 'ATT' &&
-            shEservicesAtt.some((shEservice) => shEservice === eservice.activeDescriptor?.id)) ||
+            SH_ESERVICES_ATT_TEMP.some(
+              (shEservice) => shEservice === eservice.activeDescriptor?.id
+            )) ||
           (STAGE === 'UAT' &&
-            shEservicesUat.some((shEservice) => shEservice === eservice.activeDescriptor?.id)) ? (
+            SH_ESERVICES_UAT_TEMP.some(
+              (shEservice) => shEservice === eservice.activeDescriptor?.id
+            )) ? (
             <CatalogCard key={eservice.activeDescriptor?.id} eservice={eservice} disabled={true} />
           ) : (
             <CatalogCard key={eservice.activeDescriptor?.id} eservice={eservice} disabled={false} />
