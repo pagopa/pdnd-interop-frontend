77cb22b0db0e67c91c3b9fa83eadaf3abb974b1e
martinaCampoli <164033351+martinaCampoli@users.noreply.github.com>
2024-12-20 09:54:29 +0100

    feature/pin-5686-download-signal-hub-openapi-files (#1008)
    * Changed interface for downloading openapi files + Added section download sh openapi
    
    * Removed comment
    
    * Fixed typo
    
    * Added push and pull sh url
    
    * Changed strings pull and push api signal hub
    
    * Added isSignalHubEnabled
    
    * Update VoucherInstructionsStep4.tsx
    
    * Fix links
    
    ---------
    
    Co-authored-by: Carmine Porricelli <carmine.porricelli@pagopa.it>

--- src/pages/ConsumerClientManagePage/components/VoucherInstructions/VoucherInstructionsStep4.tsx
+++ src/pages/ConsumerClientManagePage/components/VoucherInstructions/VoucherInstructionsStep4.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { StepActions } from '@/components/shared/StepActions'
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 import { useVoucherInstructionsContext } from './VoucherInstructionsContext'
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline'
-import { Button, Stack, Typography } from '@mui/material'
+import { Alert, AlertTitle, Button, Stack, Typography } from '@mui/material'
 import { useClientKind } from '@/hooks/useClientKind'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import { SectionContainer } from '@/components/layout/containers'
-import { Link } from '@/router'
 import { PurposeQueries } from '@/api/purpose'
-import { API_GATEWAY_INTEFACE_URL } from '@/config/env'
+import {
+  API_GATEWAY_INTERFACE_URL,
+  API_SIGNAL_HUB_PULL_INTERFACE_URL,
+  API_SIGNAL_HUB_PUSH_INTERFACE_URL,
+  isSignalHubEnabled,
+} from '@/config/env'
 import { useQuery } from '@tanstack/react-query'
+import DownloadIcon from '@mui/icons-material/Download'
+import { Link } from '@/router'
 
 export const VoucherInstructionsStep4: React.FC = () => {
   const { t } = useTranslation('voucher')
@@ -22,38 +27,114 @@ export const VoucherInstructionsStep4: React.FC = () => {
     enabled: Boolean(selectedPurposeId),
   })
 
+  const eserviceName = purpose ? purpose.eservice.name : ''
+  const producer = purpose ? purpose.eservice.producer.name : ''
+
+  console.log({ API_SIGNAL_HUB_PUSH_INTERFACE_URL })
+
   return (
     <>
+      <Alert severity="success" variant="outlined">
+        <AlertTitle>{t(`step4.${clientKind}.title`)}</AlertTitle>
+        <Trans>
+          {clientKind === 'CONSUMER' && purpose
+            ? t(`step4.${clientKind}.description`, {
+                eserviceName: eserviceName,
+                producerName: producer,
+              })
+            : t(`step4.${clientKind}.description`)}
+        </Trans>
+      </Alert>
       <SectionContainer>
-        <Stack spacing={2} alignItems="center" sx={{ textAlign: 'center' }}>
-          <Stack alignItems="center" spacing={1}>
-            <CheckCircleOutlineIcon color="primary" sx={{ fontSize: 80 }} />
-            <Typography variant="h6" component="h2">
-              {t(`step4.${clientKind}.title`)}
-            </Typography>
-            <Typography variant="body2">{t(`step4.${clientKind}.description`)}</Typography>
+        <Stack spacing={2}>
+          <Typography variant="h6" component="h2">
+            {t(`step4.${clientKind}.actionTitle`)}
+          </Typography>
+          <Stack direction="row" spacing={2} alignItems="center" justifyContent="space-between">
+            <Typography variant="body2">{t(`step4.${clientKind}.actionDescription`)}</Typography>
+            {clientKind === 'API' && (
+              <Button
+                sx={{
+                  '&:hover': {
+                    backgroundColor: 'white',
+                  },
+                }}
+                disableRipple
+                href={API_GATEWAY_INTERFACE_URL}
+                download
+              >
+                <DownloadIcon fontSize="small" />
+                {t(`step4.${clientKind}.actionLabel`)}
+              </Button>
+            )}
+            {purpose && clientKind === 'CONSUMER' && (
+              <Link
+                as="button"
+                to={'SUBSCRIBE_CATALOG_VIEW'}
+                params={{
+                  eserviceId: purpose.eservice.id,
+                  descriptorId: purpose.eservice.descriptor.id,
+                }}
+                sx={{
+                  '&:hover': {
+                    backgroundColor: 'white',
+                  },
+                }}
+                disableRipple
+              >
+                {t(`step4.${clientKind}.actionLabel`)}
+              </Link>
+            )}
           </Stack>
-          {purpose && clientKind === 'CONSUMER' && (
-            <Link
-              to="SUBSCRIBE_CATALOG_VIEW"
-              params={{
-                eserviceId: purpose.eservice.id,
-                descriptorId: purpose.eservice.descriptor.id,
-              }}
-              as="button"
-              variant="contained"
-              color="primary"
-            >
-              {t(`step4.${clientKind}.actionLabel`)}
-            </Link>
-          )}
-          {clientKind === 'API' && (
-            <Button href={API_GATEWAY_INTEFACE_URL} download variant="contained" color="primary">
-              {t(`step4.${clientKind}.actionLabel`)}
-            </Button>
-          )}
         </Stack>
       </SectionContainer>
+      {clientKind === 'API' && isSignalHubEnabled && (
+        <SectionContainer>
+          <Stack spacing={2}>
+            <Typography variant="h6" component="h2">
+              {t(`step4.API.titleSignalHub`)}
+            </Typography>
+            <Typography variant="body2" fontWeight={600}>
+              {t(`step4.API.pushApiSH.title`)}
+              <Stack direction="row" spacing={1} alignItems="center" justifyContent="space-between">
+                <Typography variant="body2">{t(`step4.API.pushApiSH.description`)}</Typography>
+                <Button
+                  sx={{
+                    '&:hover': {
+                      backgroundColor: 'white',
+                    },
+                  }}
+                  disableRipple
+                  href={API_SIGNAL_HUB_PUSH_INTERFACE_URL}
+                  download
+                >
+                  <DownloadIcon fontSize="small" />
+                  {t(`step4.actionLabel`)}
+                </Button>
+              </Stack>
+            </Typography>
+            <Typography variant="body2" fontWeight={600}>
+              {t(`step4.API.pullApiSH.title`)}
+              <Stack direction="row" spacing={1} alignItems="center" justifyContent="space-between">
+                <Typography variant="body2">{t(`step4.API.pullApiSH.description`)}</Typography>
+                <Button
+                  sx={{
+                    '&:hover': {
+                      backgroundColor: 'white',
+                    },
+                  }}
+                  disableRipple
+                  href={API_SIGNAL_HUB_PULL_INTERFACE_URL}
+                  download
+                >
+                  <DownloadIcon fontSize="small" />
+                  {t(`step4.actionLabel`)}
+                </Button>
+              </Stack>
+            </Typography>
+          </Stack>
+        </SectionContainer>
+      )}
       <StepActions
         back={{
           label: t('backBtn'),
