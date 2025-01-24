4144179b5f229036d03e15f1af2502d0ba91becd
martinaCampoli <martina.campoli@pagopa.it>
2025-01-24 17:28:26 +0100

    Added template table

---
+++ src/pages/ProviderTemplatesListPage/components/TemplateTable.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { TemplateTableRow, TemplateTableRowSkeleton } from './TemplateTableRow'
+import { Table } from '@pagopa/interop-fe-commons'
+//import type { ProducerTemplate } from '@/api/api.generatedTypes'
+
+type TemplateTableProps = {
+  templates:
+    | Array<{
+        id: string
+        name: string
+        version: string
+        status: string
+      }>
+    | undefined //TODO EServiceTemplates
+}
+
+export const TemplateTable: React.FC<TemplateTableProps> = ({ templates }) => {
+  const { t: tCommon } = useTranslation('common', { keyPrefix: 'table.headData' })
+  const { t } = useTranslation('template')
+
+  const headLabels = [tCommon('templateName'), tCommon('version'), tCommon('status'), '']
+
+  const isEmpty = templates && templates.length === 0
+
+  return (
+    <Table headLabels={headLabels} isEmpty={isEmpty} noDataLabel={t('noMultiDataLabel')}>
+      {templates?.map((template) => <TemplateTableRow key={template.id} template={template} />)}
+    </Table>
+  )
+}
+
+export const TemplateTableSkeleton: React.FC = () => {
+  const { t } = useTranslation('common', { keyPrefix: 'table.headData' })
+  const headLabels = [t('templateName'), t('version'), t('status'), '']
+
+  return (
+    <Table headLabels={headLabels}>
+      <TemplateTableRowSkeleton />
+      <TemplateTableRowSkeleton />
+      <TemplateTableRowSkeleton />
+      <TemplateTableRowSkeleton />
+      <TemplateTableRowSkeleton />
+    </Table>
+  )
+}
