139e329c370970ebfb97d08e3661cee54f6ec385
martinaCampoli <martina.campoli@pagopa.it>
2025-01-28 11:43:03 +0100

    Renamed files from Template to EServiceTemplates

--- src/components/layout/SideNav/hooks/useGetSideNavItems.ts
+++ src/components/layout/SideNav/hooks/useGetSideNavItems.ts
@@ -23,8 +23,8 @@ const views = [
     routeKey: 'PROVIDE',
     id: 'provider',
     children: [
-      'PROVIDE_TEMPLATES_CATALOG',
-      'PROVIDE_TEMPLATES_LIST',
+      'PROVIDE_ESERVICE_TEMPLATES_CATALOG',
+      'PROVIDE_ESERVICE_TEMPLATES_LIST',
       'PROVIDE_ESERVICE_LIST',
       'PROVIDE_AGREEMENT_LIST',
       'PROVIDE_PURPOSE_LIST',
