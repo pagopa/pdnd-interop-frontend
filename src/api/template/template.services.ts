a9999eddd74c8e5948b01104f1fdd1b179fbf2fd
martinaCampoli <martina.campoli@pagopa.it>
2025-01-29 17:18:00 +0100

    Updated mock getSingle

--- src/api/template/template.services.ts
+++ src/api/template/template.services.ts
@@ -25,7 +25,36 @@ async function getSingle(eserviceTemplateId: string) {
     `${BACKEND_FOR_FRONTEND_URL}/eservices/templates//${eServiceTemplateId}`
   )
   return response.data*/
-  return []
+  return {
+    id: 'mock_templateid1',
+    name: 'mock_templatename1',
+    versions: [
+      {
+        id: '1',
+        version: '1',
+        description: 'mock_versionDescription1',
+        state: 'active',
+        voucherLifespan: 600,
+        dailyCallsPerConsumer: 10,
+        dailyCallsTotal: 100,
+      },
+    ],
+    state: 'ACTIVE',
+    eserviceDescription: 'mock_description1',
+    audienceDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum efficitur viverra egestas. Aenean mollis libero sit amet leo dignissim, vel elementum libero iaculis. Praesent tempus ex et iaculis ultrices. Maecenas faucibus, neque quis vulputate iaculis, purus lacus.',
+    creatorId: 'SFDGVDVDGF',
+    technology: 'REST',
+    mode: 'RECEIVE',
+    isSignalHubEnabled: false,
+    attributes: [
+      {
+        certified: [''],
+        verified: [''],
+        declared: [''],
+      },
+    ],
+  }
 }
 
 export const TemplateServices = {
