f3fcfddfcf0b83c5e66e8cb1e3bb46d9dfa467ff
borgesis95 <salvatore.borgesi@pagopa.it>
2025-02-19 10:33:05 +0100

    chore: update ProviderEserviceTemplatePage

--- src/pages/ProviderEServiceTemplatesCatalogPage/ProviderEServiceTemplatesCatalog.page.tsx
+++ src/pages/ProviderEServiceTemplatesCatalogPage/ProviderEServiceTemplatesCatalog.page.tsx
@@ -1,17 +1,54 @@
 import React from 'react'
 import { PageContainer } from '@/components/layout/containers'
 import { AuthHooks } from '@/api/auth'
-import { TemplateQueries, TemplateServices } from '@/api/template'
-
+import { TemplateQueries } from '@/api/template'
 import type { ActionItemButton } from '@/types/common.types'
 import { useTranslation } from 'react-i18next'
-import { Filters } from '@pagopa/interop-fe-commons'
+import { ProviderEServiceCatalogGrid } from './components'
+import {
+  Filters,
+  Pagination,
+  useAutocompleteTextInput,
+  useFilters,
+  usePagination,
+} from '@pagopa/interop-fe-commons'
 import { EServiceCatalogGridSkeleton } from '../ConsumerEServiceCatalogPage/components'
-import { useQuery } from '@tanstack/react-query'
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
+import { GetEServicesCatalogParams } from '@/api/api.generatedTypes'
 
 const ProviderEServiceTemplatesCatalogPage: React.FC = () => {
   const { isAdmin } = AuthHooks.useJwt()
   const { t } = useTranslation('pages', { keyPrefix: 'providerEServiceTemplatesCatalog' })
+  const { t: tTemplate } = useTranslation('template', { keyPrefix: 'list.filters' })
+
+  const [templateProducersAutocompleteInput, setTemplateProducersAutocompleteInput] =
+    useAutocompleteTextInput()
+
+  const { paginationParams, paginationProps, getTotalPageCount } = usePagination({ limit: 12 })
+
+  const { filtersParams, ...filtersHandlers } = useFilters<
+    Omit<GetEServicesCatalogParams, 'limit' | 'offset'>
+  >([
+    {
+      name: 'q',
+      label: tTemplate('nameField.label'),
+      type: 'freetext',
+    },
+    {
+      name: 'producersIds',
+      label: tTemplate('templateProviderField.label'),
+      type: 'autocomplete-multiple',
+      options: [],
+      onTextInputChange: setTemplateProducersAutocompleteInput,
+    },
+  ])
+  const queryParams = { ...paginationParams, ...filtersParams }
+
+  const { data } = useQuery({
+    ...TemplateQueries.getProviderTemplatesCatalogList(queryParams),
+    placeholderData: keepPreviousData,
+  })
+
   const topSideActions: Array<ActionItemButton> = []
 
   return (
@@ -20,8 +57,12 @@ const ProviderEServiceTemplatesCatalogPage: React.FC = () => {
       description={t('description')}
       topSideActions={isAdmin ? topSideActions : undefined}
     >
-      CATALOGO TEMPLATE
+      <Filters {...filtersHandlers} />
       <ProviderEServiceTemplatesCatalogWrapper params={{ limit: 20, offset: 1 }} />
+      <Pagination
+        {...paginationProps}
+        totalPages={getTotalPageCount(data?.pagination.totalCount)}
+      />
     </PageContainer>
   )
 }
@@ -33,7 +74,7 @@ const ProviderEServiceTemplatesCatalogWrapper: React.FC<{
 
   if (!data && isFetching) return <EServiceCatalogGridSkeleton />
 
-  return <div></div>
+  return <ProviderEServiceCatalogGrid eservicesTemplateList={data?.results} />
 }
 
 export default ProviderEServiceTemplatesCatalogPage
