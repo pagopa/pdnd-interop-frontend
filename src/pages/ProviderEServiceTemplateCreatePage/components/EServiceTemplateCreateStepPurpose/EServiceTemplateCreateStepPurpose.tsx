319e8dbbeda96a953b26dbbd94d6d4bc1283c816
martinaCampoli <164033351+martinaCampoli@users.noreply.github.com>
2025-03-03 16:19:36 +0100

    Feature/pin 6019 eservice template creation form (#1139)

---
+++ src/pages/ProviderEServiceTemplateCreatePage/components/EServiceTemplateCreateStepPurpose/EServiceTemplateCreateStepPurpose.tsx
@@ -0,0 +1,61 @@
+import { SectionContainer, SectionContainerSkeleton } from '@/components/layout/containers'
+import { StepActions } from '@/components/shared/StepActions'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+import { EServiceTemplateCreateStepPurposeRiskAnalysis } from './EServiceTemplateCreateStepPurposeRiskAnalysis/EServiceTemplateCreateStepPurposeRiskAnalysis'
+import { useEServiceTemplateCreateContext } from '../ProviderEServiceTemplateContext'
+import { EServiceTemplateCreateStepPurposeAddPurposesTable } from './EServiceTemplateCreateStepPurposeAddPurposeTable'
+
+export const EServiceTemplateCreateStepPurpose: React.FC = () => {
+  const { t } = useTranslation('eservice', { keyPrefix: 'create' })
+
+  const { template, forward, back, riskAnalysisFormState } = useEServiceTemplateCreateContext()
+
+  return (
+    <>
+      {!riskAnalysisFormState.isOpen ? (
+        <>
+          <SectionContainer
+            title={t('stepPurpose.purposeTableSection.title')}
+            description={t('stepPurpose.purposeTableSection.description')}
+          >
+            <EServiceTemplateCreateStepPurposeAddPurposesTable />
+          </SectionContainer>
+          <StepActions
+            back={{
+              label: t('backWithoutSaveBtn'),
+              type: 'button',
+              onClick: back,
+              startIcon: <ArrowBackIcon />,
+            }}
+            forward={{
+              label: t('forwardWithoutSaveBtn'),
+              type: 'button',
+              onClick: forward,
+              endIcon: <ArrowForwardIcon />,
+              disabled: template?.eserviceTemplate.riskAnalysis.length === 0,
+              tooltip:
+                template?.eserviceTemplate.riskAnalysis.length === 0
+                  ? t('stepPurpose.purposeTableSection.noSelectedPurposesTooltip')
+                  : undefined,
+            }}
+          />
+        </>
+      ) : (
+        <EServiceTemplateCreateStepPurposeRiskAnalysis />
+      )}
+    </>
+  )
+}
+
+export const EServiceTemplateCreateStepPurposeSkeleton: React.FC = () => {
+  const { t } = useTranslation('eservice', { keyPrefix: 'create.stepPurpose' })
+
+  return (
+    <>
+      <SectionContainerSkeleton height={246} />
+    </>
+  )
+}
---
+++ src/pages/ProviderEServiceTemplateSummaryPage/components/ProviderEServiceTemplateGeneralInfoSummary.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+import { InformationContainer } from '@pagopa/interop-fe-commons'
+import { useTranslation } from 'react-i18next'
+import { useParams } from '@/router'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { FEATURE_FLAG_SIGNALHUB_WHITELIST, SIGNALHUB_WHITELIST_PRODUCER } from '@/config/env'
+import { AuthHooks } from '@/api/auth'
+import { TemplateQueries } from '@/api/template'
+
+export const ProviderEServiceTemplateGeneralInfoSummary: React.FC = () => {
+  const producerId = AuthHooks.useJwt().jwt?.organizationId as string
+  const isSignalHubFlagEnabled = FEATURE_FLAG_SIGNALHUB_WHITELIST
+    ? SIGNALHUB_WHITELIST_PRODUCER.includes(producerId)
+    : true
+
+  const { t } = useTranslation('template', { keyPrefix: 'summary.generalInfoSummary' })
+  const params = useParams<'PROVIDE_ESERVICE_TEMPLATE_SUMMARY'>()
+
+  const { data: template } = useSuspenseQuery(
+    TemplateQueries.getSingle(params.eServiceTemplateId, params.eServiceTemplateVersionId)
+  )
+
+  return (
+    <Stack spacing={2}>
+      <InformationContainer
+        label={t('description.label')}
+        content={template.eserviceTemplate.eserviceDescription}
+      />
+      <InformationContainer
+        label={t('apiTechnology.label')}
+        content={template.eserviceTemplate.technology}
+      />
+      {isSignalHubFlagEnabled && (
+        <InformationContainer
+          label={t('isSignalHubEnabled.label')}
+          content={t(`isSignalHubEnabled.value.${template.eserviceTemplate.isSignalHubEnabled}`)}
+        />
+      )}
+    </Stack>
+  )
+}
