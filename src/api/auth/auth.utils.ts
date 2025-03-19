4daf5b9701215b54f517be78741c994bdbeb1161
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-03-19 17:09:10 +0100

    Hotfix: Fix decode jwt and Bump version (#1185)
    * Fix decode jwt and Bump version
    
    * Skip bugged test

--- src/api/auth/auth.utils.ts
+++ src/api/auth/auth.utils.ts
@@ -1,6 +1,7 @@
 import { PRODUCER_ALLOWED_ORIGINS } from '@/config/env'
 import type { JwtUser } from '@/types/party.types'
 import memoize from 'lodash/memoize'
+import { jwtDecode } from 'jwt-decode'
 
 export type ParsedJwt = ReturnType<typeof parseJwt>
 
@@ -8,7 +9,7 @@ export type ParsedJwt = ReturnType<typeof parseJwt>
  * Parse the JWT token and return the user informations stored in it
  */
 export const parseJwt = memoize((token: string | null | undefined) => {
-  const jwt = token ? (JSON.parse(window.atob(token.split('.')[1])) as JwtUser) : undefined
+  const jwt = token ? jwtDecode<JwtUser>(token) : undefined
   const currentRoles = jwt ? jwt.organization.roles.map((r) => r.role) : []
   const isAdmin = currentRoles.length === 1 && currentRoles[0] === 'admin'
   const isOperatorAPI = currentRoles.includes('api')
