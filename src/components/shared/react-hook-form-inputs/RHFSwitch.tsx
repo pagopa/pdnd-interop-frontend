ad2b29d381cf4bd134a6374ca4068cbdcf02d8bf
alten-dturus <davide.turus@alten.it>
2025-04-28 15:16:49 +0200

    add margin right to create space from the label and the switch

--- src/components/shared/react-hook-form-inputs/RHFSwitch.tsx
+++ src/components/shared/react-hook-form-inputs/RHFSwitch.tsx
@@ -52,6 +52,7 @@ export const RHFSwitch: React.FC<RHFSwitchProps> = ({
                 inputProps={{ ...props.inputProps, ...accessibilityProps }}
                 checked={value}
                 inputRef={ref}
+                sx={{ mr: 1 }}
               />
             }
             label={label}
