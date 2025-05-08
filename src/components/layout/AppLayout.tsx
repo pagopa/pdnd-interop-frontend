07ddba14a140803973f8473688374aa37bbab70a
borgesis95 <salvatore.borgesi@pagopa.it>
2025-05-08 15:22:51 +0200

    feat: sidenav menu with collapsable

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Box, Stack } from '@mui/material'
 import type { SxProps } from '@mui/material'
 import { SideNav } from '@/components/layout'
+import type { SidebarRoutes } from '../sidebar/sidebar.types'
+import PeopleIcon from '@mui/icons-material/People'
+import { Sidebar } from '../sidebar/Sidebar'
 
 type AppLayoutProps = {
   children: React.ReactNode
@@ -10,6 +13,63 @@ type AppLayoutProps = {
 }
 
 export const AppLayout: React.FC<AppLayoutProps> = ({ children, hideSideNav, sx }) => {
+  const interopRoutes: SidebarRoutes = [
+    {
+      subpath: 'SUBSCRIBE_CATALOG_LIST',
+      icon: PeopleIcon,
+      children: [],
+      divider: true,
+    },
+    {
+      showNotification: false,
+      icon: PeopleIcon,
+      label: 'Fruizione',
+      subpath: 'PROVIDE_AGREEMENT_LIST',
+      children: [
+        { to: 'PROVIDE_AGREEMENT_LIST', label: 'Richieste inoltrate' },
+        { to: 'SUBSCRIBE_PURPOSE_LIST', label: 'Finalità inoltrate' },
+      ],
+      divider: false,
+    },
+    {
+      icon: PeopleIcon,
+      label: 'Erogazione',
+      subpath: 'PROVIDE_ESERVICE_LIST',
+      hide: false,
+      children: [
+        { to: 'PROVIDE_ESERVICE_TEMPLATE_LIST', label: 'Template E-Service' },
+        { to: 'PROVIDE_ESERVICE_LIST', label: 'I Miei E-service' },
+        { to: 'PROVIDE_AGREEMENT_LIST', label: 'Richieste di fruizione' },
+        { to: 'PROVIDE_PURPOSE_LIST', label: 'Finalità ricevute' },
+      ],
+    },
+    // {
+    //   icon: PeopleIcon,
+    //   subpath: '/privacy-policy',
+    //   label: 'Gestione del client',
+    //   hide: false,
+    //   children: [
+    //     { to: '/gestione-client/portachiavi', label: 'Portachiavi' },
+    //     { to: '/gestione-client/api-fruizione', label: 'API fruizione' },
+    //     { to: '/gestione-client/api-fruizione-interop', label: 'API Fruizione Interop' },
+    //     { to: '/gestione-client/debug-client-assertion', label: 'Debug client assertion' },
+    //   ],
+    // },
+    // {
+    //   icon: PeopleIcon,
+    //   label: 'Il mio ente',
+    //   subpath: '/il-mio-ente',
+    //   children: [
+    //     { to: '/il-mio-ente/anagrafica', label: 'Anagrafica e attributi' },
+    //     {
+    //       to: '/il-mio-ente/gestione-delle-deleghe',
+    //       hide: false,
+    //       label: 'Gestione delle deleghe',
+    //     },
+    //   ],
+    // },
+  ]
+
   if (hideSideNav) {
     return (
       <Box
@@ -30,7 +90,8 @@ export const AppLayout: React.FC<AppLayoutProps> = ({ children, hideSideNav, sx 
   return (
     <Box sx={{ flexGrow: 1, height: '100%' }}>
       <Stack direction="row" sx={{ height: '100%', overflowX: 'hidden' }}>
-        <SideNav />
+        {/* <SideNav /> */}
+        <Sidebar routes={interopRoutes} />
         <Box
           sx={{
             px: 3,
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,216 @@
+import React from 'react'
+import {
+  Box,
+  Divider,
+  List,
+  useTheme,
+  Tooltip,
+  IconButton,
+  Stack,
+  useMediaQuery,
+  Typography,
+} from '@mui/material'
+import { sidebarStyles } from './sidebar.styles'
+import { SidebarItemRoot } from './SidebarItemRoot'
+import { useState } from 'react'
+import { type RouteKey, useCurrentRoute } from '@/router'
+import MenuIcon from '@mui/icons-material/Menu'
+import { getCurrentSelfCareProductId } from '@/utils/common.utils'
+import ExitToAppRoundedIcon from '@mui/icons-material/ExitToAppRounded'
+import PeopleIcon from '@mui/icons-material/People'
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle'
+import { SidebarLink } from './SidebarLink'
+import { useTranslation } from 'react-i18next'
+import type { SidebarRoutes } from './sidebar.types'
+import { SELFCARE_BASE_URL } from '@/config/env'
+
+type SidebarProps = {
+  routes: SidebarRoutes
+}
+export const Sidebar: React.FC<SidebarProps> = ({ routes }) => {
+  const theme = useTheme()
+  // const pathname = useCurrentRoute().pathname
+  const pathname = useCurrentRoute().routeKey
+  const { t } = useTranslation('shared-components', { keyPrefix: 'sidenav' })
+
+  const matchMobile = useMediaQuery(theme.breakpoints.up('sm'))
+
+  const [collapsed, setCollapsed] = useState(false)
+  const handleCollapsed = () => {
+    setCollapsed((prev) => !prev)
+  }
+
+  const styles = sidebarStyles(theme, collapsed)
+
+  const [expandItemRoot, setExpandItemRoot] = useState<RouteKey | undefined>(
+    () => routes.find((route) => pathname.startsWith(route.subpath))?.subpath
+  )
+
+  const handleExpandItemRoot = (subpath: RouteKey) => {
+    setExpandItemRoot((prev) => (prev === subpath ? undefined : subpath))
+  }
+  return (
+    <Box sx={styles.container} component="aside">
+      {matchMobile ? (
+        <Stack
+          component="nav"
+          display="flex"
+          flexDirection="column"
+          aria-expanded={!collapsed}
+          aria-label={t('navigationMenu')}
+          role="navigation"
+          sx={styles.nav}
+        >
+          <SidebarList
+            routes={routes}
+            expandItemRoot={expandItemRoot}
+            handleExpandItemRoot={handleExpandItemRoot}
+            collapsed={collapsed}
+          />
+          <HamburgerBox collapsed={collapsed} handleCollapsed={handleCollapsed} />
+        </Stack>
+      ) : (
+        <SidebarMobile routes={routes} />
+      )}
+    </Box>
+  )
+}
+
+type SidebarListProps = {
+  routes: SidebarRoutes
+  expandItemRoot: RouteKey | undefined
+  handleExpandItemRoot: (subpath: RouteKey) => void
+  collapsed: boolean
+}
+const SidebarList: React.FC<SidebarListProps> = ({
+  routes,
+  expandItemRoot,
+  handleExpandItemRoot,
+  collapsed,
+}) => {
+  return (
+    <List disablePadding sx={{ marginTop: 1 }}>
+      {routes
+        .filter(({ hide }) => !hide)
+        .map((route) => (
+          <SidebarItemRoot
+            key={route.label}
+            notification={{
+              show: route?.showNotification ?? false,
+              content: 0, // TODO: This will change, right now is fixed
+            }}
+            label={route.label}
+            divider={route.divider}
+            isExpanded={expandItemRoot === route.subpath}
+            // eslint-disable-next-line react/no-children-prop
+            children={route?.children}
+            StartIcon={route.icon}
+            subpath={route.subpath}
+            handleExpanded={() => handleExpandItemRoot(route.subpath)}
+            collapsed={collapsed}
+          />
+        ))}
+      <Divider />
+      <SidebarLink
+        label="Utenti"
+        component="a"
+        href={`${SELFCARE_BASE_URL}/dashboard/${getCurrentSelfCareProductId()}/users#${getCurrentSelfCareProductId()}`}
+        target="_blank"
+        StartIcon={PeopleIcon}
+        EndIcon={ExitToAppRoundedIcon}
+        typographyProps={{ sx: { fontWeight: 600 } }}
+        collapsed={collapsed}
+      />
+      <SidebarLink
+        label="Gruppi"
+        href={`${SELFCARE_BASE_URL}/dashboard/${getCurrentSelfCareProductId()}/groups`}
+        target="_blank"
+        StartIcon={SupervisedUserCircleIcon}
+        EndIcon={ExitToAppRoundedIcon}
+        typographyProps={{ sx: { fontWeight: 600 } }}
+        collapsed={collapsed}
+      />
+    </List>
+  )
+}
+
+const SidebarMobile: React.FC<SidebarProps> = ({ routes }) => {
+  const pathname = useCurrentRoute().routeKey
+  // const pathname = useCurrentRoute().pathname
+
+  const [expandItemRoot, setExpandItemRoot] = useState<RouteKey | undefined>(
+    () => routes.find((route) => pathname.startsWith(route.subpath))?.subpath
+  )
+
+  const [isOpenSidebar, setIsOpenSidebar] = useState(false)
+
+  const handleExpandItemRoot = (subpath: RouteKey) => {
+    setExpandItemRoot((prev) => (prev === subpath ? undefined : subpath))
+  }
+
+  const handleOpenSidebar = () => {
+    setIsOpenSidebar(!isOpenSidebar)
+  }
+
+  return (
+    <>
+      <Box display="flex" flexDirection="row" padding={1}>
+        <Tooltip placement="right" title="Menu">
+          <IconButton
+            sx={{ padding: { xs: 1 } }}
+            data-testid="hamburgerButton"
+            aria-label="hamburger-icon"
+            onClick={handleOpenSidebar}
+            size="large"
+          >
+            <MenuIcon />
+          </IconButton>
+        </Tooltip>
+        <Typography ml={1} mt={1} variant="h6" component="h6">
+          Menù
+        </Typography>
+      </Box>
+      <Divider orientation="horizontal" component="div" />
+      {isOpenSidebar && (
+        <>
+          <SidebarList
+            routes={routes}
+            expandItemRoot={expandItemRoot}
+            handleExpandItemRoot={handleExpandItemRoot}
+            collapsed={false}
+          />
+        </>
+      )}
+    </>
+  )
+}
+
+type HamburgerMenuBoxProps = {
+  collapsed: boolean
+  handleCollapsed: () => void
+}
+const HamburgerBox: React.FC<HamburgerMenuBoxProps> = ({ collapsed, handleCollapsed }) => {
+  const theme = useTheme()
+  const styles = sidebarStyles(theme, collapsed)
+  const { t } = useTranslation('shared-components', { keyPrefix: 'sidenav' })
+  const tooltipTitle = t(!collapsed ? 'collapse' : 'expand')
+
+  return (
+    <Box sx={styles.hamburgerBox}>
+      <Divider orientation="horizontal" />
+      <Box sx={styles.hamburgerIcon}>
+        <Tooltip placement="right" title={tooltipTitle}>
+          <IconButton
+            sx={{ padding: { xs: 1 } }}
+            data-testid="hamburgerButton"
+            aria-label="hamburger-icon"
+            onClick={handleCollapsed}
+            size="large"
+          >
+            <MenuIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
+    </Box>
+  )
+}
