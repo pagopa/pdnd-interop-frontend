fde4364e79951b32e8a5842229d792055f8a74b9
borgesis95 <salvatore.borgesi@pagopa.it>
2025-05-08 17:22:26 +0200

    feat: added icons and other stuff

--- src/components/layout/SideNav/hooks/useIsRouteInCurrentSubtree.tsx
+++ src/components/layout/SideNav/hooks/useIsRouteInCurrentSubtree.tsx
@@ -2,6 +2,8 @@ import { type RouteKey, getParentRoutes, useCurrentRoute } from '@/router'
 
 export function useIsRouteInCurrentSubtree() {
   const { routeKey: currentRouteKey } = useCurrentRoute()
+
+  console.log('currentRouteKey', currentRouteKey)
   return (routeKey: RouteKey) => {
     return [...getParentRoutes(currentRouteKey), currentRouteKey].includes(routeKey)
   }
