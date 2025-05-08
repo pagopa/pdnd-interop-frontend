3626d5944f94f25a90b0a07f26006a8b1edbfcd2
borgesis95 <salvatore.borgesi@pagopa.it>
2025-05-08 10:40:29 +0200

    chore: moved RiskAnalysisInfoSummary component

--- src/pages/RiskAnalysisEserviceAssociatedPage/RiskAnalysisEServiceAssociated.page.tsx
+++ src/pages/RiskAnalysisEserviceAssociatedPage/RiskAnalysisEServiceAssociated.page.tsx
@@ -7,15 +7,13 @@ import { Box } from '@mui/material'
 import React, { useEffect } from 'react'
 import { Redirect, useParams } from '@/router'
 import { useTranslation } from 'react-i18next'
-import { useQuery, useSuspenseQuery } from '@tanstack/react-query'
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { EServiceQueries } from '@/api/eservice'
 import type { EServiceRiskAnalysis } from '@/api/api.generatedTypes'
 import { useSearchParams } from 'react-router-dom'
-import useCurrentLanguage from '@/hooks/useCurrentLanguage'
 import { FormProvider, useForm } from 'react-hook-form'
-import { PurposeQueries } from '@/api/purpose'
-import { List, ListItem, ListItemText, Typography } from '@mui/material'
 import { RHFAutocompleteSingle } from '@/components/shared/react-hook-form-inputs'
+import { RiskAnalysisInfoSummary } from '@/components/shared/RiskAnalysisInfoSummary'
 
 const RiskAnalysisEServiceAssociatedPage: React.FC = () => {
   return (
@@ -90,7 +88,7 @@ const RiskAnalysisEServiceAssociatedPageContent: React.FC = () => {
           </SectionContainer>
           {riskAnalysisId && eserviceId && (
             <SectionContainer>
-              <RiskAnalysisInfo riskAnalysisId={riskAnalysisId} eServiceId={eserviceId} />
+              <RiskAnalysisInfoSummary riskAnalysisId={riskAnalysisId} eServiceId={eserviceId} />
             </SectionContainer>
           )}
         </Box>
@@ -125,90 +123,6 @@ const RiskAnalysisSelect: React.FC<RiskyAnalysisSelectProps> = ({
       label={t('watchRiskyAnalysisAssociated.title')}
       options={menuLabelOptions}
     />
-  )
-}
-
-type RiskAnalysisInfoProps = {
-  riskAnalysisId: string
-  eServiceId: string
-}
-const RiskAnalysisInfo: React.FC<RiskAnalysisInfoProps> = ({ riskAnalysisId, eServiceId }) => {
-  type QuestionItem = { question: string; answer: string; questionInfoLabel?: string }
-
-  const { t } = useTranslation('purpose', { keyPrefix: 'create.eserviceRiskAnalysisSection' })
-  const currentLanguage = useCurrentLanguage()
-
-  const { data: riskAnalysis } = useQuery({
-    ...EServiceQueries.getEServiceRiskAnalysis(eServiceId!, riskAnalysisId!),
-    enabled: Boolean(eServiceId && riskAnalysisId),
-  })
-  const riskAnalysisTemplate = riskAnalysis?.riskAnalysisForm.answers
-
-  const { data: riskAnalysisConfig } = useQuery({
-    ...PurposeQueries.getRiskAnalysisVersion({
-      riskAnalysisVersion: riskAnalysis?.riskAnalysisForm.version as string,
-      eserviceId: eServiceId!,
-    }),
-    enabled: Boolean(riskAnalysis?.riskAnalysisForm.version && eServiceId),
-  })
-
-  const questions: Array<QuestionItem> = React.useMemo(() => {
-    if (!riskAnalysisTemplate || !riskAnalysisConfig) return []
-
-    // Answers in this form
-    const answerIds = Object.keys(riskAnalysisTemplate)
-
-    // Corresponding questions
-    const questionsWithAnswer = riskAnalysisConfig.questions.filter(({ id }) =>
-      answerIds.includes(id)
-    )
-
-    const answers = questionsWithAnswer.map(({ label, options, id, visualType, infoLabel }) => {
-      const question = label[currentLanguage]
-
-      const questionInfoLabel = infoLabel?.[currentLanguage]
-
-      // Get the value of the answer
-      // The value can be of three types: plain text, multiple options, single option
-      const answerValue = riskAnalysisTemplate[id]
-
-      // Plain text: this value comes from a text field
-      if (visualType === 'text') {
-        return { question, answer: answerValue[0] }
-      }
-
-      // Multiple options: this value comes from a multiple choice checkbox
-      const selectedOptions = options?.filter(({ value }) => answerValue.includes(String(value)))
-      const answer = selectedOptions?.map((o) => o.label[currentLanguage]).join(', ') ?? ''
-
-      return { question, answer, questionInfoLabel }
-    })
-
-    return answers
-  }, [riskAnalysisTemplate, riskAnalysisConfig, currentLanguage])
-
-  if (!riskAnalysisTemplate) return null
-
-  return (
-    <SectionContainer innerSection title={t('riskAnalysis.title')}>
-      <List>
-        {questions.map(({ question, answer, questionInfoLabel }, i) => (
-          <ListItem key={i} sx={{ pl: 0 }}>
-            <ListItemText>
-              <Typography variant="body2">{question}</Typography>
-              {questionInfoLabel && (
-                <Typography variant="caption" color="grey" component="p">
-                  {questionInfoLabel}
-                </Typography>
-              )}
-              <Typography variant="body2" fontWeight={600} mt={0.5}>
-                {answer}
-              </Typography>
-            </ListItemText>
-          </ListItem>
-        ))}
-      </List>
-    </SectionContainer>
   )
 }
 
