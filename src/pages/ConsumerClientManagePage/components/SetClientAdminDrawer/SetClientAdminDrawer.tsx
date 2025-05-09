bacffd7d8322fa8fdbd59f904cf2fed23aecba5e
alten-dturus <davide.turus@alten.it>
2025-05-09 15:21:29 +0200

    Implemented new setClientAdminDrawer

---
+++ src/pages/ConsumerClientManagePage/components/SetClientAdminDrawer/SetClientAdminDrawer.tsx
@@ -0,0 +1,133 @@
+import type { CompactUser } from '@/api/api.generatedTypes'
+import { ClientMutations, ClientQueries } from '@/api/client'
+import { Drawer } from '@/components/shared/Drawer'
+import { RHFAutocompleteSingle } from '@/components/shared/react-hook-form-inputs'
+import { Box } from '@mui/material'
+import { useAutocompleteTextInput } from '@pagopa/interop-fe-commons'
+import { useQuery } from '@tanstack/react-query'
+import React from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+
+type SetClientAdminFormValues = {
+  selectedAdminId: string
+}
+
+type SetClientAdminDrawerProps = {
+  isOpen: boolean
+  onClose: VoidFunction
+  clientId: string
+  admin?: CompactUser
+}
+
+export const SetClientAdminDrawer: React.FC<SetClientAdminDrawerProps> = ({
+  isOpen,
+  onClose,
+  clientId,
+  admin,
+}) => {
+  const { t } = useTranslation('client', { keyPrefix: 'edit.setClientAdminDrawer' })
+  const { t: tCommon } = useTranslation('common')
+
+  const { mutate: setClientAdmin } = ClientMutations.useSetClientAdmin()
+
+  const selectedAdminRef = React.useRef<CompactUser | undefined>(admin)
+  const hasSetAdmin = React.useRef(Boolean(admin))
+
+  const formMethods = useForm<SetClientAdminFormValues>({
+    defaultValues: {
+      selectedAdminId: admin?.userId,
+    },
+  })
+
+  const { setValue, watch } = formMethods
+  const [adminAutocompleteTextInput, setAdminAutocompleteTextInput] = useAutocompleteTextInput(
+    admin?.name ?? ''
+  )
+
+  const selectedAdminId = watch('selectedAdminId')
+
+  /**
+   * TEMP: This is a workaround to avoid the "q" param in the query to be equal to the selected attribute name.
+   */
+  function getQ() {
+    let result = adminAutocompleteTextInput
+
+    if (selectedAdminRef.current && adminAutocompleteTextInput === selectedAdminRef.current.name) {
+      result = ''
+    }
+
+    return result
+  }
+
+  const { data: users = [], isLoading } = useQuery({
+    ...ClientQueries.getOperatorsList({
+      clientId: clientId,
+      name: getQ(),
+    }),
+    enabled: Boolean(clientId),
+    select: (results) => results ?? [],
+  })
+
+  // React.useEffect(() => {
+  //   if (!selectedAdminId && !hasSetAdmin.current && users && users?.length > 0) {
+  //     setValue('selectedAdminId', users[0].userId)
+  //     setAdminAutocompleteTextInput(users[0].name)
+  //     selectedAdminRef.current = users[0]
+  //     hasSetAdmin.current = true
+  //   }
+  // }, [setValue, selectedAdminId, hasSetAdmin, users, setAdminAutocompleteTextInput])
+
+  const handleCloseDrawer = () => {
+    onClose()
+  }
+
+  const autocompleteOptions = users.map((user) => ({
+    label: user.name,
+    value: user.userId,
+  }))
+
+  const onSubmit = (values: SetClientAdminFormValues) => {
+    setClientAdmin(
+      { clientId: clientId, payload: { adminId: values.selectedAdminId } },
+      { onSuccess: handleCloseDrawer }
+    )
+  }
+
+  const handleTransitionExited = () => {
+    formMethods.reset()
+  }
+
+  return (
+    <FormProvider {...formMethods}>
+      <Drawer
+        isOpen={isOpen}
+        onClose={handleCloseDrawer}
+        onTransitionExited={handleTransitionExited}
+        title={admin ? t('title.substitute') : t('title.select')}
+        subtitle={t('subtitle')}
+        buttonAction={{
+          label: admin ? tCommon('actions.substituteAdmin') : tCommon('actions.selectAdmin'),
+          action: formMethods.handleSubmit(onSubmit),
+        }}
+      >
+        <Box sx={{ mt: 3 }} component="form" noValidate>
+          <RHFAutocompleteSingle
+            focusOnMount
+            label={t('adminField.label')}
+            infoLabel={t('adminField.infoLabel')}
+            sx={{ my: 0 }}
+            onInputChange={(_, value) => setAdminAutocompleteTextInput(value)}
+            onValueChange={(value) =>
+              (selectedAdminRef.current = users.find((user) => user.userId === value?.value))
+            }
+            name="selectedAdminId"
+            rules={{ required: true }}
+            options={autocompleteOptions}
+            loading={isLoading}
+          />
+        </Box>
+      </Drawer>
+    </FormProvider>
+  )
+}
