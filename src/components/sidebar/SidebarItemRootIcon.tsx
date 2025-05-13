c32c8d03d8a8969b82fd92d6f4e9d054fb73a463
borgesis95 <salvatore.borgesi@pagopa.it>
2025-05-13 12:13:14 +0200

    feat: added tooltipLabel

--- src/components/sidebar/SidebarItemRootIcon.tsx
+++ src/components/sidebar/SidebarItemRootIcon.tsx
@@ -1,22 +1,27 @@
+import React from 'react'
 import { type SvgIconComponent } from '@mui/icons-material'
-import { ListItemIcon, Badge } from '@mui/material'
+import { ListItemIcon, Badge, Tooltip } from '@mui/material'
 import type { Notification } from './sidebar.types'
 
 type SidebarRootIconProps = {
   Icon: SvgIconComponent
   collapsed: boolean
   notification?: Notification
+  tooltipLabel: string
 }
 
 export const SidebarRootIcon: React.FC<SidebarRootIconProps> = ({
   Icon,
   collapsed,
   notification,
+  tooltipLabel,
 }) => {
   return !collapsed || !notification || notification?.content <= 0 ? (
-    <ListItemIcon>
-      <Icon fontSize="inherit" />
-    </ListItemIcon>
+    <Tooltip title={tooltipLabel}>
+      <ListItemIcon>
+        <Icon fontSize="inherit" />
+      </ListItemIcon>
+    </Tooltip>
   ) : (
     <ListItemIcon>
       <Badge color="primary" badgeContent={notification.content} variant="dot">
