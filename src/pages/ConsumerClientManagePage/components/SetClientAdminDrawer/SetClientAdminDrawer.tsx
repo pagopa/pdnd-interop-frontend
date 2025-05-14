fc385b798c2779fe1b9fb0e3399a73f1c14ea8f7
alten-dturus <davide.turus@alten.it>
2025-05-14 14:51:11 +0200

    removed name filter

--- src/pages/ConsumerClientManagePage/components/SetClientAdminDrawer/SetClientAdminDrawer.tsx
+++ src/pages/ConsumerClientManagePage/components/SetClientAdminDrawer/SetClientAdminDrawer.tsx
@@ -3,7 +3,6 @@ import { ClientMutations, ClientQueries } from '@/api/client'
 import { Drawer } from '@/components/shared/Drawer'
 import { RHFAutocompleteSingle } from '@/components/shared/react-hook-form-inputs'
 import { Box } from '@mui/material'
-import { useAutocompleteTextInput } from '@pagopa/interop-fe-commons'
 import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
@@ -31,39 +30,15 @@ export const SetClientAdminDrawer: React.FC<SetClientAdminDrawerProps> = ({
 
   const { mutate: setClientAdmin } = ClientMutations.useSetClientAdmin()
 
-  const selectedAdminRef = React.useRef<CompactUser | undefined>(admin)
-
   const formMethods = useForm<SetClientAdminFormValues>({
     defaultValues: {
       selectedAdminId: admin?.userId,
     },
   })
 
-  const [adminAutocompleteTextInput, setAdminAutocompleteTextInput] = useAutocompleteTextInput(
-    admin ? `${admin.name} ${admin.familyName}` : ''
-  )
-
-  /**
-   * TEMP: This is a workaround to avoid the "q" param in the query to be equal to the selected attribute name.
-   */
-  function getQ() {
-    let result = adminAutocompleteTextInput
-
-    if (
-      selectedAdminRef.current &&
-      adminAutocompleteTextInput ===
-        `${selectedAdminRef.current.name} ${selectedAdminRef.current.familyName}`
-    ) {
-      result = ''
-    }
-
-    return result
-  }
-
   const { data: users = [], isLoading } = useQuery({
     ...ClientQueries.getOperatorsList({
       clientId: clientId,
-      name: getQ(),
     }),
     enabled: Boolean(clientId),
     select: (results) => results ?? [],
@@ -108,10 +83,6 @@ export const SetClientAdminDrawer: React.FC<SetClientAdminDrawerProps> = ({
             label={t('adminField.label')}
             infoLabel={t('adminField.infoLabel')}
             sx={{ my: 0 }}
-            onInputChange={(_, value) => setAdminAutocompleteTextInput(value)}
-            onValueChange={(value) =>
-              (selectedAdminRef.current = users.find((user) => user.userId === value?.value))
-            }
             name="selectedAdminId"
             rules={{ required: true }}
             options={autocompleteOptions}
