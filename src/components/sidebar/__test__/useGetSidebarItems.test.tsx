894b88538b72ecfeb1c720ccb1064a7718f2fdf5
borgesis95 <salvatore.borgesi@pagopa.it>
2025-05-15 15:26:55 +0200

    test: add test for useGetSidebarItems

---
+++ src/components/sidebar/__test__/useGetSidebarItems.test.tsx
@@ -0,0 +1,78 @@
+import { renderHook } from '@testing-library/react'
+import { useGetSidebarItems } from '../useGetSidebarItems'
+import { mockUseGetActiveUserParty, mockUseJwt } from '@/utils/testing.utils'
+import { CatalogIcon } from '@/assets/CatalogIcon'
+import { SidebarRoutes } from '../sidebar.types'
+
+const interopSidenavRoutes: SidebarRoutes = [
+  {
+    rootRouteKey: 'SUBSCRIBE_CATALOG_LIST',
+    icon: CatalogIcon,
+    children: [],
+    divider: true,
+  },
+  {
+    showNotification: false,
+    icon: CatalogIcon,
+    label: 'Fruizione',
+    rootRouteKey: 'PROVIDE_AGREEMENT_LIST',
+    children: [
+      { to: 'PROVIDE_AGREEMENT_LIST', label: 'Richieste inoltrate' },
+      { to: 'PROVIDE_PURPOSE_LIST', label: 'Finalità inoltrate' },
+    ],
+    divider: false,
+  },
+]
+
+const routes = vi.mock('@/router', () => ({
+  routes: {
+    SUBSCRIBE_CATALOG_LIST: { authLevels: ['admin'] },
+    PROVIDE_AGREEMENT_LIST: { authLevels: ['admin'] },
+    PROVIDE_PURPOSE_LIST: { authLevels: ['admin'] },
+  },
+  RouteKey: {},
+}))
+
+describe('useGetSidebarItems', () => {
+  it('should return empty navigation list if jwt not contain roles', () => {
+    mockUseJwt({ currentRoles: [] })
+    mockUseGetActiveUserParty()
+    const { result } = renderHook(() => useGetSidebarItems(interopSidenavRoutes))
+    expect(result.current).toEqual([])
+  })
+
+  it('should return the entire navigation list for admin', () => {
+    mockUseJwt({ currentRoles: ['admin'], isOrganizationAllowedToProduce: true })
+    mockUseGetActiveUserParty()
+    const { result } = renderHook(() => useGetSidebarItems(interopSidenavRoutes))
+
+    expect(result.current.length).toBe(2)
+    expect(result.current).toMatchInlineSnapshot(`
+      [
+        {
+          "children": [],
+          "divider": true,
+          "icon": [Function],
+          "rootRouteKey": "SUBSCRIBE_CATALOG_LIST",
+        },
+        {
+          "children": [
+            {
+              "label": "Richieste inoltrate",
+              "to": "PROVIDE_AGREEMENT_LIST",
+            },
+            {
+              "label": "Finalità inoltrate",
+              "to": "PROVIDE_PURPOSE_LIST",
+            },
+          ],
+          "divider": false,
+          "icon": [Function],
+          "label": "Fruizione",
+          "rootRouteKey": "PROVIDE_AGREEMENT_LIST",
+          "showNotification": false,
+        },
+      ]
+    `)
+  })
+})
