36e179ed0aeef49b6472705698aaa0276c9b1d9e
borgesis95 <salvatore.borgesi@pagopa.it>
2025-05-15 15:26:55 +0200

    test: add test for useGetSidebarItems

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -4,18 +4,76 @@ import type { SxProps } from '@mui/material'
 import type { SidebarRoutes } from '../sidebar/sidebar.types'
 import { Sidebar } from '../sidebar/Sidebar'
 import { useGetSidebarItems } from '../sidebar/useGetSidebarItems'
-
+import DnsIcon from '@mui/icons-material/Dns'
+import PeopleIcon from '@mui/icons-material/People'
+import { CatalogIcon } from '@/assets/CatalogIcon'
+import { ConsumerIcon, ProviderIcon } from '@/assets'
 type AppLayoutProps = {
   children: React.ReactNode
   hideSideNav?: boolean
   sx?: SxProps
 }
 
+const INTEROP_NAVIGATION_ROUTES: SidebarRoutes = [
+  {
+    rootRouteKey: 'SUBSCRIBE_CATALOG_LIST',
+    icon: CatalogIcon,
+    children: [],
+    divider: true,
+  },
+  {
+    showNotification: false,
+    icon: ConsumerIcon,
+    label: 'Fruizione',
+    rootRouteKey: 'PROVIDE_AGREEMENT_LIST',
+    children: [
+      { to: 'PROVIDE_AGREEMENT_LIST', label: 'Richieste inoltrate' },
+      { to: 'SUBSCRIBE_PURPOSE_LIST', label: 'Finalità inoltrate' },
+    ],
+    divider: false,
+  },
+  {
+    icon: ProviderIcon,
+    label: 'Erogazione',
+    rootRouteKey: 'PROVIDE_ESERVICE_LIST',
+    hide: false,
+    children: [
+      { to: 'PROVIDE_ESERVICE_LIST', label: 'I Miei E-service' },
+      { to: 'PROVIDE_ESERVICE_TEMPLATE_LIST', label: 'Template E-Service' },
+      { to: 'PROVIDE_AGREEMENT_LIST', label: 'Richieste di fruizione' },
+      { to: 'PROVIDE_PURPOSE_LIST', label: 'Finalità ricevute' },
+    ],
+  },
+  {
+    icon: DnsIcon,
+    rootRouteKey: 'PROVIDE_KEYCHAINS_LIST',
+    label: 'Gestione del client',
+    hide: false,
+    children: [
+      { to: 'PROVIDE_KEYCHAINS_LIST', label: 'Portachiavi' },
+      { to: 'SUBSCRIBE_CLIENT_LIST', label: 'API fruizione' },
+      { to: 'SUBSCRIBE_INTEROP_M2M', label: 'API Fruizione Interop' },
+      { to: 'SUBSCRIBE_DEBUG_VOUCHER', label: 'Debug client assertion' },
+    ],
+  },
+  {
+    icon: PeopleIcon,
+    label: 'Il mio ente',
+    rootRouteKey: 'PARTY_REGISTRY',
+    children: [
+      { to: 'PARTY_REGISTRY', label: 'Anagrafica e attributi' },
+      {
+        to: 'DELEGATIONS',
+        hide: false,
+        label: 'Gestione delle deleghe',
+      },
+    ],
+  },
+]
+
 export const AppLayout: React.FC<AppLayoutProps> = ({ children, hideSideNav, sx }) => {
   const theme = useTheme()
-  const interopRoutes = useGetSidebarItems()
-
-  console.log('routes', interopRoutes)
+  const interopRoutes = useGetSidebarItems(INTEROP_NAVIGATION_ROUTES)
 
   const matchMobile = useMediaQuery(theme.breakpoints.down('sm'))
 
