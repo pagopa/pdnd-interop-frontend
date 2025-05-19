f3c6bd31753296ec651f9c64ef8ab8ca66f8cb15
Carmine Porricelli <carmine.porricelli@pagopa.it>
2025-05-19 15:41:04 +0200

    Update open-api-type-generator.js

--- scripts/open-api-type-generator.js
+++ scripts/open-api-type-generator.js
@@ -2,7 +2,7 @@ import { generateApi } from 'swagger-typescript-api'
 import path from 'path'
 
 const openApiSpecificationFileUrl =
-  'https://raw.githubusercontent.com/pagopa/interop-be-monorepo/145acd61839d96c9373fd58c04a7a07c3e17b5d6/packages/api-clients/open-api/bffApi.yml'
+  'https://raw.githubusercontent.com/pagopa/interop-be-monorepo/refs/heads/develop/packages/api-clients/open-api/bffApi.yml'
 
 const apiFolderPath = path.resolve('./src/api/')
 
