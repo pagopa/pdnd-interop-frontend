e8262fbe13e938dc627dd1e575b9ce5a8dda0ad1
martinaCampoli <martina.campoli@pagopa.it>
2025-06-17 15:09:11 +0200

    Added step purpose for eservice created from template

---
+++ src/pages/ProviderEServiceCreatePage/components/EServiceCreateStepPurpose/EServiceCreateFromTemplateStepPurpose.tsx
@@ -0,0 +1,95 @@
+import { SectionContainer, SectionContainerSkeleton } from '@/components/layout/containers'
+import { Button, Stack } from '@mui/material'
+import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useEServiceCreateContext } from '../EServiceCreateContext'
+import { RHFAutocompleteSingle } from '@/components/shared/react-hook-form-inputs'
+import { FormProvider, useForm } from 'react-hook-form'
+import { StepActions } from '@/components/shared/StepActions'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+import { RiskAnalysisInfoSummary } from '@/components/shared/RiskAnalysisInfoSummary'
+
+export const EServiceCreateFromTemplateStepPurpose: React.FC = () => {
+  const { t } = useTranslation('eservice', { keyPrefix: 'create' })
+  const { t: tCommon } = useTranslation('common')
+
+  const { descriptor, forward, back } = useEServiceCreateContext()
+
+  const methods = useForm()
+
+  const riskAnalysisList = descriptor?.eservice.riskAnalysis ?? []
+
+  const autocompleteOptions = (riskAnalysisList ?? []).map((riskAnalysis) => ({
+    label: riskAnalysis.name,
+    value: riskAnalysis.id,
+  }))
+
+  const [selectedRiskAnalysis, setSelectedRiskAnalysis] = useState({ label: '', value: '' })
+  const [showRiskAnalysis, setShowRiskAnalisys] = useState(false)
+
+  const resetStates = () => {
+    setShowRiskAnalisys(false)
+    setSelectedRiskAnalysis({ label: '', value: '' })
+  }
+
+  return (
+    <FormProvider {...methods}>
+      <SectionContainer
+        title={t('stepPurpose.purposeTableSection.title')}
+        description={t('stepPurpose.purposeTableSection.descriptionEServiceFromTemplate')}
+      >
+        <Stack spacing={2} alignItems="flex-start">
+          <RHFAutocompleteSingle
+            sx={{ my: 0 }}
+            name="riskAnalysis"
+            label={t('stepPurpose.purposeTableSection.labelAutocompleteEServiceFromTemplate')}
+            options={autocompleteOptions}
+            onValueChange={(value) =>
+              value === null ? resetStates() : setSelectedRiskAnalysis(value)
+            }
+            rules={{ required: true }}
+          />
+          <Button
+            variant="contained"
+            disabled={selectedRiskAnalysis.value === ''}
+            onClick={() => setShowRiskAnalisys(true)}
+          >
+            {tCommon('actions.inspect')}
+          </Button>
+        </Stack>
+      </SectionContainer>
+      {showRiskAnalysis && (
+        <SectionContainer title={selectedRiskAnalysis.label}>
+          <RiskAnalysisInfoSummary
+            eServiceId={descriptor?.eservice.id as string}
+            riskAnalysisId={selectedRiskAnalysis.value}
+          />
+        </SectionContainer>
+      )}
+      <StepActions
+        back={{
+          label: t('backWithoutSaveBtn'),
+          type: 'button',
+          onClick: back,
+          startIcon: <ArrowBackIcon />,
+        }}
+        forward={{
+          label: t('forwardWithoutSaveBtn'),
+          type: 'button',
+          onClick: forward,
+          endIcon: <ArrowForwardIcon />,
+        }}
+      />
+    </FormProvider>
+  )
+}
+
+export const EServiceCreateFromTemplateStepPurposeSkeleton: React.FC = () => {
+  return (
+    <>
+      <SectionContainerSkeleton height={365} />
+      <SectionContainerSkeleton height={178} />
+    </>
+  )
+}
