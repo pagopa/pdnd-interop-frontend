4d593ddbacfed4534a491ecf7bba01a943aef4e5
borgesis95 <salvatore.borgesi@pagopa.it>
2025-06-26 16:16:06 +0200

    chore: added sidebar.json for translation (it,en)

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -9,83 +9,87 @@ import { CatalogIcon } from '@/assets/CatalogIcon'
 import { ConsumerIcon, ProviderIcon } from '@/assets'
 import { DeveloperToolIcon } from '@/assets/DeveloperToolIcon'
 import { MyTenantIcon } from '@/assets/MyTenantIcon'
+import { useTranslation } from 'react-i18next'
 type AppLayoutProps = {
   children: React.ReactNode
   hideSideNav?: boolean
   sx?: SxProps
 }
 
-const INTEROP_NAVIGATION_ROUTES: SidebarRoutes = [
-  {
-    rootRouteKey: 'SUBSCRIBE_CATALOG_LIST',
-    icon: CatalogIcon,
-    children: [],
-    divider: true,
-  },
-  {
-    showNotification: false,
-    icon: ConsumerIcon,
-    label: 'Fruizione',
-    rootRouteKey: 'PROVIDE_AGREEMENT_LIST',
-    children: [
-      { to: 'SUBSCRIBE_AGREEMENT_LIST', label: 'Richieste inoltrate' },
-      { to: 'SUBSCRIBE_PURPOSE_LIST', label: 'Finalità inoltrate' },
-    ],
-    divider: false,
-  },
-  {
-    icon: ProviderIcon,
-    label: 'Erogazione',
-    rootRouteKey: 'PROVIDE_ESERVICE_LIST',
-    hide: false,
-    children: [
-      { to: 'PROVIDE_ESERVICE_LIST', label: 'I miei e-service' },
-      { to: 'PROVIDE_AGREEMENT_LIST', label: 'Richieste ricevute' },
-      { to: 'PROVIDE_PURPOSE_LIST', label: 'Finalità ricevute' },
-      { to: 'PROVIDE_ESERVICE_TEMPLATE_CATALOG', label: 'Template e-service' },
-      { to: 'PROVIDE_ESERVICE_TEMPLATE_LIST', label: 'I miei template' },
-      { to: 'PROVIDE_KEYCHAINS_LIST', label: 'I miei portachiavi erogatore' },
-    ],
-  },
-  {
-    icon: DnsIcon,
-    rootRouteKey: 'PROVIDE_KEYCHAINS_LIST',
-    label: 'Gestione del client',
-    hide: false,
-    children: [
-      { to: 'SUBSCRIBE_CLIENT_LIST', label: 'API e-service' },
-      { to: 'SUBSCRIBE_INTEROP_M2M', label: 'API interoperabilità' },
-      { to: 'SUBSCRIBE_DEBUG_VOUCHER', label: 'Debug client assertion' },
-    ],
-  },
-  {
-    icon: MyTenantIcon,
-    label: 'Il mio ente',
-    rootRouteKey: 'PARTY_REGISTRY',
-    children: [
-      { to: 'PARTY_REGISTRY', label: 'Anagrafica e attributi' },
-      {
-        to: 'TENANT_CERTIFIER',
-        hide: false,
-        label: 'Ente certificatore',
-      },
-      {
-        to: 'DELEGATIONS',
-        hide: false,
-        label: 'Gestione delle deleghe',
-      },
-    ],
-  },
-  {
-    icon: DeveloperToolIcon,
-    rootRouteKey: 'DEVELOPER_TOOLS',
-    label: 'Tool per lo sviluppo ',
-    hide: false,
-    children: [],
-  },
-]
-
 export const AppLayout: React.FC<AppLayoutProps> = ({ children, hideSideNav, sx }) => {
+  const { t } = useTranslation('sidebar', { keyPrefix: 'menuItem' })
+
+  const INTEROP_NAVIGATION_ROUTES: SidebarRoutes = [
+    {
+      rootRouteKey: 'SUBSCRIBE_CATALOG_LIST',
+      icon: CatalogIcon,
+      label: t('eserviceCatalog'),
+      children: [],
+      divider: true,
+    },
+    {
+      showNotification: false,
+      icon: ConsumerIcon,
+      label: t('subscribe.subscribe'),
+      rootRouteKey: 'PROVIDE_AGREEMENT_LIST',
+      children: [
+        { to: 'SUBSCRIBE_AGREEMENT_LIST', label: t('subscribe.agreement') },
+        { to: 'SUBSCRIBE_PURPOSE_LIST', label: t('subscribe.purpose') },
+      ],
+      divider: false,
+    },
+    {
+      icon: ProviderIcon,
+      label: t('provider.provider'),
+      rootRouteKey: 'PROVIDE_ESERVICE_LIST',
+      hide: false,
+      children: [
+        { to: 'PROVIDE_ESERVICE_LIST', label: t('provider.providerEservice') },
+        { to: 'PROVIDE_AGREEMENT_LIST', label: t('provider.providerAgreeemnt') },
+        { to: 'PROVIDE_PURPOSE_LIST', label: t('provider.providerPurpose') },
+        { to: 'PROVIDE_ESERVICE_TEMPLATE_CATALOG', label: t('provider.eServiceTemplatesCatalog') },
+        { to: 'PROVIDE_ESERVICE_TEMPLATE_LIST', label: t('provider.providerEserviceTemplate') },
+        { to: 'PROVIDE_KEYCHAINS_LIST', label: t('provider.providerKeychainsList') },
+      ],
+    },
+    {
+      icon: DnsIcon,
+      rootRouteKey: 'PROVIDE_KEYCHAINS_LIST',
+      label: t('client.client'),
+      hide: false,
+      children: [
+        { to: 'SUBSCRIBE_CLIENT_LIST', label: t('client.eservice') },
+        { to: 'SUBSCRIBE_INTEROP_M2M', label: t('client.interop') },
+        { to: 'SUBSCRIBE_DEBUG_VOUCHER', label: t('client.debugClientAssertion') },
+      ],
+    },
+    {
+      icon: MyTenantIcon,
+      label: t('tenant.tenant'),
+      rootRouteKey: 'PARTY_REGISTRY',
+      children: [
+        { to: 'PARTY_REGISTRY', label: t('tenant.partyRegistry') },
+        {
+          to: 'TENANT_CERTIFIER',
+          hide: false,
+          label: t('tenant.tenantCertifier'),
+        },
+        {
+          to: 'DELEGATIONS',
+          hide: false,
+          label: t('tenant.delegations'),
+        },
+      ],
+    },
+    {
+      icon: DeveloperToolIcon,
+      rootRouteKey: 'DEVELOPER_TOOLS',
+      label: 'Tool per lo sviluppo ',
+      hide: false,
+      children: [],
+    },
+  ]
+
   const theme = useTheme()
   const interopRoutes = useGetSidebarItems(INTEROP_NAVIGATION_ROUTES)
   const matchMobile = useMediaQuery(theme.breakpoints.down('sm'))
