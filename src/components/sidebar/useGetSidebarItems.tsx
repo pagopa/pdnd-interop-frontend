aed1841672f0104a1292006d9cf04f2b5a973e88
borgesis95 <salvatore.borgesi@pagopa.it>
2025-07-08 14:38:19 +0200

    refactor: start to move BL

--- src/components/sidebar/useGetSidebarItems.tsx
+++ src/components/sidebar/useGetSidebarItems.tsx
@@ -6,47 +6,110 @@ import { isTenantCertifier } from '@/utils/tenant.utils'
 import React from 'react'
 import type { RouteKey } from '@/router'
 import { routes } from '@/router'
+import DnsIcon from '@mui/icons-material/Dns'
+import { CatalogIcon } from '@/assets/CatalogIcon'
+import { ConsumerIcon, ProviderIcon } from '@/assets'
+import { DeveloperToolIcon } from '@/assets/DeveloperToolIcon'
+import { MyTenantIcon } from '@/assets/MyTenantIcon'
+import { useTranslation } from 'react-i18next'
 
-export function useGetSidebarItems(interopRoutes: SidebarRoutes): SidebarRoutes {
+export function useGetSidebarItems(): SidebarRoutes {
+  const { t } = useTranslation('sidebar', { keyPrefix: 'menuItem' })
   const { currentRoles, isSupport, isOrganizationAllowedToProduce } = AuthHooks.useJwt()
 
   const { data: tenant } = TenantHooks.useGetActiveUserParty()
 
-  const isCertifier = isTenantCertifier(tenant)
-
   return React.useMemo(() => {
-    /**
-     * Checks if the user as the authorization level required to access a given route.
-     * The no-IPA organizations cannot access the PROVIDE routes.
-     * The no-certifier organizations cannot access the TENANT_CERTIFIER routes.
-     * The no-PA organizations cannot access the DELEGATIONS route.
-     */
-    const isAuthorizedToRoute = (routeKey: RouteKey) => {
-      if (!isSupport && !isOrganizationAllowedToProduce && routeKey === 'PROVIDE') return false
+    const interopRoutes: SidebarRoutes = [
+      {
+        rootRouteKey: 'SUBSCRIBE_CATALOG_LIST',
+        icon: CatalogIcon,
+        label: t('eserviceCatalog'),
+        children: [],
+        divider: true,
+      },
+      {
+        showNotification: false,
+        icon: ConsumerIcon,
+        label: t('subscribe.subscribe'),
+        rootRouteKey: 'PROVIDE_AGREEMENT_LIST',
+        children: [
+          { to: 'SUBSCRIBE_AGREEMENT_LIST', label: t('subscribe.agreement') },
+          { to: 'SUBSCRIBE_PURPOSE_LIST', label: t('subscribe.purpose') },
+        ],
+        divider: false,
+      },
+      {
+        icon: ProviderIcon,
+        label: t('provider.provider'),
+        rootRouteKey: 'PROVIDE_ESERVICE_LIST',
+        hide: !isSupport && !isOrganizationAllowedToProduce,
+        children: [
+          { to: 'PROVIDE_ESERVICE_LIST', label: t('provider.providerEservice') },
+          { to: 'PROVIDE_AGREEMENT_LIST', label: t('provider.providerAgreeemnt') },
+          { to: 'PROVIDE_PURPOSE_LIST', label: t('provider.providerPurpose') },
+          {
+            to: 'PROVIDE_ESERVICE_TEMPLATE_CATALOG',
+            label: t('provider.eServiceTemplatesCatalog'),
+          },
+          { to: 'PROVIDE_ESERVICE_TEMPLATE_LIST', label: t('provider.providerEserviceTemplate') },
+          { to: 'PROVIDE_KEYCHAINS_LIST', label: t('provider.providerKeychainsList') },
+        ],
+      },
+      {
+        icon: DnsIcon,
+        rootRouteKey: 'PROVIDE_KEYCHAINS_LIST',
+        label: t('client.client'),
+        hide: false,
+        children: [
+          { to: 'SUBSCRIBE_CLIENT_LIST', label: t('client.eservice') },
+          { to: 'SUBSCRIBE_INTEROP_M2M', label: t('client.interop') },
+          { to: 'SUBSCRIBE_DEBUG_VOUCHER', label: t('client.debugClientAssertion') },
+        ],
+      },
+      {
+        icon: MyTenantIcon,
+        label: t('tenant.tenant'),
+        rootRouteKey: 'PARTY_REGISTRY',
+        children: [
+          { to: 'PARTY_REGISTRY', label: t('tenant.partyRegistry') },
+          {
+            to: 'TENANT_CERTIFIER',
+            hide: !isTenantCertifier(tenant),
+            label: t('tenant.tenantCertifier'),
+          },
+          {
+            to: 'DELEGATIONS',
+            hide: !isOrganizationAllowedToProduce,
+            label: t('tenant.delegations'),
+          },
+        ],
+      },
+      {
+        icon: DeveloperToolIcon,
+        rootRouteKey: 'DEVELOPER_TOOLS',
+        label: 'Tool per lo sviluppo ',
+        hide: false,
+        children: [],
+      },
+    ]
 
-      if (!isCertifier && routeKey === 'TENANT_CERTIFIER') return false
-
-      if (!isOrganizationAllowedToProduce && routeKey === 'DELEGATIONS') return false
-
+    const userHasRouteRoles = (routeKey: RouteKey) => {
       const authLevels = routes[routeKey].authLevels
       return authLevels.some((authLevel) => currentRoles.includes(authLevel))
     }
 
-    /**
-     * Filters out views that the user is not authorized to access
-     */
-
     return interopRoutes.reduce<SidebarRoutes>((acc, item) => {
       // If the view is not authorized, we don't need to check its children
-      if (!isAuthorizedToRoute(item.rootRouteKey)) return acc
+      if (!userHasRouteRoles(item.rootRouteKey)) return acc
 
       // If the view has children, we need to filter out the ones that the user is not authorized to access
       if ('children' in item) {
-        const children = [...item.children!].filter(() => isAuthorizedToRoute(item.rootRouteKey))
+        const children = [...item.children!].filter(() => userHasRouteRoles(item.rootRouteKey))
 
         return [...acc, { ...item, children }]
       }
       return [...acc, item]
     }, [])
-  }, [currentRoles, isOrganizationAllowedToProduce, isSupport, isCertifier])
+  }, [currentRoles, isOrganizationAllowedToProduce, isSupport, t, tenant])
 }
