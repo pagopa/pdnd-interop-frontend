ab3c7d4aa8e9d62a99288a1d1d1477a8ed58b43e
borgesis95 <salvatore.borgesi@pagopa.it>
2025-07-22 10:31:27 +0200

    refactor: removed tooltipLabel from SidebarIcon

--- src/components/sidebar/SidebarIcon.tsx
+++ src/components/sidebar/SidebarIcon.tsx
@@ -1,26 +1,23 @@
 import React from 'react'
 import { type SvgIconComponent } from '@mui/icons-material'
-import { ListItemIcon, Badge, Tooltip } from '@mui/material'
+import { ListItemIcon, Badge } from '@mui/material'
 import type { Notification } from './sidebar.types'
 
 type SidebarIconProps = {
   Icon: SvgIconComponent
   notification?: Notification
-  tooltipLabel: string
 }
 
-export const SidebarIcon: React.FC<SidebarIconProps> = ({ Icon, notification, tooltipLabel }) => {
+export const SidebarIcon: React.FC<SidebarIconProps> = ({ Icon, notification }) => {
   return (
-    <Tooltip data-testid="sidebar-icon" title={tooltipLabel} placement="right">
-      <ListItemIcon>
-        {!notification || notification?.content <= 0 ? (
+    <ListItemIcon>
+      {!notification || notification?.content <= 0 ? (
+        <Icon fontSize="inherit" />
+      ) : (
+        <Badge color="primary" badgeContent={notification.content} variant="dot">
           <Icon fontSize="inherit" />
-        ) : (
-          <Badge color="primary" badgeContent={notification.content} variant="dot">
-            <Icon fontSize="inherit" />
-          </Badge>
-        )}
-      </ListItemIcon>
-    </Tooltip>
+        </Badge>
+      )}
+    </ListItemIcon>
   )
 }
