39e5c04d2f6316e43ee2db4378f5170c81ec6644
borgesis95 <salvatore.borgesi@pagopa.it>
2025-07-23 10:51:23 +0200

    feat: added several components

--- src/pages/NotificationConfigPage/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as NotificationConfigPage } from './NotificationConfig.page'
---
+++ src/pages/NotificationUserConfigPage/components/NotificationConfigSection.tsx
@@ -0,0 +1,54 @@
+import { SectionContainer } from '@/components/layout/containers'
+import { RHFRadioGroup, RHFSwitch } from '@/components/shared/react-hook-form-inputs'
+import { Stack } from '@mui/system'
+import { useFormContext } from 'react-hook-form'
+import type { NotificationSectionSchema } from './EmailNotificationUserConfigTab'
+
+type NotificationConfigSectionProps = {
+  section: NotificationSectionSchema
+}
+export const NotificationConfigSection: React.FC<NotificationConfigSectionProps> = ({
+  section,
+}) => {
+  const { watch } = useFormContext()
+
+  const customizeNotification = watch('generalUpdate')
+
+  const renderSubSections = () => {
+    return section.subsections.map((subsection) => {
+      return (
+        <SectionContainer
+          innerSection
+          key={subsection.name}
+          title={subsection.title}
+          description={subsection.description}
+        >
+          <Stack ml={2}>
+            {subsection.components.map((component) => {
+              return (
+                <RHFSwitch
+                  key={component.name}
+                  name={component.name}
+                  label={component.description}
+                />
+              )
+            })}
+          </Stack>
+        </SectionContainer>
+      )
+    })
+  }
+
+  return (
+    <SectionContainer title={section.title} description={section.description}>
+      <RHFRadioGroup
+        name="generalUpdate"
+        options={[
+          { label: 'ricevi aggiornamenti', value: 'general' },
+          { label: 'personalizza gli aggiormaneti', value: 'custom' },
+        ]}
+      />
+      {customizeNotification === 'custom' && renderSubSections()}
+    </SectionContainer>
+  )
+}
