1f99a6c57a835d7230df83f1bd0164b9bbb8cd77
martinaCampoli <martina.campoli@pagopa.it>
2025-09-08 16:44:03 +0200

    Added default values for new purpose template

--- src/pages/ConsumerPurposeTemplateEditPage/components/PurposeTemplateEditStepGeneral/PurposeTemplateEditStepGeneral.tsx
+++ src/pages/ConsumerPurposeTemplateEditPage/components/PurposeTemplateEditStepGeneral/PurposeTemplateEditStepGeneral.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useParams } from '@/router'
-import { NotFoundError } from '@/utils/errors.utils'
 import type { ActiveStepProps } from '@/hooks/useActiveStep'
 import { useQuery } from '@tanstack/react-query'
 import { PurposeTemplateQueries } from '@/api/purposeTemplate/purposeTemplate.queries'
@@ -8,9 +7,13 @@ import type { PurposeTemplateEditStepGeneralFormValues } from './PurposeTemplate
 import PurposeTemplateEditStepGeneralForm, {
   PurposeTemplateEditStepGeneralFormSkeleton,
 } from './PurposeTemplateEditStepGeneralForm'
+import { useTranslation } from 'react-i18next'
+import { NotFoundError } from '@/utils/errors.utils'
 
 export const PurposeTemplateEditStepGeneral: React.FC<ActiveStepProps> = (props) => {
-  const { purposeTemplateId } = useParams<'SUBCRIBE_PURPOSE_TEMPLATE_EDIT'>()
+  const { t } = useTranslation('purposeTemplate', { keyPrefix: 'edit.defaultPurposeTemplate' })
+
+  const { purposeTemplateId } = useParams<'SUBSCRIBE_PURPOSE_TEMPLATE_EDIT'>()
   const { data: purposeTemplate, isLoading: isLoadingPurpose } = useQuery(
     PurposeTemplateQueries.getSingle(purposeTemplateId)
   )
@@ -24,11 +27,12 @@ export const PurposeTemplateEditStepGeneral: React.FC<ActiveStepProps> = (props)
   }
 
   const defaultValues: PurposeTemplateEditStepGeneralFormValues = {
-    title: purposeTemplate.purposeTitle,
-    description: purposeTemplate.purposeDescription,
-    dailyCalls: purposeTemplate.purposeDailyCalls,
-    isFreeOfCharge: purposeTemplate.purposeIsFreeOfCharge ? 'YES' : 'NO',
-    freeOfChargeReason: purposeTemplate.purposeFreeOfChargeReason,
+    title: (purposeTemplate && purposeTemplate.purposeTitle) ?? t('title'),
+    description: (purposeTemplate && purposeTemplate.purposeDescription) ?? t('description'),
+    dailyCalls: purposeTemplate?.purposeDailyCalls,
+    isFreeOfCharge: purposeTemplate?.purposeIsFreeOfCharge ? 'YES' : 'NO',
+    freeOfChargeReason:
+      (purposeTemplate && purposeTemplate.purposeFreeOfChargeReason) ?? t('freeOfChargeReason'),
   }
 
   return (
