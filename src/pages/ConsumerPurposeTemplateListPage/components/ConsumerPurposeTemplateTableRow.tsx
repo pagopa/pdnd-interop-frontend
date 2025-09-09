20a90340bfbda104b1ac482adbea07ed31415b1a
martinaCampoli <martina.campoli@pagopa.it>
2025-09-09 16:06:00 +0200

    Added inspect button

--- src/pages/ConsumerPurposeTemplateListPage/components/ConsumerPurposeTemplateTableRow.tsx
+++ src/pages/ConsumerPurposeTemplateListPage/components/ConsumerPurposeTemplateTableRow.tsx
@@ -2,14 +2,18 @@ import type { PurposeTemplate } from '@/api/purposeTemplate/mockedResponses'
 import { ActionMenu, ActionMenuSkeleton } from '@/components/shared/ActionMenu'
 import { ButtonSkeleton } from '@/components/shared/MUI-skeletons'
 import { StatusChip, StatusChipSkeleton } from '@/components/shared/StatusChip'
+import useGetConsumerPurposeTemplatesActions from '@/hooks/useGetConsumerPurposeTemplatesActions'
+import { Link } from '@/router'
 import { Skeleton } from '@mui/material'
 import { Box } from '@mui/system'
 import { TableRow } from '@pagopa/interop-fe-commons'
+import { useTranslation } from 'react-i18next'
 
 export const ConsumerPurposeTemplateTableRow: React.FC<{ purposeTemplate: PurposeTemplate }> = ({
   purposeTemplate,
 }) => {
-  //const { actions } =  useGetConsumerPurposesActions(purposeTemplate)
+  const { actions } = useGetConsumerPurposeTemplatesActions(purposeTemplate)
+  const { t: tCommon } = useTranslation('common')
 
   return (
     <TableRow
@@ -19,8 +23,20 @@ export const ConsumerPurposeTemplateTableRow: React.FC<{ purposeTemplate: Purpos
         <StatusChip key={purposeTemplate.id} for="purposeTemplate" state={purposeTemplate.state} />,
       ]}
     >
+      <Link
+        as="button"
+        onPointerEnter={() => {}}
+        onFocusVisible={() => {}}
+        variant="outlined"
+        size="small"
+        to="NOT_FOUND" //{purposeTemplate.state === 'DRAFT' ? 'SUBCRIBE_PURPOSE_TEMPLATE_EDIT' : 'SUBCRIBE_PURPOSE_TEMPLATE_DETAILS'}
+        // TODO: TO COMMENT OUT WHEN THE PAGE WILL BE READY
+        //params={{ delegationId: delegation.id }}
+      >
+        {tCommon(`actions.${purposeTemplate.state === 'DRAFT' ? 'manageDraft' : 'inspect'}`)}
+      </Link>
       <Box component="span" sx={{ ml: 2, display: 'inline-block' }}>
-        <ActionMenu actions={[]} />
+        <ActionMenu actions={actions} />
       </Box>
     </TableRow>
   )
