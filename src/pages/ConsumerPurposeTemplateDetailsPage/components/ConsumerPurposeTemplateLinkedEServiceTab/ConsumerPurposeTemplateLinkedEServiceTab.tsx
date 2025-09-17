eb3f1cc102b21dfecc75102288ffeb926478ca2a
martinaCampoli <martina.campoli@pagopa.it>
2025-09-17 17:31:20 +0200

    Added linked eservices tab in purpose template details page

---
+++ src/pages/ConsumerPurposeTemplateDetailsPage/components/ConsumerPurposeTemplateLinkedEServiceTab/ConsumerPurposeTemplateLinkedEServiceTab.tsx
@@ -0,0 +1,61 @@
+import { AuthHooks } from '@/api/auth'
+import { Button, Stack, Tooltip } from '@mui/material'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { PurposeTemplate } from '@/api/purposeTemplate/mockedResponses'
+import EditIcon from '@mui/icons-material/Edit'
+import { PageContainer, SectionContainer } from '@/components/layout/containers'
+import { ActionItemButton } from '@/types/common.types'
+import {
+  ConsumerPurposeTemplateLinkedEServiceTableSkeleton,
+  ConsumerPurposeTemplateLinkedEServiceTable,
+} from './ConsumerPurposeTemplateLinkedEServiceTable'
+
+type ConsumerPurposeTemplateLinkedEServiceTabProps = {
+  purposeTemplate: PurposeTemplate
+}
+
+export const ConsumerPurposeTemplateLinkedEServiceTab: React.FC<
+  ConsumerPurposeTemplateLinkedEServiceTabProps
+> = ({ purposeTemplate }) => {
+  const { t } = useTranslation('purposeTemplate', { keyPrefix: 'read.linkedEservicesTab' })
+  const { t: tCommon } = useTranslation('common')
+
+  const topSideActions: Array<ActionItemButton> = [
+    {
+      action: () => {},
+      label: tCommon('actions.edit'),
+      variant: 'contained',
+      icon: EditIcon,
+    },
+  ]
+
+  const descriptionLabel = t(
+    'description'
+  ) /** TODO: TO FIX DESCRIPTION UNDER THE BUTTON USING BOX? */
+    .split('\n')
+    .map((line, idx) => (
+      <span key={idx}>
+        {line}
+        <br />
+      </span>
+    ))
+
+  return (
+    <>
+      <SectionContainer
+        title={t('title')}
+        description={descriptionLabel}
+        topSideActions={topSideActions}
+        sx={{
+          backgroundColor: 'transparent',
+        }}
+      >
+        <React.Suspense fallback={<ConsumerPurposeTemplateLinkedEServiceTableSkeleton />}>
+          <ConsumerPurposeTemplateLinkedEServiceTable purposeTemplate={purposeTemplate} />
+        </React.Suspense>
+      </SectionContainer>
+    </>
+  )
+}
