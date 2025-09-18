b2a2f38e538669044fdb9bd5925c872b86d8b215
martinaCampoli <martina.campoli@pagopa.it>
2025-09-18 16:42:20 +0200

    Refactor tests

--- src/pages/ConsumerPurposeTemplateListPage/__test__/ConsumerPurposeTemplateListPage.test.tsx
+++ src/pages/ConsumerPurposeTemplateListPage/__test__/ConsumerPurposeTemplateListPage.test.tsx
@@ -2,26 +2,37 @@ import { mockUseJwt, renderWithApplicationContext } from '@/utils/testing.utils'
 import ConsumerPurposeTemplateListPage from '../ConsumerPurposeTemplateList.page'
 import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { ConsumerPurposeTemplateTable } from '../components/ConsumerPurposeTemplateTable'
 
 mockUseJwt()
 
-describe('Purpose Template list page', () => {
-  it('should be visibile Purpose Template list page with title, the table and create button', () => {
+// vi.mock('../ConsumerPurposeTemplateTable', () => ({
+//   ConsumerPurposeTemplateTable: () => <div>purpose-template-table</div>,
+//   ConsumerPurposeTemplateTableSkeleton: () => <div>purpose-template-table-skeleton</div>,
+// }))
+
+describe('ConsumerPurposeTemplateListPage', () => {
+  beforeEach(() => {
     renderWithApplicationContext(<ConsumerPurposeTemplateListPage />, {
-      withRouterContext: true,
       withReactQueryContext: true,
+      withRouterContext: true,
     })
-    expect(screen.getByText('title')).toBeInTheDocument()
-    expect(screen.getByText('createNewBtn')).toBeInTheDocument()
-    expect(screen.getByRole('table')).toBeInTheDocument()
+  })
+
+  // describe('ConsumerPurposeTemplateListPage is loading data', () => {
+  //   it('Should be render the skeleton at the beginning', () => {
+  //     expect(screen.getByText('purpose-template-table-skeleton')).toBeInTheDocument()
+  //   })
+  // }) TODO: IT'S NOT WORKING
+
+  describe('Purpose Template list page', () => {
+    it('should be visibile Purpose Template list page with title, the table and create button', () => {
+      expect(screen.getByText('title')).toBeInTheDocument()
+      expect(screen.getByText('createNewBtn')).toBeInTheDocument()
+      expect(screen.getByRole('table')).toBeInTheDocument()
+    })
   })
 
   it('should open the dialog when clicking the create button', async () => {
-    renderWithApplicationContext(<ConsumerPurposeTemplateListPage />, {
-      withRouterContext: true,
-      withReactQueryContext: true,
-    })
     const createButton = screen.getByText('createNewBtn')
     await userEvent.click(createButton)
 
@@ -29,10 +40,6 @@ describe('Purpose Template list page', () => {
   })
 
   it('should be have four columns (intended target, purpose template, status, actions)', () => {
-    renderWithApplicationContext(<ConsumerPurposeTemplateTable purposeTemplates={[]} />, {
-      withRouterContext: true,
-      withReactQueryContext: true,
-    })
     expect(screen.getAllByRole('columnheader')).toHaveLength(4)
   })
 })
