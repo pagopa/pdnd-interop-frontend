40ef0be0dfc335ec9546e2e3edb6a0189ef0b816
martinaCampoli <martina.campoli@pagopa.it>
2025-09-19 12:32:20 +0200

    Updated link/unlink eservices section

--- src/pages/ConsumerPurposeTemplateEditPage/components/PurposeTemplateEditStepLinkedEServices/AddEServiceToForm.tsx
+++ src/pages/ConsumerPurposeTemplateEditPage/components/PurposeTemplateEditStepLinkedEServices/AddEServiceToForm.tsx
@@ -3,23 +3,29 @@ import { useFormContext } from 'react-hook-form'
 import { Box, Stack } from '@mui/material'
 import { EServiceGroup } from './EServiceGroup'
 import type { EditStepLinkedEServicesForm } from './PurposeTemplateEditLinkedEService'
+import { PurposeTemplateMutations } from '@/api/purposeTemplate/purposeTemplate.mutations'
+import type { PurposeTemplate } from '@/api/purposeTemplate/mockedResponses'
 
 export type AddEServiceToFormProps = {
   readOnly: boolean
+  purposeTemplate: PurposeTemplate
 }
 
-export const AddEServiceToForm: React.FC<AddEServiceToFormProps> = ({ readOnly }) => {
+export const AddEServiceToForm: React.FC<AddEServiceToFormProps> = ({
+  readOnly,
+  purposeTemplate,
+}) => {
   const { watch, setValue } = useFormContext<EditStepLinkedEServicesForm>()
+  const { mutate: removeEService } = PurposeTemplateMutations.useRemoveEserviceToPurposeTemplate()
 
   const eserviceGroup = watch(`eservices`)
 
   const handleRemoveAttributeFromGroup = (eserviceId: string) => {
-    const newEServicesGroup = eserviceGroup.filter((eservice) => eservice.id !== eserviceId)
+    const newEServicesGroup = eserviceGroup.filter((eservice) => eservice.id !== eserviceId) //TODO: SHOULD IT BE REMOVED WHEN THE API IS AVAILABLE?
     setValue(`eservices`, newEServicesGroup, {
       shouldValidate: false,
     })
-
-    //TODO IS THERE A API CALL FOR DELETING AN ESERVICE FROM A PURPOSE TEMPLATE?
+    removeEService({ purposeTemplateId: purposeTemplate.id, eserviceId })
   }
 
   return (
@@ -30,6 +36,7 @@ export const AddEServiceToForm: React.FC<AddEServiceToFormProps> = ({ readOnly }
           group={eserviceGroup}
           readOnly={readOnly}
           onRemoveEServiceFromGroup={handleRemoveAttributeFromGroup}
+          purposeTemplate={purposeTemplate}
         />
       </Stack>
     </Box>
