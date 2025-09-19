45865cee2484290dad0d744575e9eca29a4746a6
borgesis95 <salvatore.borgesi@pagopa.it>
2025-09-19 12:10:14 +0200

    chore: added outcome snackbar when saving new configuration

--- src/api/notification/notification.mutations.ts
+++ src/api/notification/notification.mutations.ts
@@ -1,8 +1,16 @@
 import { useMutation } from '@tanstack/react-query'
 import { NotificationServices } from './index'
+import { useTranslation } from 'react-i18next'
 function useUpdateNotificationUserConfigs() {
+  const { t } = useTranslation('notification', {
+    keyPrefix: 'configurationPage.outcome',
+  })
   return useMutation({
     mutationFn: NotificationServices.updateUserNotificationConfigs,
+    meta: {
+      successToastLabel: t('success'),
+      errorToastLabel: t('error'),
+    },
   })
 }
 
