284ac241ec4131cab3616e7f7d02754d47175202
martinaCampoli <martina.campoli@pagopa.it>
2025-09-22 14:34:43 +0200

    Update linked eservices list

--- src/pages/ConsumerPurposeTemplateEditPage/components/PurposeTemplateEditStepLinkedEServices/AddEServiceToForm.tsx
+++ src/pages/ConsumerPurposeTemplateEditPage/components/PurposeTemplateEditStepLinkedEServices/AddEServiceToForm.tsx
@@ -5,20 +5,25 @@ import { EServiceGroup } from './EServiceGroup'
 import type { EditStepLinkedEServicesForm } from './PurposeTemplateEditLinkedEService'
 import { PurposeTemplateMutations } from '@/api/purposeTemplate/purposeTemplate.mutations'
 import type { PurposeTemplate } from '@/api/purposeTemplate/mockedResponses'
+import type { CatalogEService } from '@/api/api.generatedTypes'
 
 export type AddEServiceToFormProps = {
   readOnly: boolean
   purposeTemplate: PurposeTemplate
+  linkedEServices: CatalogEService[]
 }
 
 export const AddEServiceToForm: React.FC<AddEServiceToFormProps> = ({
   readOnly,
   purposeTemplate,
+  linkedEServices,
 }) => {
   const { watch, setValue } = useFormContext<EditStepLinkedEServicesForm>()
   const { mutate: removeEService } = PurposeTemplateMutations.useRemoveEserviceToPurposeTemplate()
 
   const eserviceGroup = watch(`eservices`)
+
+  const mergedEServices = [...eserviceGroup, ...linkedEServices]
 
   const handleRemoveAttributeFromGroup = (eserviceId: string) => {
     const newEServicesGroup = eserviceGroup.filter((eservice) => eservice.id !== eserviceId) //TODO: SHOULD IT BE REMOVED WHEN THE API IS AVAILABLE?
@@ -33,7 +38,7 @@ export const AddEServiceToForm: React.FC<AddEServiceToFormProps> = ({
       <Stack spacing={3}>
         <EServiceGroup
           key={0}
-          group={eserviceGroup}
+          group={mergedEServices}
           readOnly={readOnly}
           onRemoveEServiceFromGroup={handleRemoveAttributeFromGroup}
           purposeTemplate={purposeTemplate}
