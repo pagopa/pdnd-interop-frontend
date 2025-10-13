4ac652d7b7ca292ecdbb844ad71af55cb485ca87
martinaCampoli <martina.campoli@pagopa.it>
2025-10-13 16:56:09 +0200

    Added FF + fixed types for showing alert in risk analysis

--- src/components/shared/CreateStepPurposeRiskAnalysisForm.tsx
+++ src/components/shared/CreateStepPurposeRiskAnalysisForm.tsx
@@ -11,12 +11,13 @@ import { useTranslation } from 'react-i18next'
 import { RiskAnalysisFormComponents } from '@/components/shared/RiskAnalysisFormComponents'
 import { useRiskAnalysisForm } from '@/hooks/useRiskAnalysisForm'
 import { InformationContainer } from '@pagopa/interop-fe-commons'
+import { FEATURE_FLAG_ESERVICE_PERSONAL_DATA } from '@/config/env'
 
 type CreateStepPurposeRiskAnalysisFormProps = {
   defaultName?: string | undefined
   defaultAnswers?: Record<string, string[]>
   riskAnalysis: RiskAnalysisFormConfig
-  personalDataFlag: boolean
+  personalDataFlag?: boolean | undefined
   onSubmit: (name: string, answers: Record<string, string[]>) => void
   onCancel: VoidFunction
 }
@@ -56,10 +57,12 @@ export const CreateStepPurposeRiskAnalysisForm: React.FC<
           title={t('riskAnalysis.riskAnalysisSection.title')}
           description={t('riskAnalysis.riskAnalysisSection.description')}
         >
-          <InformationContainer
-            label={t('riskAnalysis.riskAnalysisSection.personalDataFlag.label')}
-            content={t(`riskAnalysis.riskAnalysisSection.personalDataFlag.${personalDataFlag}`)}
-          />
+          {FEATURE_FLAG_ESERVICE_PERSONAL_DATA === 'true' && (
+            <InformationContainer
+              label={t('riskAnalysis.riskAnalysisSection.personalDataFlag.label')}
+              content={t(`riskAnalysis.riskAnalysisSection.personalDataFlag.${personalDataFlag}`)}
+            />
+          )}
           <Alert sx={{ mt: 2, mb: -1 }} severity="warning">
             {t('riskAnalysis.riskAnalysisSection.personalDataAlert')}
           </Alert>
