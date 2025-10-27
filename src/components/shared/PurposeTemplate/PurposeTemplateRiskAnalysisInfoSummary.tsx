10e75af503a90778910471543ba9e6aae309d767
martinaCampoli <martina.campoli@pagopa.it>
2025-10-27 14:35:18 +0100

    PurposeTemplateRiskAnalysisInfoSummary in shared components

---
+++ src/components/shared/PurposeTemplate/PurposeTemplateRiskAnalysisInfoSummary.tsx
@@ -0,0 +1,248 @@
+import useCurrentLanguage from '@/hooks/useCurrentLanguage'
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Button,
+  Chip,
+  List,
+  ListItem,
+  ListItemText,
+  Stack,
+  Typography,
+} from '@mui/material'
+import React from 'react'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import type {
+  PurposeTemplateWithCompactCreator,
+  RiskAnalysisFormConfig,
+  RiskAnalysisFormTemplate,
+  RiskAnalysisTemplateAnswerAnnotation,
+} from '@/api/api.generatedTypes'
+import { SectionContainer } from '@/components/layout/containers'
+import { useTranslation } from 'react-i18next'
+import DownloadIcon from '@mui/icons-material/Download'
+
+type PurposeTemplateRiskAnalysisInfoSummaryProps = {
+  riskAnalysisConfig: RiskAnalysisFormConfig
+  riskAnalysisForm: RiskAnalysisFormTemplate
+  purposeTemplate: PurposeTemplateWithCompactCreator
+}
+
+const RiskAnalysisInfoSummary: React.FC<PurposeTemplateRiskAnalysisInfoSummaryProps> = ({
+  riskAnalysisConfig,
+  riskAnalysisForm,
+}) => {
+  type QuestionItem = {
+    question: string
+    answer: string
+    isEditable: boolean
+    questionInfoLabel?: string
+    annotations?: RiskAnalysisTemplateAnswerAnnotation
+  }
+
+  const { t } = useTranslation('shared-components', {
+    keyPrefix: 'purposeTemplateRiskAnalysisInfoSummary',
+  })
+
+  const panelContentId = React.useId()
+  const headerId = React.useId()
+
+  const currentLanguage = useCurrentLanguage()
+
+  const questions: Array<QuestionItem> = React.useMemo(() => {
+    // Answers in this form
+    const answerIds = Object.keys(riskAnalysisForm.answers)
+
+    // Corresponding questions
+    const questionsWithAnswer = riskAnalysisConfig.questions.filter(({ id }) =>
+      answerIds.includes(id)
+    )
+
+    const answers = questionsWithAnswer.map(({ label, options, id, visualType, infoLabel }) => {
+      const question = label[currentLanguage]
+
+      const questionInfoLabel = infoLabel?.[currentLanguage]
+
+      const annotations = riskAnalysisForm.answers[id].annotation
+
+      const isEditable = riskAnalysisForm.answers[id].editable
+
+      // Get the value of the answer
+      // The value can be of three types: plain text, multiple options, single option
+      const answerValue = riskAnalysisForm.answers[id].values
+
+      // Plain text: this value comes from a text field
+      if (visualType === 'text') {
+        return {
+          question,
+          answer: answerValue[0],
+          isEditable: riskAnalysisForm.answers[id].editable ?? false, //TODO: CHECK WHEN BE IS READY
+          questionInfoLabel,
+          annotations,
+        }
+      }
+
+      // Multiple options: this value comes from a multiple choice checkbox
+      const selectedOptions = options?.filter(({ value }) => answerValue.includes(String(value)))
+      const answer = selectedOptions?.map((o) => o.label[currentLanguage]).join(', ') ?? ''
+
+      return { question, answer, questionInfoLabel, annotations, isEditable }
+    })
+
+    return answers
+  }, [riskAnalysisForm, riskAnalysisConfig, currentLanguage])
+
+  const [hasExpandedOnce, setHasExpandedOnce] = React.useState(false)
+
+  return (
+    <SectionContainer innerSection>
+      <List>
+        {questions.map(({ question, answer, questionInfoLabel, annotations, isEditable }, i) => (
+          <SectionContainer
+            innerSection
+            key={i}
+            sx={{ p: 2, mb: 2, border: '1px solid', borderColor: 'divider', borderRadius: 2 }}
+          >
+            <ListItem key={i} sx={{ pl: 0 }}>
+              <ListItemText>
+                <Typography variant="body2" fontWeight={600} sx={{ mb: 3 }}>
+                  <Box display="flex" justifyContent="space-between" alignItems="center">
+                    <span>{question}</span>
+                    {!isEditable && (
+                      <Chip
+                        size="small"
+                        label={t('notEditableLabel')}
+                        color="default"
+                        sx={{ borderRadius: 1 }}
+                      />
+                    )}
+                  </Box>
+                </Typography>
+
+                {questionInfoLabel && (
+                  <Typography variant="caption" color="grey" component="p">
+                    {questionInfoLabel}
+                  </Typography>
+                )}
+                <Typography variant="body2" fontWeight={600} mt={2}>
+                  {t('answerLabel')}
+                  <span style={{ fontWeight: 400 }}>{answer ? answer : '-'}</span>
+                </Typography>
+                {annotations && (
+                  <>
+                    <Accordion
+                      sx={{
+                        '&:before': { display: 'none' },
+                        boxShadow: 'none',
+                        mt: 0, // no top margin before accordion
+                      }}
+                    >
+                      <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        onClick={() => setHasExpandedOnce(true)}
+                        aria-controls={panelContentId}
+                        id={headerId}
+                        sx={{
+                          px: 0,
+                          py: 0,
+                          '& .MuiAccordionSummary-expandIconWrapper': {
+                            color: 'primary.main',
+                            mr: '84%',
+                          },
+                        }}
+                      >
+                        <Typography fontWeight={700} sx={{ color: 'primary.main' }}>
+                          {t('annotationSection.readAnnotationBtnLabel')}
+                        </Typography>
+                      </AccordionSummary>
+
+                      <AccordionDetails
+                        sx={{
+                          px: 0,
+                          py: 0,
+                        }}
+                      >
+                        {hasExpandedOnce && <AnnotationDetails annotation={annotations} />}
+                      </AccordionDetails>
+                    </Accordion>
+                  </>
+                )}
+              </ListItemText>
+            </ListItem>
+          </SectionContainer>
+        ))}
+      </List>
+    </SectionContainer>
+  )
+}
+
+export const PurposeTemplateRiskAnalysisInfoSummary: React.FC<
+  PurposeTemplateRiskAnalysisInfoSummaryProps
+> = ({ purposeTemplate, riskAnalysisConfig, riskAnalysisForm }) => {
+  const { t: tPurposeTemplate } = useTranslation('purposeTemplate', {
+    keyPrefix: 'read.riskAnalysisTab',
+  })
+
+  const title =
+    purposeTemplate.targetTenantKind === 'PA'
+      ? tPurposeTemplate('titlePA')
+      : tPurposeTemplate('titleNotPA')
+
+  if (!riskAnalysisConfig || !riskAnalysisForm) return null
+  return (
+    <SectionContainer
+      title={title}
+      sx={{
+        backgroundColor: 'paper.main',
+      }}
+    >
+      <RiskAnalysisInfoSummary
+        riskAnalysisConfig={riskAnalysisConfig}
+        riskAnalysisForm={riskAnalysisForm}
+        purposeTemplate={purposeTemplate}
+      />
+    </SectionContainer>
+  )
+}
+
+const AnnotationDetails: React.FC<{ annotation: RiskAnalysisTemplateAnswerAnnotation }> = ({
+  annotation,
+}) => {
+  const { t } = useTranslation('shared-components', {
+    keyPrefix: 'purposeTemplateRiskAnalysisInfoSummary.annotationSection',
+  })
+
+  return (
+    <SectionContainer
+      innerSection
+      title={t('title')}
+      sx={{ backgroundColor: 'grey.50', p: 3, fontWeight: 700 }}
+    >
+      <Stack spacing={2}>
+        <Typography variant="body2">{annotation.text}</Typography>
+        {annotation.docs && annotation.docs.length > 0 && (
+          <Stack spacing={1}>
+            {annotation.docs.map((doc) => (
+              <Button
+                key={doc.id}
+                endIcon={<DownloadIcon fontSize="small" />}
+                component="button"
+                onClick={() => {}} // TODO: handle download
+                sx={{
+                  fontWeight: 700,
+                  alignSelf: 'flex-start',
+                  p: 0,
+                }}
+                disableRipple
+              >
+                {doc.prettyName}
+              </Button>
+            ))}
+          </Stack>
+        )}
+      </Stack>
+    </SectionContainer>
+  )
+}
