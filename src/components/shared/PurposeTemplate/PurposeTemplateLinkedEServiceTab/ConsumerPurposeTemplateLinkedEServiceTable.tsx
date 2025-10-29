74d13e6b56e6d106589d7cebc3676d5f4f798c85
martinaCampoli <martina.campoli@pagopa.it>
2025-10-29 11:05:33 +0100

    Added todo comment for filter

--- src/components/shared/PurposeTemplate/PurposeTemplateLinkedEServiceTab/ConsumerPurposeTemplateLinkedEServiceTable.tsx
+++ src/components/shared/PurposeTemplate/PurposeTemplateLinkedEServiceTab/ConsumerPurposeTemplateLinkedEServiceTable.tsx
@@ -47,7 +47,7 @@ export const ConsumerPurposeTemplateLinkedEServiceTable: React.FC<
   const { filtersParams, ...filtersHandlers } = useFilters<
     Omit<GetPurposeTemplateEServicesParams, 'limit' | 'offset' | 'purposeTemplateId'>
   >([
-    // { name: 'q', label: t('filters.eserviceField.label'), type: 'freetext' },
+    // { name: 'q', label: t('filters.eserviceField.label'), type: 'freetext' }, todo: add filter when BE is ready
     {
       name: 'producerIds',
       label: t('filters.providerField.label'),
