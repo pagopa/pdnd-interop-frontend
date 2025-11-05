50a780d308ae2d76cd8fc2c6eec9eabf01431129
Alepazz <alessio.pazzani@gmail.com>
2025-11-05 16:51:08 +0100

    Refactor: Remove empty docs array from annotation in RiskAnalysis components (PIN-7784)

--- src/pages/ConsumerPurposeTemplateEditPage/components/PurposeTemplateEditStepRiskAnalysis/RiskAnalysisForm/__tests__/RiskAnalysisFormTemplate.test.tsx
+++ src/pages/ConsumerPurposeTemplateEditPage/components/PurposeTemplateEditStepRiskAnalysis/RiskAnalysisForm/__tests__/RiskAnalysisFormTemplate.test.tsx
@@ -203,7 +203,7 @@ describe('RiskAnalysisFormTemplate', () => {
       callback({
         validAnswers: { q1: ['answer1'] },
         assignToTemplateUsers: { q1: false },
-        annotations: { q1: { text: 'annotation text', docs: [] } },
+        annotations: { q1: { text: 'annotation text' } },
         suggestedValues: {},
       })
     })
@@ -232,7 +232,7 @@ describe('RiskAnalysisFormTemplate', () => {
           q1: {
             values: ['answer1'],
             editable: false,
-            annotation: { text: 'annotation text', docs: [] },
+            annotation: { text: 'annotation text' },
             suggestedValues: [],
           },
         },
