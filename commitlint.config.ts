97f8b9fc450abc880b151baefd695980c720edb2
borgesis95 <salvatore.borgesi@pagopa.it>
2025-11-11 16:55:46 +0100

    refactor: added commitlint configuration (PIN-8192)

---
+++ commitlint.config.ts
@@ -0,0 +1,28 @@
+import type { UserConfig } from '@commitlint/types'
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 200],
+    'header-match-pattern': [2, 'always'],
+  },
+  plugins: [
+    {
+      rules: {
+        'header-match-pattern': (parsed) => {
+          const { header } = parsed
+          const pattern =
+            /^(feat|fix|docs|chore|refactor|test|style|perf|ci|build|revert): .+ \([A-Z]+-\d+(, [A-Z]+-\d+)*\)$/
+
+          if (!header || !pattern.test(header)) {
+            return [false, 'Message should follow the format: <type>: <description> (<JIRA-KEY>)']
+          }
+
+          return [true]
+        },
+      },
+    },
+  ],
+}
+
+export default Configuration
