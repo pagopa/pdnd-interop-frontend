5ef810abe160cc2e72eb68cb704b189ed8549007
Alepazz <alessio.pazzani@gmail.com>
2025-11-19 10:48:38 +0100

    refactor: Optimize document download handling in PurposeTemplateDocumentationSection by using memoization for answer ID mapping (PIN-8278)

--- src/components/shared/PurposeTemplate/PurposeTemplateDetailsTab/PurposeTemplateDocumentationSection.tsx
+++ src/components/shared/PurposeTemplate/PurposeTemplateDetailsTab/PurposeTemplateDocumentationSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { SectionContainer, SectionContainerSkeleton } from '@/components/layout/containers'
 import { useTranslation } from 'react-i18next'
 import { InformationContainer } from '@pagopa/interop-fe-commons'
@@ -25,7 +25,36 @@ export const PurposeTemplateDocumentationSection: React.FC<
 
   const downloadDocument = PurposeTemplateDownloads.useDownloadDocument()
 
-  const handleDownload = (doc: RiskAnalysisTemplateAnswerAnnotationDocument, answerId: string) => {
+  const documentToAnswerIdMap = useMemo(() => {
+    const map = new Map<string, string>()
+    const answers = purposeTemplate.purposeRiskAnalysisForm?.answers as
+      | Record<
+          string,
+          {
+            id: string
+            annotation?: { docs?: RiskAnalysisTemplateAnswerAnnotationDocument[] }
+          }
+        >
+      | undefined
+
+    if (answers) {
+      Object.values(answers).forEach((answer) => {
+        answer.annotation?.docs?.forEach((doc) => {
+          map.set(doc.id, answer.id)
+        })
+      })
+    }
+
+    return map
+  }, [purposeTemplate.purposeRiskAnalysisForm?.answers])
+
+  const handleDownload = (doc: RiskAnalysisTemplateAnswerAnnotationDocument) => {
+    const answerId = documentToAnswerIdMap.get(doc.id)
+    if (!answerId) {
+      console.error('Could not find answerId for document:', doc.id)
+      return
+    }
+
     downloadDocument(
       {
         purposeTemplateId: purposeTemplate.id,
@@ -36,21 +65,7 @@ export const PurposeTemplateDocumentationSection: React.FC<
     )
   }
 
-  const answers = purposeTemplate.purposeRiskAnalysisForm?.answers as
-    | Record<
-        string,
-        {
-          id: string
-          annotation?: { docs?: RiskAnalysisTemplateAnswerAnnotationDocument[] }
-        }
-      >
-    | undefined
-
-  const documents = answers
-    ? Object.values(answers).flatMap((answer) =>
-        (answer.annotation?.docs || []).map((doc) => ({ doc, answerId: answer.id }))
-      )
-    : []
+  const documentation = purposeTemplate.annotationDocuments
 
   return (
     <SectionContainer title={t('title')} description={t('description')}>
@@ -58,13 +73,13 @@ export const PurposeTemplateDocumentationSection: React.FC<
         <InformationContainer
           label={t('documentsLabel')}
           content={
-            documents.length > 0 ? (
+            documentation && documentation.length > 0 ? (
               <Stack spacing={2} direction="column">
-                {documents.map(({ doc, answerId }) => (
+                {documentation.map((doc) => (
                   <IconLink
                     key={doc.id}
                     component="button"
-                    onClick={handleDownload.bind(null, doc, answerId)}
+                    onClick={handleDownload.bind(null, doc)}
                     startIcon={<AttachFileIcon fontSize="small" />}
                   >
                     {doc.prettyName}
