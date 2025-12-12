c3f6859bb0a031f23b21dd9507e3f1af14ce8d86
Antonio Forte <antonio.forte@DTFORTEA01.fritz.box>
2025-12-12 09:52:16 +0100

    feat(pin-8688): adding more tests

--- src/pages/ConsumerSimulateGetVoucherPage/components/VoucherInstructions/__test__/VoucherInstructions.test.tsx
+++ src/pages/ConsumerSimulateGetVoucherPage/components/VoucherInstructions/__test__/VoucherInstructions.test.tsx
@@ -4,9 +4,24 @@ import { screen } from '@testing-library/react'
 import { renderWithApplicationContext } from '@/utils/testing.utils'
 
 describe('VoucherInstructions testing', () => {
-  it('should render instruction for get voucher simulation', () => {
+  it('should render instruction for get consumer voucher simulation', () => {
     renderWithApplicationContext(
       <MemoryRouter initialEntries={['/tool-sviluppo/api-e-service/simulate-get-voucher']}>
+        <VoucherInstructions />
+      </MemoryRouter>,
+      {
+        withReactQueryContext: true,
+      }
+    )
+
+    expect(screen.getByText('step1.description')).toBeInTheDocument()
+    expect(screen.getByLabelText('step1.clientSelectInput.label')).toBeInTheDocument()
+    expect(screen.getByLabelText('step1.purposeSelectInput.label')).toBeInTheDocument()
+  })
+
+  it('should render instruction for get api voucher simulation', () => {
+    renderWithApplicationContext(
+      <MemoryRouter initialEntries={['/tool-sviluppo/api-interop/simulate-get-voucher']}>
         <VoucherInstructions />
       </MemoryRouter>,
       {
