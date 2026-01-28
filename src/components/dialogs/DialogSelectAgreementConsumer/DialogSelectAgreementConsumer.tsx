1c162ebf27574b8156f45d4519a1c7d4bc993f57
alten-dturus <davide.turus@alten.it>
2026-01-28 18:10:48 +0100

    Implement new dialog to select the agreement consumer

---
+++ src/components/dialogs/DialogSelectAgreementConsumer/DialogSelectAgreementConsumer.tsx
@@ -0,0 +1,97 @@
+import type { DelegationTenant } from '@/api/api.generatedTypes'
+import { AuthHooks } from '@/api/auth'
+import { useNavigate } from '@/router'
+import { useDialog } from '@/stores'
+import type { DialogSelectAgreementConsumerProps } from '@/types/dialog.types'
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Stack,
+  Typography,
+} from '@mui/material'
+import React from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+import { DialogSelectAgreementConsumerAutocomplete } from './DialogSelectAgreementConsumerAutocomplete'
+
+type SelectAgreementConsumerFormValues = {
+  consumerId: string
+}
+
+export const DialogSelectAgreementConsumer: React.FC<DialogSelectAgreementConsumerProps> = ({
+  action,
+  eserviceId,
+  agreements,
+}) => {
+  const ariaLabelId = React.useId()
+  const { t: tCommon } = useTranslation('common', { keyPrefix: 'actions' })
+  const { t } = useTranslation('shared-components', {
+    keyPrefix: action === 'inspect' ? 'dialogCreateAgreementDraft' : 'dialogCreateAgreementDraft',
+  }) // TODO strings
+
+  const navigate = useNavigate()
+  const { closeDialog } = useDialog()
+  const { jwt } = AuthHooks.useJwt()
+
+  const formMethods = useForm<SelectAgreementConsumerFormValues>({
+    defaultValues: {
+      consumerId: jwt?.organizationId ?? undefined,
+    },
+  })
+
+  const selectedConsumerId = formMethods.watch('consumerId')
+
+  const handleSubmit = ({ consumerId }: SelectAgreementConsumerFormValues) => {
+    const agreementId = agreements.find((agreement) => agreement.consumerId === consumerId)?.id
+
+    if (!agreementId) return
+
+    navigate('SUBSCRIBE_AGREEMENT_READ', {
+      params: {
+        agreementId: agreementId,
+      },
+    })
+  }
+
+  return (
+    <Dialog aria-labelledby={ariaLabelId} open onClose={closeDialog} maxWidth="md" fullWidth>
+      <FormProvider {...formMethods}>
+        <Box component="form" noValidate onSubmit={formMethods.handleSubmit(handleSubmit)}>
+          <DialogTitle id={ariaLabelId}>{t('title')}</DialogTitle>
+
+          <DialogContent>
+            <Stack spacing={2}>
+              <Typography>{t('description')}</Typography>
+              <DialogSelectAgreementConsumerAutocomplete
+                eserviceId={eserviceId}
+                preselectedConsumer={
+                  jwt
+                    ? ({
+                        id: jwt?.organizationId,
+                        name: jwt?.organization.name,
+                      } as DelegationTenant)
+                    : undefined
+                }
+                agreements={agreements}
+                action={action}
+              />
+            </Stack>
+          </DialogContent>
+
+          <DialogActions>
+            <Button type="button" variant="outlined" onClick={closeDialog}>
+              {tCommon('cancel')}
+            </Button>
+            <Button variant="contained" type="submit" disabled={!selectedConsumerId}>
+              {tCommon('createNewDraft')}
+            </Button>
+          </DialogActions>
+        </Box>
+      </FormProvider>
+    </Dialog>
+  )
+}
---
+++ src/components/dialogs/DialogSelectAgreementConsumer/DialogSelectAgreementConsumerAutocomplete.tsx
@@ -0,0 +1,140 @@
+import type { CompactAgreement, DelegationTenant } from '@/api/api.generatedTypes'
+import { AuthHooks } from '@/api/auth'
+import { DelegationQueries } from '@/api/delegation'
+import { RHFAutocompleteSingle } from '@/components/shared/react-hook-form-inputs'
+import { useAutocompleteTextInput } from '@pagopa/interop-fe-commons'
+import { useQuery } from '@tanstack/react-query'
+import React from 'react'
+import { useFormContext } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+import { match } from 'ts-pattern'
+
+type DialogSelectAgreementConsumerAutocompleteProps = {
+  eserviceId: string
+  preselectedConsumer: DelegationTenant | undefined
+  agreements: CompactAgreement[]
+  action: 'inspect' | 'edit'
+}
+
+export const DialogSelectAgreementConsumerAutocomplete: React.FC<
+  DialogSelectAgreementConsumerAutocompleteProps
+> = ({ eserviceId, preselectedConsumer, agreements, action }) => {
+  const { t } = useTranslation('shared-components', {
+    keyPrefix: 'dialogCreateAgreementDraft',
+  })
+  const { jwt } = AuthHooks.useJwt()
+
+  const agreementOptions = match(action)
+    .with('inspect', () =>
+      agreements.filter(
+        (agreement) =>
+          agreement.state === 'ACTIVE' ||
+          agreement.state === 'SUSPENDED' ||
+          agreement.state === 'PENDING'
+      )
+    )
+    .with('edit', () => agreements.filter((agreement) => agreement.state === 'DRAFT'))
+    .exhaustive()
+
+  const { data: delegations = [] } = useQuery({
+    ...DelegationQueries.getList({
+      limit: 50,
+      offset: 0,
+      eserviceIds: [eserviceId],
+      kind: 'DELEGATED_CONSUMER',
+      states: ['ACTIVE'],
+      delegatorIds: [jwt?.organizationId as string],
+    }),
+    enabled: Boolean(jwt?.organizationId),
+    select: ({ results }) => results ?? [],
+  })
+  const isDelegator = delegations.length > 0
+
+  const selectedConsumerRef = React.useRef<DelegationTenant | undefined>(preselectedConsumer)
+  const hasSetFirstConsumer = React.useRef(Boolean(preselectedConsumer))
+
+  const { setValue, watch } = useFormContext()
+  const [consumerAutocompleteTextInput, setConsumerAutocompleteTextInput] =
+    useAutocompleteTextInput(preselectedConsumer?.name ?? '')
+
+  const selectedConsumerId = watch('consumerId')
+
+  /**
+   * TEMP: This is a workaround to avoid the "q" param in the query to be equal to the selected attribute name.
+   */
+  function getQ() {
+    let result = consumerAutocompleteTextInput
+
+    if (
+      selectedConsumerRef.current &&
+      consumerAutocompleteTextInput === selectedConsumerRef.current.name
+    ) {
+      result = ''
+    }
+
+    return result
+  }
+
+  const { data: delegators = [], isLoading } = useQuery({
+    ...DelegationQueries.getConsumerDelegators({
+      q: getQ(),
+      limit: 50,
+      offset: 0,
+      eserviceIds: [eserviceId],
+    }),
+    select: ({ results }) => results ?? [],
+  })
+
+  React.useEffect(() => {
+    if (
+      !selectedConsumerId &&
+      !hasSetFirstConsumer.current &&
+      delegators &&
+      delegators?.length > 0
+    ) {
+      setValue('consumerId', delegators[0])
+      setConsumerAutocompleteTextInput(delegators[0].name)
+      selectedConsumerRef.current = delegators[0]
+      hasSetFirstConsumer.current = true
+    }
+  }, [setValue, selectedConsumerId, delegators, setConsumerAutocompleteTextInput])
+
+  const delegatorOptions = delegators.filter((delegator) =>
+    agreementOptions.some((agreement) => agreement.consumerId === delegator.id)
+  )
+
+  const tenantOptions = match(action)
+    .with('inspect', () =>
+      jwt && agreementOptions.some((agreement) => agreement.consumerId === jwt.organizationId)
+        ? [{ id: jwt.organizationId, name: jwt.organization.name }, ...delegatorOptions]
+        : delegatorOptions
+    )
+    .with('edit', () =>
+      jwt &&
+      !isDelegator &&
+      agreementOptions.some((agreement) => agreement.consumerId === jwt.organizationId)
+        ? [{ id: jwt.organizationId, name: jwt.organization.name }, ...delegatorOptions]
+        : delegatorOptions
+    )
+    .exhaustive()
+
+  const autocompleteOptions = tenantOptions.map((tenant) => ({
+    label: tenant.name,
+    value: tenant.id,
+  }))
+
+  return (
+    <RHFAutocompleteSingle
+      sx={{ my: 0 }}
+      loading={isLoading}
+      name="consumerId"
+      options={autocompleteOptions}
+      label={t('consumerField.label')}
+      onValueChange={(value) => {
+        selectedConsumerRef.current = tenantOptions?.find((tenant) => tenant.id === value?.value)
+      }}
+      onInputChange={(_, value) => setConsumerAutocompleteTextInput(value)}
+      rules={{ required: true }}
+    />
+  )
+}
