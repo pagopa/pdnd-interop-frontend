64dcc0b295a14429e1186ce88e060c824add70ed
borgesis95 <salvatore.borgesi@pagopa.it>
2026-01-30 12:47:47 +0100

    fix: AVATAR_BASEPATH with https instead http

--- public/env.js
+++ public/env.js
@@ -22,5 +22,5 @@ window.pagopa_env = {
   FEATURE_FLAG_NOTIFICATION_CONFIG: 'true',
   FEATURE_FLAG_ESERVICE_PERSONAL_DATA: 'true',
   FEATURE_FLAG_USE_SIGNED_DOCUMENT: 'true',
-  AVATAR_BASEPATH: 'http://selc-u-checkout-cdn-endpoint.azureedge.net',
+  AVATAR_BASEPATH: 'https://selc-u-checkout-cdn-endpoint.azureedge.net',
 }
