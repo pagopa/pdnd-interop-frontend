335ab3c5f5bddd8ca7cdd60f79458537b3ef15ad
borgesis95 <salvatore.borgesi@pagopa.it>
2026-02-26 15:35:08 +0100

    fix: added MutationMeta type

--- src/types/react-query.d.ts
+++ src/types/react-query.d.ts
@@ -1,20 +1,19 @@
 import '@tanstack/react-query'
 
-interface MutationMeta {
-  loadingLabel?: string | ((variables: unknown) => string)
-  successToastLabel?: string | ((data: unknown, variables: unknown, context: unknown) => string)
-  errorToastLabel?: string | ((error: unknown, variables: unknown, context: unknown) => string)
-  confirmationDialog?: {
-    title: string | ((variables: unknown) => string)
-    description?: string | ((variables: unknown) => string)
-    proceedLabel?: string
-    checkbox?: string
-  }
-}
-
 declare module '@tanstack/react-query' {
+  interface MutationMeta {
+    loadingLabel?: string | ((variables: unknown) => string)
+    successToastLabel?: string | ((data: unknown, variables: unknown, context: unknown) => string)
+    errorToastLabel?: string | ((error: unknown, variables: unknown, context: unknown) => string)
+    confirmationDialog?: {
+      title: string | ((variables: unknown) => string)
+      description?: string | ((variables: unknown) => string)
+      proceedLabel?: string
+      checkbox?: string
+    }
+  }
+
   interface Register {
-    queryMeta: Meta
     mutationMeta: MutationMeta
   }
 }
