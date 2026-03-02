54c1613e297c237d1a7ce82f537be1ce2d323a2b
Andrea Gorletta <andrea.alice.gorletta@gmail.com>
2026-03-02 14:05:16 +0100

    fix: update duplicate instanceLabel error code to match BE format (PIN-9271)

--- src/api/eserviceTemplate/eserviceTemplate.mutations.ts
+++ src/api/eserviceTemplate/eserviceTemplate.mutations.ts
@@ -8,7 +8,7 @@ import type {
 } from '../api.generatedTypes'
 import type { AttributeKey } from '@/types/attribute.types'
 
-export const DUPLICATE_INSTANCE_LABEL_ERROR_CODE = '007'
+export const DUPLICATE_INSTANCE_LABEL_ERROR_CODE = '001-007'
 
 function useUpdateEServiceTemplateName() {
   const { t } = useTranslation('mutations-feedback', {
