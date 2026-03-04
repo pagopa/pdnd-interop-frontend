eba79da9e2f81a576d384167f2c782abee99d4ac
borgesis95 <salvatore.borgesi@pagopa.it>
2026-03-04 12:02:13 +0100

    chore: removed tab when generated are equal to 0

--- src/pages/ConsumerEServiceTemplateDetailsPage/ConsumerEServiceTemplateDetails.page.tsx
+++ src/pages/ConsumerEServiceTemplateDetailsPage/ConsumerEServiceTemplateDetails.page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { PageContainer } from '@/components/layout/containers'
+import { PageContainer, SectionContainerSkeleton } from '@/components/layout/containers'
 import { useParams } from '@/router'
-import { Tab } from '@mui/material'
+import { Grid, Tab } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 import { useQuery } from '@tanstack/react-query'
 import { TabContext, TabList, TabPanel } from '@mui/lab'
@@ -9,6 +9,7 @@ import { useActiveTab } from '@/hooks/useActiveTab'
 import { EServiceTemplateQueries } from '@/api/eserviceTemplate'
 import { ConsumerEServiceTemplateDetails, ConsumerEServiceTemplateInstancesTab } from './components'
 import { useGetConsumerEServiceTemplateActions } from './hooks/useGetConsumerEServiceTemplateActions'
+import { ConsumerEServiceGeneralInfoSectionSkeleton } from '../ConsumerEServiceDetailsPage/components/ConsumerEServiceDetailsTab/ConsumerEServiceGeneralInfoSection'
 
 const ConsumerEServiceTemplateDetailsPage: React.FC = () => {
   const { t } = useTranslation('eserviceTemplate', { keyPrefix: 'read' })
@@ -21,8 +22,18 @@ const ConsumerEServiceTemplateDetailsPage: React.FC = () => {
     EServiceTemplateQueries.getSingle(eServiceTemplateId, eServiceTemplateVersionId)
   )
 
+  const { data: templateInstancesCount, isLoading: isLoadingTemplateInstancesCount } = useQuery({
+    ...EServiceTemplateQueries.getMyEServiceTemplateInstancesList({
+      limit: 1,
+      offset: 0,
+      eserviceTemplateId: eServiceTemplateId,
+    }),
+  })
+
   const hasRequesterRiskAnalysis = eserviceTemplate?.hasRequesterRiskAnalysis ?? true
   const hasPersonalDataValue = eserviceTemplate?.eserviceTemplate.personalData !== undefined
+  const isAvailableAtLeastOneInstance =
+    templateInstancesCount && templateInstancesCount?.results?.length > 0
 
   const { actions } = useGetConsumerEServiceTemplateActions(
     eServiceTemplateId,
@@ -48,21 +59,49 @@ const ConsumerEServiceTemplateDetailsPage: React.FC = () => {
         to: 'PROVIDE_ESERVICE_TEMPLATE_CATALOG',
       }}
     >
-      <TabContext value={activeTab}>
-        <TabList onChange={updateActiveTab} aria-label={t('tabs.ariaLabel')} variant="fullWidth">
-          <Tab label={t('tabs.eserviceTemplateDetails')} value="eserviceTemplateDetails" />
-          <Tab label={t('tabs.eserviceTemplateInstances')} value="eserviceTemplateInstances" />
-        </TabList>
-        <TabPanel value="eserviceTemplateDetails">
-          <ConsumerEServiceTemplateDetails />
-        </TabPanel>
-        <TabPanel value="eserviceTemplateInstances">
-          <ConsumerEServiceTemplateInstancesTab
-            eserviceTemplateVersions={eserviceTemplate?.eserviceTemplate.versions ?? []}
-          />
-        </TabPanel>
-      </TabContext>
+      <React.Suspense fallback={<ConsumerEServiceTemplateDetailsSkeleton />}>
+        <TabContext value={activeTab}>
+          {isAvailableAtLeastOneInstance && (
+            <TabList
+              onChange={updateActiveTab}
+              aria-label={t('tabs.ariaLabel')}
+              variant="fullWidth"
+            >
+              <Tab label={t('tabs.eserviceTemplateDetails')} value="eserviceTemplateDetails" />
+
+              <Tab label={t('tabs.eserviceTemplateInstances')} value="eserviceTemplateInstances" />
+            </TabList>
+          )}
+          <TabPanel value="eserviceTemplateDetails">
+            <ConsumerEServiceTemplateDetails />
+          </TabPanel>
+
+          {isAvailableAtLeastOneInstance && (
+            <TabPanel value="eserviceTemplateInstances">
+              <ConsumerEServiceTemplateInstancesTab
+                eserviceTemplateVersions={eserviceTemplate?.eserviceTemplate.versions ?? []}
+              />
+            </TabPanel>
+          )}
+        </TabContext>
+      </React.Suspense>
     </PageContainer>
+  )
+}
+
+export const ConsumerEServiceTemplateDetailsSkeleton = () => {
+  return (
+    <>
+      <Grid container sx={{ mt: 10 }}>
+        <Grid xs={12} sm={6}>
+          <SectionContainerSkeleton sx={{ mt: 4, mr: 2 }} height={25} />
+        </Grid>
+        <Grid xs={12} sm={6}>
+          <SectionContainerSkeleton sx={{ mt: 4 }} height={25} />
+        </Grid>
+      </Grid>
+      <SectionContainerSkeleton sx={{ mt: 4 }} height={500} />
+    </>
   )
 }
 
