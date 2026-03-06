49433316dcc1e8e4c063f2d535bf8f4efe2a17f8
antofo <antonio.forte@digitouch.it>
2026-03-06 09:16:36 +0100

    feat: adjust test for e-service create new version step I (PIN-9389)

--- src/pages/ProviderEServiceCreatePage/components/sections/__tests__/EServiceInfoSection.test.tsx
+++ src/pages/ProviderEServiceCreatePage/components/sections/__tests__/EServiceInfoSection.test.tsx
@@ -1,11 +1,18 @@
 import { ReactHookFormWrapper, renderWithApplicationContext } from '@/utils/testing.utils'
 import { EServiceInfoSection } from '../EServiceInfoSection'
 import { screen } from '@testing-library/react'
+import type { ProducerEServiceDescriptor } from '@/api/api.generatedTypes'
+import { createMockEServiceDescriptorProvider } from '@/../__mocks__/data/eservice.mocks'
 
-const renderComponent = () => {
+const descriptor: ProducerEServiceDescriptor = createMockEServiceDescriptorProvider()
+
+const renderComponent = (areEServiceGeneralInfoEditable: boolean = true) => {
   return renderWithApplicationContext(
     <ReactHookFormWrapper>
-      <EServiceInfoSection />
+      <EServiceInfoSection
+        areEServiceGeneralInfoEditable={areEServiceGeneralInfoEditable}
+        descriptor={descriptor}
+      />
     </ReactHookFormWrapper>,
     {
       withReactQueryContext: false,
@@ -15,13 +22,24 @@ const renderComponent = () => {
 }
 
 describe('EServiceInfoSection', () => {
-  it('should render the title', () => {
+  it('should render the title and description when info are editable', () => {
     renderComponent()
     expect(screen.getByText('title')).toBeInTheDocument()
   })
 
-  it('should render 2 inputs (name and description)', () => {
+  it('should render 2 inputs (name and description) when info are editable', () => {
     renderComponent()
+    expect(screen.getByText('nameField.label')).toBeInTheDocument()
+    expect(screen.getByText('descriptionField.label')).toBeInTheDocument()
+  })
+
+  it('should render readOnlyDescription  when info are not editable', () => {
+    renderComponent(false)
+    expect(screen.getByText('readOnlyDescription')).toBeInTheDocument()
+  })
+
+  it('should render two information container (name, description) when info are not editable', () => {
+    renderComponent(false)
     expect(screen.getByText('nameField.label')).toBeInTheDocument()
     expect(screen.getByText('descriptionField.label')).toBeInTheDocument()
   })
