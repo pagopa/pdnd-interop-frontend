d616251826cb39374aee9ee2b020adcc7dc0d8d6
Alepazz <alessio.pazzani@gmail.com>
2026-03-09 11:05:54 +0100

    fix: restore interface as required in e-service creation and summary (PIN-9410)

--- src/pages/ProviderEServiceSummaryPage/ProviderEServiceSummary.page.tsx
+++ src/pages/ProviderEServiceSummaryPage/ProviderEServiceSummary.page.tsx
@@ -200,6 +200,7 @@ const ProviderEServiceSummaryPage: React.FC = () => {
     return (
       !!(
         descriptor &&
+        descriptor.interface &&
         descriptor.description &&
         descriptor.audience[0] &&
         descriptor.voucherLifespan &&
@@ -272,6 +273,8 @@ const ProviderEServiceSummaryPage: React.FC = () => {
     Boolean(descriptor?.audience?.length) &&
     Boolean(descriptor?.voucherLifespan)
 
+  const isDocumentationSectionValid = Boolean(descriptor?.interface)
+
   return (
     <>
       <PageContainer
@@ -338,6 +341,8 @@ const ProviderEServiceSummaryPage: React.FC = () => {
             <SummaryAccordion
               headline={isReceiveMode ? '4' : '3'}
               title={t('summary.documentationSummary.title')}
+              showWarning={!isDocumentationSectionValid}
+              warningLabel={t('summary.missingInformationsLabel')}
             >
               <ProviderEServiceDocumentationSummarySection />
             </SummaryAccordion>
