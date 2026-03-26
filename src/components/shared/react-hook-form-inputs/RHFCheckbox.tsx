ceed5ddb01ce57bf9d69598ee3ce4c9d855d9057
antofo <antonio.forte@digitouch.it>
2026-03-26 13:01:33 +0100

    feat: fix disabled delegate client association (PIN-9559)

--- src/components/shared/react-hook-form-inputs/RHFCheckbox.tsx
+++ src/components/shared/react-hook-form-inputs/RHFCheckbox.tsx
@@ -43,6 +43,7 @@ export const RHFCheckbox: React.FC<RHFCheckboxProps> = ({
         render={({ field: { value, ref, ...fieldProps } }) => (
           <FormControlLabel
             label={label}
+            disabled={disabled}
             control={
               <MUICheckbox
                 {...fieldProps}
