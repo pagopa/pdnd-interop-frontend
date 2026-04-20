693a0407f41520cdf37129255e8c213e1a81d07e
lorenzo.giacomini <lorenzo.giacomini@digitouch.it>
2026-04-20 10:07:45 +0200

    feat: added dummy first DPoP step and second DPoP step files (PIN-9718)

---
+++ src/pages/ConsumerSimulateGetVoucherPage/components/VoucherInstructions/steps/VoucherInstructionsFirstDPoPProofStep.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const VoucherInstructionsFirstDPoPProofStep: React.FC = () => {
+  return <></>
+}
---
+++ src/pages/ConsumerSimulateGetVoucherPage/components/VoucherInstructions/steps/VoucherInstructionsSecondDPoPProofStep.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const VoucherInstructionsSecondDPoPProofStep: React.FC = () => {
+  return <></>
+}
