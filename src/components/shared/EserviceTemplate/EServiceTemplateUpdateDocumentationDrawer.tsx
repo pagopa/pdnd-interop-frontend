8fa7a3d3748f4d57ce5802acef0d95dfe4553200
martinaCampoli <martina.campoli@pagopa.it>
2026-04-21 15:13:54 +0200

    feat: added role and aria attribute to info icon to accessibility reasons (PIN-A2-2666 - A11Y)

--- src/components/shared/EserviceTemplate/EServiceTemplateUpdateDocumentationDrawer.tsx
+++ src/components/shared/EserviceTemplate/EServiceTemplateUpdateDocumentationDrawer.tsx
@@ -171,7 +171,7 @@ export const EServiceTemplateUpdateDocumentationDrawer: React.FC<
           if (!doc) return null
           if (index === 0 && paginationParams.offset === 0)
             return (
-              <Stack spacing={1}>
+              <Stack spacing={1} key={doc.id}>
                 <InterfaceDocumentContainer doc={doc} onDownload={handleDownloadDocument} />
                 <Divider />
               </Stack>
@@ -260,7 +260,14 @@ const InterfaceDocumentContainer: React.FC<InterfaceDocumentContainerProps> = ({
       <Stack direction="row" alignItems="center" spacing={1}>
         <Typography variant="body2">{doc.prettyName}</Typography>
         <Tooltip title={t('interfaceInfoTooltip')}>
-          <InfoIcon fontSize="small" sx={{ color: 'text.secondary' }} />
+          <InfoIcon
+            fontSize="small"
+            sx={{ color: 'text.secondary' }}
+            tabIndex={0}
+            role="button"
+            aria-disabled={true} //we just want to make it focusable for accessibility reasons, so we set aria-disabled to true to communicate that it's not interactive
+            aria-label={t('interfaceInfoTooltip')}
+          />
         </Tooltip>
       </Stack>
       {onDownload && (
