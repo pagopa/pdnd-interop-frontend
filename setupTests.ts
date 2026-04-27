aa9a05067a67d7e8e0b42e3f82b70c1bbdcac750
borgesis95 <salvatore.borgesi@pagopa.it>
2026-04-27 09:24:49 +0200

    Merge branch 'dev' into align_main_dev_2704_2

--- setupTests.ts
+++ setupTests.ts
@@ -23,6 +23,7 @@ export const testConfigs = {
   FEATURE_FLAG_ADMIN_CLIENT: 'true',
   FEATURE_FLAG_AGREEMENT_APPROVAL_POLICY_UPDATE: 'true',
   SIGNALHUB_PERSONAL_DATA_PROCESS_URL: 'http://localhost',
+  FEATURE_FLAG_NOTIFICATION_CONFIG: 'true',
   FEATURE_FLAG_USE_SIGNED_DOCUMENT: 'true',
   DOCUMENTATION_URL:
     'https://developer.pagopa.it/pdnd-interoperabilita/guides/manuale-operativo-pdnd-interoperabilita/v1.0',
