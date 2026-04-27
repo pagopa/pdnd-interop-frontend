718077e5779cf93bada89c39e3eed0e16cf7ff24
martinaCampoli <martina.campoli@pagopa.it>
2026-04-27 12:28:09 +0200

    fix: enhance accessibility for FormLabel components in RiskAnalysisInputWrapper and RHFRadioGroup (PIN-A2-2683 - A11Y)

--- src/components/shared/react-hook-form-inputs/RHFRadioGroup.tsx
+++ src/components/shared/react-hook-form-inputs/RHFRadioGroup.tsx
@@ -55,7 +55,7 @@ export const RHFRadioGroup: React.FC<RHFRadioGroupProps> = ({
   })
 
   return (
-    <InputWrapper error={error} sx={sx} infoLabel={infoLabel} {...ids}>
+    <InputWrapper error={error} sx={sx} infoLabel={infoLabel} {...ids} component="fieldset">
       {label && (
         <FormLabel
           sx={{
@@ -67,7 +67,7 @@ export const RHFRadioGroup: React.FC<RHFRadioGroupProps> = ({
           }}
           id={labelId}
           required={required}
-          component="div" /* Use div due to accessibility reasons, otherwise label tag is rendered without for attribute */
+          component="legend"
         >
           {label}
         </FormLabel>
@@ -77,7 +77,7 @@ export const RHFRadioGroup: React.FC<RHFRadioGroupProps> = ({
         rules={mapValidationErrorMessages(rules, t)}
         render={({ field: { onChange, ...fieldProps } }) => (
           <MUIRadioGroup
-            aria-labelledby={labelId}
+            aria-labelledby={label ? labelId : undefined}
             {...props}
             {...fieldProps}
             {...accessibilityProps}
