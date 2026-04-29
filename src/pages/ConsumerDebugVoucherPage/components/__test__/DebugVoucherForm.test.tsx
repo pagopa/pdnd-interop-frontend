e92515beed92aa80474611526eed16d4de984b85
lorenzo.giacomini <lorenzo.giacomini@digitouch.it>
2026-04-29 16:18:18 +0200

    feat: added tests (PIN-9674)

--- src/pages/ConsumerDebugVoucherPage/components/__test__/DebugVoucherForm.test.tsx
+++ src/pages/ConsumerDebugVoucherPage/components/__test__/DebugVoucherForm.test.tsx
@@ -88,4 +88,85 @@ describe('DebugVoucherForm testing', () => {
       })
     )
   })
+
+  it('should include dpop_proof when user provides it', async () => {
+    const setDebugVoucherValuesMockFn = vi.fn()
+
+    const screen = renderWithApplicationContext(
+      <DebugVoucherForm setDebugVoucherValues={setDebugVoucherValuesMockFn} />,
+      {
+        withReactQueryContext: true,
+        withRouterContext: true,
+      }
+    )
+
+    const clientAssertionInput = screen.getByRole('textbox', {
+      name: 'clientAssertionLabel',
+    })
+
+    const dpopInput = screen.getByRole('textbox', {
+      name: 'dpopProofLabel',
+    })
+
+    const submitButton = screen.getByRole('button', { name: 'submitBtn' })
+
+    fireEvent.change(clientAssertionInput, {
+      target: { value: 'test client assertion' },
+    })
+
+    fireEvent.change(dpopInput, {
+      target: { value: 'test dpop proof' },
+    })
+
+    fireEvent.click(submitButton)
+
+    const request = createMockDebugVoucherRequest({
+      client_id: undefined,
+      dpop_proof: 'test dpop proof',
+      client_assertion: 'test client assertion',
+    })
+
+    await waitFor(() =>
+      expect(setDebugVoucherValuesMockFn).toBeCalledWith({
+        request: request,
+        response: response,
+      })
+    )
+  })
+
+  it('should send dpop_proof as undefined when empty', async () => {
+    const setDebugVoucherValuesMockFn = vi.fn()
+
+    const screen = renderWithApplicationContext(
+      <DebugVoucherForm setDebugVoucherValues={setDebugVoucherValuesMockFn} />,
+      {
+        withReactQueryContext: true,
+        withRouterContext: true,
+      }
+    )
+
+    const clientAssertionInput = screen.getByRole('textbox', {
+      name: 'clientAssertionLabel',
+    })
+
+    const submitButton = screen.getByRole('button', { name: 'submitBtn' })
+
+    fireEvent.change(clientAssertionInput, {
+      target: { value: 'test client assertion' },
+    })
+
+    fireEvent.click(submitButton)
+
+    const request = createMockDebugVoucherRequest({
+      client_assertion: 'test client assertion',
+      client_id: undefined,
+    })
+
+    await waitFor(() =>
+      expect(setDebugVoucherValuesMockFn).toBeCalledWith({
+        request: request,
+        response: response,
+      })
+    )
+  })
 })
