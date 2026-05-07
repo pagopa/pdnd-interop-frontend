c8788a37bcfe169804bb1c639dc02d9ce0ab6cc4
alten-dturus <davide.turus@alten.it>
2026-05-07 14:36:08 +0200

    feat: set bgColor (PIN-9937)

--- src/components/layout/containers/NewPageContainer.tsx
+++ src/components/layout/containers/NewPageContainer.tsx
@@ -220,7 +220,7 @@ export const HeaderInfoSection: React.FC<HeaderInfoSectionProps> = ({
   actions = [],
 }) => {
   return (
-    <Box bgcolor="ThreeDFace" py={2} px={2} borderRadius={1} mt={3}>
+    <Box bgcolor="grey.100" py={2} px={2} borderRadius={1} mt={3}>
       <Stack direction="row" alignItems="center" justifyContent="space-between">
         <Stack direction="row" spacing={4} alignItems="center">
           <Typography component="h2" variant="body2" textTransform="uppercase">
