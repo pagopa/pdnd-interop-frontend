f0aa51bba0f64bfae545da5b557252606b24f316
lorenzo.giacomini <lorenzo.giacomini@digitouch.it>
2026-05-08 11:53:32 +0200

    feat: fixed condition on eserviceId field (PIN-9879)

--- src/pages/ConsumerSimulateGetVoucherPage/components/VoucherInstructions/VoucherInstructionsGeneralForm/VoucherInstructionsGeneralFormCurrentIdsDrawer.tsx
+++ src/pages/ConsumerSimulateGetVoucherPage/components/VoucherInstructions/VoucherInstructionsGeneralForm/VoucherInstructionsGeneralFormCurrentIdsDrawer.tsx
@@ -132,7 +132,7 @@ export const VoucherInstructionsGeneralFormCurrentIdsDrawer: React.FC<
             />
           </>
         )}
-        {eserviceId && (
+        {eserviceId && !purposeId && (
           <>
             <InformationContainer
               direction="column"
