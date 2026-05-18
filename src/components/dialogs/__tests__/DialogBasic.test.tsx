27f41a991b2281152b99c2546bc10b74ec00a92e
Denis Lantero <denis.lantero@gmail.com>
2026-05-18 12:47:32 +0200

    test: cover async confirmation flow (PIN-10036)

--- src/components/dialogs/__tests__/DialogBasic.test.tsx
+++ src/components/dialogs/__tests__/DialogBasic.test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { DialogBasic } from '../DialogBasic'
 import { apiGuideLink } from '@/config/constants'
 import { renderWithApplicationContext } from '@/utils/testing.utils'
@@ -17,5 +18,30 @@ describe('DialogBasic', () => {
     )
 
     expect(screen.getByRole('link', { name: 'guide' })).toHaveAttribute('href', apiGuideLink)
+  })
+
+  it('should require checking the confirmation checkbox before proceeding', async () => {
+    const user = userEvent.setup()
+    const onProceed = vi.fn()
+
+    renderWithApplicationContext(
+      <DialogBasic
+        type="basic"
+        title="Dialog title"
+        checkbox="I understand"
+        onProceed={onProceed}
+      />,
+      {}
+    )
+
+    const confirmButton = screen.getByRole('button', { name: 'confirm' })
+    expect(confirmButton).toBeDisabled()
+
+    await user.click(screen.getByRole('checkbox', { name: 'I understand' }))
+    expect(confirmButton).toBeEnabled()
+
+    await user.click(confirmButton)
+
+    expect(onProceed).toHaveBeenCalledTimes(1)
   })
 })
