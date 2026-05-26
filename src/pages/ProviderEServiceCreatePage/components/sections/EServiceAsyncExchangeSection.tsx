fd9a4c960880e3539f4d03d281b29cab5814289c
Denis Lantero <denis.lantero@gmail.com>
2026-05-26 17:41:15 +0200

    fix: align async template instance callback layout (PIN-10183)

--- src/pages/ProviderEServiceCreatePage/components/sections/EServiceAsyncExchangeSection.tsx
+++ src/pages/ProviderEServiceCreatePage/components/sections/EServiceAsyncExchangeSection.tsx
@@ -1,6 +1,3 @@
-import { Stack } from '@mui/material'
-import { InformationContainer } from '@pagopa/interop-fe-commons'
-import { useTranslation } from 'react-i18next'
 import { useEServiceCreateContext } from '../EServiceCreateContext'
 import { UploadCallbackInterfaceDoc } from '../components/UploadCallbackInterfaceDoc'
 import { EServiceAsyncExchangeSectionBase } from './EServiceAsyncExchangeSectionBase'
@@ -14,20 +11,11 @@ export const EServiceAsyncExchangeSection: React.FC<EServiceAsyncExchangeSection
   areEServiceGeneralInfoEditable,
   isEServiceCreatedFromTemplate = false,
 }) => {
-  const { t } = useTranslation('eservice', {
-    keyPrefix: 'create.step4.asyncExchangeSection',
-  })
-
   const { descriptor } = useEServiceCreateContext()
   const isSoap = descriptor?.eservice.technology === 'SOAP'
   const readOnlyCallbackInterfaceContent = <UploadCallbackInterfaceDoc readOnly />
   const editableCallbackInterfaceContent = isEServiceCreatedFromTemplate ? (
-    <Stack sx={{ mt: 2 }}>
-      <InformationContainer
-        label={t('callbackInterface.readOnlyLabel')}
-        content={readOnlyCallbackInterfaceContent}
-      />
-    </Stack>
+    readOnlyCallbackInterfaceContent
   ) : (
     <UploadCallbackInterfaceDoc />
   )
