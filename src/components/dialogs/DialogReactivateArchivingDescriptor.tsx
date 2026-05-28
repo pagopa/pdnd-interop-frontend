3b55dafa19022e3ed508f8a1bbbcabf05665826e
Alepazz <alessio.pazzani@gmail.com>
2026-05-28 14:39:57 +0200

    chore: extract DialogConfirmArchivingAction shared component to remove duplication (PIN-9944)

--- src/components/dialogs/DialogReactivateArchivingDescriptor.tsx
+++ src/components/dialogs/DialogReactivateArchivingDescriptor.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import {
-  Box,
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Typography,
-} from '@mui/material'
+import { Typography } from '@mui/material'
 import { Trans, useTranslation } from 'react-i18next'
 import { EServiceMutations } from '@/api/eservice'
 import { useDialog } from '@/stores'
 import type { DialogReactivateArchivingDescriptorProps } from '@/types/dialog.types'
+import { DialogConfirmArchivingAction } from './DialogConfirmArchivingAction'
 
 export const DialogReactivateArchivingDescriptor: React.FC<
   DialogReactivateArchivingDescriptorProps
@@ -19,53 +12,26 @@ export const DialogReactivateArchivingDescriptor: React.FC<
   const { t } = useTranslation('eservice', {
     keyPrefix: 'read.dialogReactivateArchivingDescriptor',
   })
-  const { t: tCommon } = useTranslation('common', { keyPrefix: 'actions' })
   const { closeDialog } = useDialog()
-  const ariaLabelId = React.useId()
-  const ariaDescriptionId = React.useId()
-
   const { mutate: reactivate } = EServiceMutations.useReactivateVersion({ skipConfirmation: true })
 
-  const handleProceed = () => reactivate({ eserviceId, descriptorId }, { onSuccess: closeDialog })
-
   return (
-    <Dialog
-      aria-labelledby={ariaLabelId}
-      aria-describedby={ariaDescriptionId}
-      open
-      onClose={closeDialog}
-      fullWidth
-      maxWidth="sm"
-    >
-      <DialogTitle id={ariaLabelId}>{t('title')}</DialogTitle>
-
-      <DialogContent id={ariaDescriptionId}>
-        <Typography variant="body2" sx={{ mb: 1 }}>
-          {t('intro')}
-        </Typography>
-        <Box component="ul" sx={{ pl: 3, my: 0 }}>
-          <li>{t('bullets.usableAgain')}</li>
-          <li>
-            <Trans
-              t={t}
-              i18nKey="bullets.archivingNotAffected"
-              components={{
-                strong: <Typography component="span" variant="inherit" fontWeight={600} />,
-              }}
-            />
-          </li>
-          <li>{t('bullets.archivedAfterNotice')}</li>
-        </Box>
-      </DialogContent>
-
-      <DialogActions>
-        <Button variant="outlined" onClick={closeDialog}>
-          {tCommon('cancel')}
-        </Button>
-        <Button variant="contained" onClick={handleProceed}>
-          {t('proceedLabel')}
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <DialogConfirmArchivingAction
+      title={t('title')}
+      intro={t('intro')}
+      primaryBulletText={t('bullets.usableAgain')}
+      archivingNotAffectedBullet={
+        <Trans
+          t={t}
+          i18nKey="bullets.archivingNotAffected"
+          components={{
+            strong: <Typography component="span" variant="inherit" fontWeight={600} />,
+          }}
+        />
+      }
+      archivedAfterNoticeText={t('bullets.archivedAfterNotice')}
+      confirmLabel={t('proceedLabel')}
+      onConfirm={() => reactivate({ eserviceId, descriptorId }, { onSuccess: closeDialog })}
+    />
   )
 }
--- src/components/dialogs/DialogSuspendArchivingEservice.tsx
+++ src/components/dialogs/DialogSuspendArchivingEservice.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import {
-  Box,
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Typography,
-} from '@mui/material'
+import { Typography } from '@mui/material'
 import { Trans, useTranslation } from 'react-i18next'
 import { EServiceMutations } from '@/api/eservice'
 import { useDialog } from '@/stores'
 import type { DialogSuspendArchivingEserviceProps } from '@/types/dialog.types'
+import { DialogConfirmArchivingAction } from './DialogConfirmArchivingAction'
 
 export const DialogSuspendArchivingEservice: React.FC<DialogSuspendArchivingEserviceProps> = ({
   eserviceId,
@@ -20,56 +13,26 @@ export const DialogSuspendArchivingEservice: React.FC<DialogSuspendArchivingEser
   const { t } = useTranslation('eservice', { keyPrefix: 'read.dialogSuspendArchivingEservice' })
   const { t: tCommon } = useTranslation('common', { keyPrefix: 'actions' })
   const { closeDialog } = useDialog()
-  const ariaLabelId = React.useId()
-  const ariaDescriptionId = React.useId()
-
   const { mutate: suspend } = EServiceMutations.useSuspendVersion({ skipConfirmation: true })
 
-  const handleProceed = () => suspend({ eserviceId, descriptorId }, { onSuccess: closeDialog })
-
   return (
-    <Dialog
-      aria-labelledby={ariaLabelId}
-      aria-describedby={ariaDescriptionId}
-      open
-      onClose={closeDialog}
-      fullWidth
-      maxWidth="sm"
-    >
-      <DialogTitle id={ariaLabelId}>{t('title')}</DialogTitle>
-
-      <DialogContent id={ariaDescriptionId}>
-        <Typography variant="body2" sx={{ mb: 1 }}>
-          {t('intro')}
-        </Typography>
-        <Box component="ul" sx={{ pl: 3, my: 0 }}>
-          <li>{t('bullets.suspended')}</li>
-          <li>
-            <Trans
-              t={t}
-              i18nKey="bullets.archivingNotAffected"
-              components={{
-                strong: <Typography component="span" variant="inherit" fontWeight={600} />,
-              }}
-            />
-          </li>
-          <li>{t('bullets.archivedAfterNotice')}</li>
-        </Box>
-      </DialogContent>
-
-      <DialogActions>
-        <Button variant="outlined" onClick={closeDialog}>
-          {tCommon('cancel')}
-        </Button>
-        <Button
-          variant="contained"
-          color="error"
-          onClick={handleProceed}
-          sx={{ color: 'common.white' }}
-        >
-          {tCommon('suspend')}
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <DialogConfirmArchivingAction
+      title={t('title')}
+      intro={t('intro')}
+      primaryBulletText={t('bullets.suspended')}
+      archivingNotAffectedBullet={
+        <Trans
+          t={t}
+          i18nKey="bullets.archivingNotAffected"
+          components={{
+            strong: <Typography component="span" variant="inherit" fontWeight={600} />,
+          }}
+        />
+      }
+      archivedAfterNoticeText={t('bullets.archivedAfterNotice')}
+      confirmLabel={tCommon('suspend')}
+      confirmColor="error"
+      onConfirm={() => suspend({ eserviceId, descriptorId }, { onSuccess: closeDialog })}
+    />
   )
 }
