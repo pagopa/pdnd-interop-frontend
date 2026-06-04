8f924fe22099736d57326f140669a9c7cd42169e
lorenzo.giacomini <lorenzo.giacomini@digitouch.it>
2026-06-04 12:55:06 +0200

    feat: extracted risk analysis summary page to custom hook (PIN-10191)

--- src/pages/RiskAnalysisSummaryPage/RiskAnalysisSummary.page.tsx
+++ src/pages/RiskAnalysisSummaryPage/RiskAnalysisSummary.page.tsx
@@ -2,72 +2,37 @@ import { PageContainer } from '@/components/layout/containers'
 import { SummaryAccordion, SummaryAccordionSkeleton } from '@/components/shared/SummaryAccordion'
 import { Alert, Button, Stack, Tooltip } from '@mui/material'
 import React from 'react'
+import CreateIcon from '@mui/icons-material/Create'
+import SendIcon from '@mui/icons-material/Send'
+import { useTranslation } from 'react-i18next'
+
 import {
   ConsumerPurposeSummaryGeneralInformationAccordion,
   ConsumerPurposeSummaryRiskAnalysisAccordion,
 } from '../ConsumerPurposeSummaryPage/components'
+
 import { ConsumerPurposeSummaryRiskAnalysisAlertContainer } from '../ConsumerPurposeSummaryPage/components/ConsumerPurposeSummaryRiskAnalysisAlertContainer'
-import CreateIcon from '@mui/icons-material/Create'
-import SendIcon from '@mui/icons-material/Send'
-import { useNavigate, useParams } from '@/router'
-import { useQuery } from '@tanstack/react-query'
-import { PurposeQueries } from '@/api/purpose'
-import { useGetConsumerPurposeAlertProps } from '../ConsumerPurposeSummaryPage/hooks/useGetConsumerPurposeAlertProps'
-import { checkIsRulesetExpired } from '@/utils/purpose.utils'
-import { useTranslation } from 'react-i18next'
+
+import { useRiskAnalysisSummaryPage } from './hooks/useRiskAnalysisSummaryPage'
 
 const RiskAnalysisSummaryPage: React.FC = () => {
   const { t } = useTranslation('purpose')
-  const { t: tCommon } = useTranslation('common', { keyPrefix: 'actions' })
+  const { t: tCommon } = useTranslation('common', {
+    keyPrefix: 'actions',
+  })
 
-  const { purposeId } = useParams<'SUBSCRIBE_RISK_ANALYSIS_SUMMARY'>()
-
-  const navigate = useNavigate()
-
-  const { data: purpose, isLoading } = useQuery(PurposeQueries.getSingle(purposeId))
-
-  const isEserviceDeliverMode = purpose?.eservice.mode === 'DELIVER'
-
-  const expirationDate = purpose?.rulesetExpiration
-
-  const isRulesetExpired = checkIsRulesetExpired(expirationDate)
-
-  const alertProps = useGetConsumerPurposeAlertProps(purpose)
-
-  const eservicePersonalData = purpose?.eservice.personalData
-
-  const checkIncompatibleAnswerValue = () => {
-    const userAnswer = purpose?.riskAnalysisForm?.answers['usesPersonalData']?.[0]
-    const isYes = userAnswer === 'YES'
-    const isNo = userAnswer === 'NO'
-
-    const incompatible =
-      (isYes && eservicePersonalData !== true) || (isNo && eservicePersonalData !== false)
-
-    return incompatible
-  }
-
-  const isPublishButtonDisabled =
-    (purpose?.riskAnalysisForm &&
-      eservicePersonalData !== undefined &&
-      checkIncompatibleAnswerValue()) ||
-    (isEserviceDeliverMode && isRulesetExpired)
-
-  const arePublishOrEditButtonsDisabled =
-    purpose?.agreement.state === 'ARCHIVED' || purpose?.eservice.descriptor.state === 'ARCHIVED'
-
-  const handleEditDraft = () => {
-    navigate('SUBSCRIBE_RISK_ANALYSIS_COMPILE', {
-      params: {
-        purposeId,
-      },
-    })
-  }
-
-  const handleApproveDraft = () => {
-    if (!purpose?.currentVersion) return
-    /* @TODO: logic will be added with https://pagopa.atlassian.net/browse/PIN-10193 */
-  }
+  const {
+    purposeId,
+    isLoading,
+    alertProps,
+    isPublishButtonDisabled,
+    arePublishOrEditButtonsDisabled,
+    handleEditDraft,
+    handleApproveDraft,
+    isEserviceDeliverMode,
+    expirationDate,
+    isRulesetExpired,
+  } = useRiskAnalysisSummaryPage()
 
   return (
     <PageContainer
@@ -79,31 +44,36 @@ const RiskAnalysisSummaryPage: React.FC = () => {
       }}
     >
       {alertProps && <Alert sx={{ mb: 3 }} {...alertProps} />}
+
       <Stack spacing={3} sx={{ mt: !alertProps ? 3 : 0 }}>
         <React.Suspense fallback={<SummaryAccordionSkeleton />}>
           <SummaryAccordion
             headline="1"
             title={t('riskAnalysisSummary.generalInformationSection.title')}
-            defaultExpanded={true}
+            defaultExpanded
           >
             <ConsumerPurposeSummaryGeneralInformationAccordion purposeId={purposeId} />
           </SummaryAccordion>
         </React.Suspense>
+
         <React.Suspense fallback={<SummaryAccordionSkeleton />}>
           <SummaryAccordion headline="2" title={t('summary.riskAnalysisSection.title')}>
             <ConsumerPurposeSummaryRiskAnalysisAccordion purposeId={purposeId} />
           </SummaryAccordion>
         </React.Suspense>
       </Stack>
+
       {isEserviceDeliverMode && (
         <ConsumerPurposeSummaryRiskAnalysisAlertContainer
           expirationDate={expirationDate}
           isRulesetExpired={isRulesetExpired}
         />
       )}
+
       <Alert severity="info" sx={{ mt: 4 }}>
         {t('riskAnalysisSummary.infoAlert')}
       </Alert>
+
       <Stack spacing={1} sx={{ mt: 4 }} direction="row" justifyContent="end">
         <Button
           disabled={arePublishOrEditButtonsDisabled}
---
+++ src/pages/RiskAnalysisSummaryPage/hooks/useRiskAnalysisSummaryPage.ts
@@ -0,0 +1,61 @@
+import { PurposeQueries } from '@/api/purpose'
+import { useNavigate, useParams } from '@/router'
+import { checkIsRulesetExpired } from '@/utils/purpose.utils'
+import { useQuery } from '@tanstack/react-query'
+import { useGetConsumerPurposeAlertProps } from '../../ConsumerPurposeSummaryPage/hooks/useGetConsumerPurposeAlertProps'
+
+export function useRiskAnalysisSummaryPage() {
+  const { purposeId } = useParams<'SUBSCRIBE_RISK_ANALYSIS_SUMMARY'>()
+  const navigate = useNavigate()
+
+  const { data: purpose, isLoading } = useQuery(PurposeQueries.getSingle(purposeId))
+
+  const isEserviceDeliverMode = purpose?.eservice.mode === 'DELIVER'
+
+  const expirationDate = purpose?.rulesetExpiration
+  const isRulesetExpired = checkIsRulesetExpired(expirationDate)
+
+  const alertProps = useGetConsumerPurposeAlertProps(purpose)
+
+  const eservicePersonalData = purpose?.eservice.personalData
+
+  const riskAnswer = purpose?.riskAnalysisForm?.answers?.usesPersonalData?.[0]
+
+  const isRiskAnswerIncompatible =
+    !!riskAnswer &&
+    eservicePersonalData !== undefined &&
+    ((riskAnswer === 'YES' && eservicePersonalData !== true) ||
+      (riskAnswer === 'NO' && eservicePersonalData !== false))
+
+  const isPublishButtonDisabled =
+    (purpose?.riskAnalysisForm && isRiskAnswerIncompatible) ||
+    (isEserviceDeliverMode && isRulesetExpired)
+
+  const arePublishOrEditButtonsDisabled =
+    purpose?.agreement.state === 'ARCHIVED' || purpose?.eservice.descriptor.state === 'ARCHIVED'
+
+  const handleEditDraft = () => {
+    navigate('SUBSCRIBE_RISK_ANALYSIS_COMPILE', {
+      params: { purposeId },
+    })
+  }
+
+  const handleApproveDraft = () => {
+    if (!purpose?.currentVersion) return
+    // TODO PIN-10193
+  }
+
+  return {
+    purposeId,
+    purpose,
+    isLoading,
+    isEserviceDeliverMode,
+    expirationDate,
+    isRulesetExpired,
+    alertProps,
+    isPublishButtonDisabled,
+    arePublishOrEditButtonsDisabled,
+    handleEditDraft,
+    handleApproveDraft,
+  }
+}
