5bddfcc8d7f5a4af41932c77c1e258139bca47c9
lorenzo.giacomini <lorenzo.giacomini@digitouch.it>
2026-06-09 09:05:24 +0200

    feat: fix catalog view permission for reviewer (PIN-10263)

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -199,7 +199,7 @@ export const { routes, reactRouterDOMRoutes, hooks, components, utils } = new In
     element: <ConsumerEServiceDetailsPage />,
     public: false,
     hideSideNav: false,
-    authLevels: ['admin', 'support', 'security', 'api', 'viewer'],
+    authLevels: ['admin', 'support', 'security', 'api', 'reviewer', 'viewer'],
   })
   .addRoute({
     key: 'SUBSCRIBE_CATALOG_LIST',
