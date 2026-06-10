5c286b91ec82f62b36d762104132413844ac3dda
Luca Pierfederici <lpier97@gmail.com>
2026-06-10 11:15:46 +0200

    fix: add missing route authLevels for viewer role

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -367,7 +367,7 @@ export const { routes, reactRouterDOMRoutes, hooks, components, utils } = new In
     redirect: 'SUBSCRIBE_AGREEMENT_LIST',
     public: false,
     hideSideNav: false,
-    authLevels: ['admin', 'support', 'security', 'api'],
+    authLevels: ['admin', 'support', 'security', 'api', 'viewer'],
   })
   .addRoute({
     key: 'PARTY_REGISTRY',
@@ -439,7 +439,7 @@ export const { routes, reactRouterDOMRoutes, hooks, components, utils } = new In
     redirect: 'PARTY_REGISTRY',
     public: false,
     hideSideNav: false,
-    authLevels: ['admin', 'support', 'api', 'security'],
+    authLevels: ['admin', 'support', 'api', 'security', 'viewer'],
   })
   .addRoute({
     key: 'TENANT_CERTIFIER',
