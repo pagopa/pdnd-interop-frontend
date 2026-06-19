fe657f2453eedde7983a4c957a398607b664688b
Denis Lantero <denis.lantero@gmail.com>
2026-06-19 11:09:37 +0200

    fix: hide product updates banner links (PIN-10357)

--- src/components/shared/banners/ProductUpdatesBanner.tsx
+++ src/components/shared/banners/ProductUpdatesBanner.tsx
@@ -1,21 +1,9 @@
 import React from 'react'
 import { useProductUpdatesBanner } from '@/hooks/bannerHooks/useProductUpdatesBanner'
 import { Banner } from './Banner'
-import Button from '@mui/material/Button'
-import OpenInNewIcon from '@mui/icons-material/OpenInNew'
-import { archiveEserviceGuideLink, userRolesGuideLink } from '@/config/constants'
 
 export const ProductUpdatesBanner: React.FC = () => {
-  const {
-    title,
-    text,
-    action1Label,
-    action2Label,
-    action1AriaLabel,
-    action2AriaLabel,
-    isOpen,
-    closeBanner,
-  } = useProductUpdatesBanner()
+  const { title, text, isOpen, closeBanner } = useProductUpdatesBanner()
 
   return (
     <Banner
@@ -23,30 +11,30 @@ export const ProductUpdatesBanner: React.FC = () => {
       content={text}
       isOpen={isOpen}
       setIsOpen={closeBanner}
-      action1={
-        <Button
-          size="small"
-          color="inherit"
-          endIcon={<OpenInNewIcon />}
-          key="action1"
-          onClick={() => window.open(archiveEserviceGuideLink, '_blank')}
-          aria-label={action1AriaLabel}
-        >
-          {action1Label}
-        </Button>
-      }
-      action2={
-        <Button
-          size="small"
-          color="inherit"
-          endIcon={<OpenInNewIcon />}
-          key="action2"
-          onClick={() => window.open(userRolesGuideLink, '_blank')}
-          aria-label={action2AriaLabel}
-        >
-          {action2Label}
-        </Button>
-      }
+      // action1={
+      //   <Button
+      //     size="small"
+      //     color="inherit"
+      //     endIcon={<OpenInNewIcon />}
+      //     key="action1"
+      //     onClick={() => window.open(archiveEserviceGuideLink, '_blank')}
+      //     aria-label={action1AriaLabel}
+      //   >
+      //     {action1Label}
+      //   </Button>
+      // }
+      // action2={
+      //   <Button
+      //     size="small"
+      //     color="inherit"
+      //     endIcon={<OpenInNewIcon />}
+      //     key="action2"
+      //     onClick={() => window.open(userRolesGuideLink, '_blank')}
+      //     aria-label={action2AriaLabel}
+      //   >
+      //     {action2Label}
+      //   </Button>
+      // }
     />
   )
 }
