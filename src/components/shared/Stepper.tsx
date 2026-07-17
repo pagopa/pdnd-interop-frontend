c829e74815949d815bb763160b4c72b5b0880028
martinaCampoli <martina.campoli@pagopa.it>
2026-07-17 17:04:58 +0200

    feat: enhance accessibility for stepper component with status labels and translations (PIN-2657 - A11Y)

--- src/components/shared/Stepper.tsx
+++ src/components/shared/Stepper.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Stepper as MUIStepper, Step, StepLabel, Box } from '@mui/material'
+import { visuallyHidden } from '@mui/utils'
 import type { StepperStep } from '@/types/common.types'
+import { useTranslation } from 'react-i18next'
 
 type StepperProps = {
   steps: Array<StepperStep>
@@ -8,14 +10,32 @@ type StepperProps = {
 }
 
 export function Stepper({ steps, activeIndex }: StepperProps) {
+  const { t } = useTranslation('shared-components', { keyPrefix: 'stepper' })
+
   return (
     <Box sx={{ py: 3 }}>
       <MUIStepper activeStep={activeIndex} alternativeLabel>
-        {steps.map(({ label }) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
+        {steps.map(({ label }, index) => {
+          const isCompleted = index < activeIndex
+          const isCurrent = index === activeIndex
+
+          let statusText = ''
+          if (isCompleted) statusText = t('completeLabel') + ': '
+          else if (isCurrent) statusText = t('currentStepLabel') + ': '
+
+          return (
+            <Step key={label}>
+              {/* force the non-interactive label into the tab order to improve accessibility */}
+              <StepLabel tabIndex={0}>
+                <span style={visuallyHidden}>
+                  {statusText}
+                  {t('stepperLabel', { currentStep: index + 1, totalSteps: steps.length })}:
+                </span>
+                {label}
+              </StepLabel>
+            </Step>
+          )
+        })}
       </MUIStepper>
     </Box>
   )
