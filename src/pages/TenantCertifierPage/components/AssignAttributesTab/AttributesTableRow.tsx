0237abc306bc4f17ef03f66f4caa59bcf446b8af
alten-dturus <davide.turus@alten.it>
2026-07-29 18:26:39 +0200

    feat: implemented new drawer to change attribute value and added to AssignAttributesTab table

--- src/pages/TenantCertifierPage/components/AssignAttributesTab/AttributesTableRow.tsx
+++ src/pages/TenantCertifierPage/components/AssignAttributesTab/AttributesTableRow.tsx
@@ -1,21 +1,27 @@
 import type { RequesterCertifiedAttribute } from '@/api/api.generatedTypes'
 import { AuthHooks } from '@/api/auth'
-import { ButtonSkeleton } from '@/components/shared/MUI-skeletons'
+import { ActionMenu, ActionMenuSkeleton } from '@/components/shared/ActionMenu'
+import { useDrawerState } from '@/hooks/useDrawerState'
 import { useDialog } from '@/stores'
-import { Button, Skeleton } from '@mui/material'
+import { formatThousands } from '@/utils/format.utils'
+import type { ActionItem } from '@/types/common.types'
+import { Skeleton } from '@mui/material'
 import { TableRow } from '@pagopa/interop-fe-commons'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import ChangeAttributeValueDrawer from './ChangeAttributeValueDrawer'
 
 type AttributesTableRowProps = {
   attribute: RequesterCertifiedAttribute
 }
 
 export const AttributesTableRow: React.FC<AttributesTableRowProps> = ({ attribute }) => {
-  const { t } = useTranslation('common')
+  const { t } = useTranslation('party', { keyPrefix: 'tenantCertifier.assignTab' })
 
   const { openDialog } = useDialog()
   const { isAdmin } = AuthHooks.useJwt()
+
+  const { isOpen, openDrawer, closeDrawer } = useDrawerState()
 
   const handleRevoke = () => {
     openDialog({
@@ -24,21 +30,42 @@ export const AttributesTableRow: React.FC<AttributesTableRowProps> = ({ attribut
     })
   }
 
+  const attributeValue = attribute.discreteValue ? formatThousands(attribute.discreteValue) : '-'
+
+  const revokeAction: ActionItem = {
+    action: handleRevoke,
+    label: t(`revokeAttributeBtn`),
+  }
+
+  const changeValueAction: ActionItem = {
+    action: openDrawer,
+    label: t('changeAttributeValueBtn'),
+  }
+
   return (
-    <TableRow cellData={[attribute.tenantName, attribute.attributeName]}>
-      {isAdmin && (
-        <Button variant="outlined" color="error" size="small" onClick={handleRevoke}>
-          {t(`actions.revoke`)}
-        </Button>
+    <>
+      <TableRow cellData={[attribute.tenantName, attribute.attributeName, attributeValue]}>
+        {isAdmin && (
+          <ActionMenu
+            actions={
+              attribute.kind === 'CERTIFIED_DISCRETE'
+                ? [revokeAction, changeValueAction]
+                : [revokeAction]
+            }
+          />
+        )}
+      </TableRow>
+      {attribute.kind === 'CERTIFIED_DISCRETE' && (
+        <ChangeAttributeValueDrawer isOpen={isOpen} onClose={closeDrawer} attribute={attribute} />
       )}
-    </TableRow>
+    </>
   )
 }
 
 export const AttributesTableRowSkeleton: React.FC = () => {
   return (
     <TableRow cellData={[<Skeleton key={0} width={220} />, <Skeleton key={1} width={220} />]}>
-      <ButtonSkeleton size="small" width={100} />
+      <ActionMenuSkeleton />
     </TableRow>
   )
 }
