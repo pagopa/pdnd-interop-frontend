44d28c295bf109c62091a8545ebd79fa16a3c61a
lorenzo.giacomini <lorenzo.giacomini@digitouch.it>
2026-08-06 08:49:54 +0200

    chore: new types

--- src/api/api.generatedTypes.ts
+++ src/api/api.generatedTypes.ts
@@ -523,7 +523,6 @@ export interface CatalogEServiceDescriptor {
   archivingSchedule?: ArchivingSchedule;
   asyncExchangeProperties?: AsyncExchangeProperties;
   asyncExchangeCallbackInterface?: EServiceDoc;
-  templateRef?: EServiceTemplateRef;
 }
 
 /** Models Client details */
@@ -2871,6 +2870,10 @@ export interface ReviewerWorkflow {
   signingState: RiskAnalysisSigningState;
   /** @format uuid */
   signedBy?: string;
+  /** @format date-time */
+  signedAt?: string;
+  /** @format uuid */
+  rejectedBy?: string;
   rejectionReason?: string;
 }
 
